32e0e87c75524965896cf6e33435faf7fc798129
Gilad Gray <ggray@palantir.com>
2016-10-26 11:26:10 -0700

    uh fix docsDist paths
    how did these break? i did this already...

--- scripts/docsDist.sh
+++ scripts/docsDist.sh
@@ -2,8 +2,8 @@
 
 find packages/docs/node_modules/@blueprint -type l -delete
 
-mkdir -p packages/docs/node_modules/@blueprint/core/build/src
-cp -fR packages/core/build/src packages/docs/node_modules/@blueprint/core/build/
+mkdir -p packages/docs/node_modules/@blueprint/core/dist
+cp -fR packages/core/dist packages/docs/node_modules/@blueprint/core/build/
 
-mkdir -p packages/docs/node_modules/@blueprint/datetime/build/src
-cp -fR packages/datetime/build/src packages/docs/node_modules/@blueprint/datetime/build/
+mkdir -p packages/docs/node_modules/@blueprint/datetime/dist
+cp -fR packages/datetime/dist packages/docs/node_modules/@blueprint/datetime/build/
