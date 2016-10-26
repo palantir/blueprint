7bcd0e0e1e55e861eb13a8be549c79bbf756c4d9
Gilad Gray <ggray@palantir.com>
2016-10-26 13:24:26 -0700

    holy hell well this is the real fix

--- scripts/docsDist.sh
+++ scripts/docsDist.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+# Circle does not handle symlinks in artifacts so we must turn lerna's symlinked local deps into actual directories.
+
 find packages/docs/node_modules/@blueprint -type l -delete
 
-mkdir -p packages/docs/node_modules/@blueprint/core/dist
-cp -fR packages/core/dist packages/docs/node_modules/@blueprint/core/
+mkdir -p packages/docs/node_modules/@blueprint/core/
+mkdir -p packages/docs/node_modules/@blueprint/datetime/
 
-mkdir -p packages/docs/node_modules/@blueprint/datetime/dist
-cp -fR packages/datetime/dist packages/docs/node_modules/@blueprint/datetime/
+cp -fR packages/core packages/datetime packages/docs/node_modules/@blueprint/
