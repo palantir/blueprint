e794eccb13f5ecc4389876cda28c210394858418
Gilad Gray <giladgray@gmail.com>
2016-10-26 10:34:01 -0700

    update test NPM deps (#3)
    * remove mocha from test/typings
    
    * update karma npm packages
    
    - didn't update istanbul-instrumenter-loader cuz it breaks everything (including itself?)
    
    * update mocha to 3.x
    
    - removes a few obnoxious packages like jade
    
    * add note explaing why sinon comes from tsd

--- test/typings/tsd.d.ts
+++ test/typings/tsd.d.ts
@@ -1,2 +1,14 @@
-/// <reference path="mocha/mocha.d.ts" />
 /// <reference path="sinon/sinon.d.ts" />
+
+/*
+NOTE
+
+Sinon lives here instead of @types because the two typings seem to encourage different usages,
+and this one better fits ours.
+
+With this typings file, a global `sinon` variable is declared so tests can easily reference `sinon.spy()`
+without an import. The `karma-sinon` plugin supports this usage.
+
+Conversely, the @types version _does not_ declare that global correctly so the package must be imported.
+But `import "sinon"` causes all sorts of webpack issues where the dependencies cannot be analyzed.
+*/
