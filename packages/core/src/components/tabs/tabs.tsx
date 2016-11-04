b740a2f665b0dfb92951229f6824d4b37bebd5f2
Chris Yu <cyu@palantir.com>
2016-11-04 00:55:17 -0700

    Fix initialSelectedTabIndex unexpectedly setting tabs back to position 0 on rerender

--- packages/core/src/components/tabs/tabs.tsx
+++ packages/core/src/components/tabs/tabs.tsx
@@ -65,7 +65,7 @@ export class Tabs extends AbstractComponent<ITabsProps, ITabsState> {
 
     public displayName = "Blueprint.Tabs";
     public state: ITabsState = {
-        selectedTabIndex: Tabs.defaultProps.initialSelectedTabIndex,
+        selectedTabIndex: null,
     };
 
     private panelIds: string[] = [];
@@ -342,7 +342,7 @@ export class Tabs extends AbstractComponent<ITabsProps, ITabsState> {
 
         if (this.isValidTabIndex(selectedTabIndex)) {
             return { selectedTabIndex };
-        } else if (this.isValidTabIndex(initialSelectedTabIndex)) {
+        } else if (this.isValidTabIndex(initialSelectedTabIndex) && this.state.selectedTabIndex == null) {
             return { selectedTabIndex: initialSelectedTabIndex };
         } else {
             return this.state;
