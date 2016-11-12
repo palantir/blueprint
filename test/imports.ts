d56717b51c5dfd51da14b1903eb62637bebd2474
Adi Dahiya <adi.dahiya14@gmail.com>
2016-11-12 15:56:02 -0500

    Update test/imports.ts (#103)

--- test/imports.ts
+++ test/imports.ts
@@ -1,13 +1,11 @@
-/*
- * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+/**
+ * @license Copyright 2016 Palantir Technologies, Inc. All rights reserved.
  */
 
-/*
-Quick explanation of what's going on here:
-
-This file imports every Blueprint package with JS exports to ensure that the typings can be consumed by various
-TypeScript project configurations. A Gulp task at the bottom of `gulp/typescript.js` attempts to compile this file
-in TypeScript 2.0 with `--strictNullChecks`.
+/**
+ * @fileoverview This file imports every Blueprint package with JS exports to ensure that the typings can be
+ * consumed by various TypeScript project configurations. A Gulp task at the bottom of `gulp/typescript.js`
+ * attempts to compile this file in TypeScript 2.0 with `--strictNullChecks`.
 */
 
 // import each blueprint package that provides JS exports to ensure that the typings can be consumed
