f60ade53057cc7d1ae27e79126be5f5605df58ad
Chris Lewis <clewis@palantir.com>
2016-11-14 19:22:52 -0800

    Clean up code in webpack.config.js

--- packages/landing/webpack.config.js
+++ packages/landing/webpack.config.js
@@ -1,50 +1,51 @@
+"use strict";
+
 var path = require("path");
-var webpack = require("webpack");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const SRC  = path.resolve("./src");
+const SRC = path.resolve("./src");
 const DEST = path.resolve("./dist");
 const NAME = "blueprint-landing.js";
 
 module.exports = {
-  entry: [
-    `${SRC}/index.tsx`,
-  ],
-
-  output: {
-    path: DEST,
-    filename: NAME,
-  },
-
-  resolve: {
-    extensions: ["", ".js", ".ts", ".tsx"],
-  },
-
-  ts: {
-    compilerOptions: { declaration: false },
-  },
-
-  module: {
-    loaders: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        include: SRC,
-      }, {
-        test: /\.s[ac]ss$/,
-        loader: ExtractTextPlugin.extract("style", "css!postcss!sass"),
-        include: SRC,
-      }, {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        loader: require.resolve('file-loader') + '?name=resources/fonts/[name].[ext]',
-      }, {
-        test: /resources\/img\/.*\.(svg|png)$/,
-        loader: require.resolve('file-loader') + '?name=resources/img/[name].[ext]',
-      }
+    entry: [
+        `${SRC}/index.tsx`,
     ],
-  },
 
-  plugins: [
-    new ExtractTextPlugin(NAME.replace(/\.js$/, ".css")),
-  ]
+    output: {
+        path: DEST,
+        filename: NAME,
+    },
+
+    resolve: {
+        extensions: ["", ".js", ".ts", ".tsx"],
+    },
+
+    ts: {
+        compilerOptions: { declaration: false },
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                include: SRC,
+            }, {
+                test: /\.s[ac]ss$/,
+                loader: ExtractTextPlugin.extract("style", "css!postcss!sass"),
+                include: SRC,
+            }, {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                loader: require.resolve('file-loader') + '?name=resources/fonts/[name].[ext]',
+            }, {
+                test: /resources\/img\/.*\.(svg|png)$/,
+                loader: require.resolve('file-loader') + '?name=resources/img/[name].[ext]',
+            },
+        ],
+    },
+
+    plugins: [
+        new ExtractTextPlugin(NAME.replace(/\.js$/, ".css")),
+    ],
 };
