347c3095e9834b719e48f5c21fa815802723ce9c
Chris Lewis <clewis@palantir.com>
2016-11-16 16:18:00 -0800

    Reorder Keys alphabetically, add Shift key code

--- packages/core/src/common/keys.ts
+++ packages/core/src/common/keys.ts
@@ -3,10 +3,11 @@
  * Licensed under the Apache License, Version 2.0 - http://www.apache.org/licenses/LICENSE-2.0
  */
 
-export const ARROW_DOWN = 40;
-export const ARROW_LEFT = 37;
-export const ARROW_RIGHT = 39;
-export const ARROW_UP = 38;
 export const ENTER = 13;
+export const SHIFT = 16;
 export const ESCAPE = 27;
 export const SPACE = 32;
+export const ARROW_LEFT = 37;
+export const ARROW_UP = 38;
+export const ARROW_RIGHT = 39;
+export const ARROW_DOWN = 40;
