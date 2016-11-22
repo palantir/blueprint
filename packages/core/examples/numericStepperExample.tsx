3c60d33a5acad02cfccc41808fde184d93bdc3e1
Chris Lewis <clewis@palantir.com>
2016-11-22 13:06:44 -0800

    Delete unneeded example comments

--- packages/core/examples/numericStepperExample.tsx
+++ packages/core/examples/numericStepperExample.tsx
@@ -8,10 +8,10 @@ import * as React from "react";
 import BaseExample, { handleBooleanChange, handleNumberChange } from "./common/baseExample";
 import { IntentSelect } from "./common/intentSelect";
 import {
-    ButtonPosition,
     Classes,
     Intent,
     NumericStepper,
+    NumericStepperButtonPosition,
     Switch,
 } from "@blueprintjs/core";
 
@@ -74,9 +74,9 @@ const MAJOR_STEP_SIZES = [
 
 const BUTTON_POSITIONS = [
     null,
-    "left" as ButtonPosition,
-    "right" as ButtonPosition,
-    "split" as ButtonPosition,
+    "left" as NumericStepperButtonPosition,
+    "right" as NumericStepperButtonPosition,
+    "split" as NumericStepperButtonPosition,
 ];
 
 export class NumericStepperExample extends BaseExample<INumericStepperExampleState> {
@@ -165,58 +165,8 @@ export class NumericStepperExample extends BaseExample<INumericStepperExampleSta
         );
         // return (
         //     <div className="pt-numeric-stepper-example">
-        //         <code>{"<NumericStepper />"}</code>
-        //         <NumericStepper />
-        //         <code>{"<NumericStepper buttonPosition='right' />"}</code>
-        //         <NumericStepper buttonPosition="right" />
-        //         <code>{"<NumericStepper buttonPosition='split' />"}</code>
-        //         <NumericStepper buttonPosition="split" />
-        //         <code>{"<NumericStepper buttonPosition='left' />"}</code>
-        //         <NumericStepper buttonPosition="left" />
-        //         <code>{"<NumericStepper buttonPosition='none' />"}</code>
-        //         <NumericStepper buttonPosition="none" />
-        //         <code>{"<NumericStepper intent={Intent.PRIMARY} />"}</code>
-        //         <NumericStepper intent={Intent.PRIMARY} />
-        //         <code>{"<NumericStepper intent={Intent.SUCCESS} />"}</code>
-        //         <NumericStepper intent={Intent.SUCCESS} />
-        //         <code>{"<NumericStepper intent={Intent.WARNING} />"}</code>
-        //         <NumericStepper intent={Intent.WARNING} />
-        //         <code>{"<NumericStepper intent={Intent.DANGER} />"}</code>
-        //         <NumericStepper intent={Intent.DANGER} />
-        //         <code>{"<NumericStepper intent={Intent.DANGER} leftIconName='variable' />"}</code>
-        //         <NumericStepper intent={Intent.DANGER} leftIconName="variable" />
-        //         <code>{"<NumericStepper placeholder='Enter a number...' />"}</code>
-        //         <NumericStepper placeholder="Enter a number..." />
-        //         <code>{"<NumericStepper value='10' />"}</code>
-        //         <NumericStepper value="10" />
-        //         <code>{"<NumericStepper value='10' disabled />"}</code>
-        //         <NumericStepper value="10" disabled />
-        //         <code>{"<NumericStepper value='10' readOnly />"}</code>
-        //         <NumericStepper value="10" readOnly />
-        //         <code>{"<NumericStepper min={4} />"}</code>
-        //         <NumericStepper min={4} />
-        //         <code>{"<NumericStepper max={4} />"}</code>
-        //         <NumericStepper max={4} />
-        //         <code>{"<NumericStepper min={0} max={10} />"}</code>
-        //         <NumericStepper min={0} max={10} />
-        //         <code>{"<NumericStepper min={0} max={100} stepSize={2} minorStepSize={0.2} majorStepSize={4} buttonPosition='split' />"}</code>
-        //         <NumericStepper min={0} max={100} stepSize={2} minorStepSize={0.2} majorStepSize={4} buttonPosition="split" />
-        //         <code>{"<NumericStepper value={4} />"}</code>
-        //         <NumericStepper value={4} />
-        //         <code>{"<NumericStepper value={'4'} />"}</code>
-        //         <NumericStepper value={'4'} />
         //         <code>{"<NumericStepper onChange={console.log} />"}</code>
         //         <NumericStepper onChange={console.log} />
-        //         <code>{"<NumericStepper placeholder={'Enter a number...'} />"}</code>
-        //         <NumericStepper placeholder={"Enter a number..."} />
-        //         <code>{"<NumericStepper leftIconName={'variable'} />"}</code>
-        //         <NumericStepper leftIconName={"variable"} />
-        //         <code>{"<NumericStepper minorStepSize={null} />"}</code>
-        //         <NumericStepper minorStepSize={null} />
-        //         <code>{"<NumericStepper majorStepSize={null} />"}</code>
-        //         <NumericStepper majorStepSize={null} />
-        //         <code>{"<NumericStepper minorStepSize={null} majorStepSize={null} />"}</code>
-        //         <NumericStepper minorStepSize={null} majorStepSize={null} />
         //         <code>{"<NumericStepper onSubmit={this.onSubmit} /> (this example can do math!)"}</code>
         //         <NumericStepper onDone={this.onDone} value={this.state.value} />
         //     </div>
