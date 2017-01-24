f1e180d942a08372efa91c350f25a429430126a4
Sebastian Brückner <sbrueckner@palantir.com>
2017-01-24 15:15:39 -0800

    Fix AnchorButton to expect HTMLAnchorElement props

--- packages/core/src/components/button/buttons.tsx
+++ packages/core/src/components/button/buttons.tsx
@@ -46,7 +46,7 @@ export class Button extends AbstractButton<HTMLButtonElement> {
 
 export const ButtonFactory = React.createFactory(Button);
 
-export class AnchorButton extends AbstractButton<HTMLButtonElement> {
+export class AnchorButton extends AbstractButton<HTMLAnchorElement> {
     public static displayName = "Blueprint.AnchorButton";
 
     public render() {
