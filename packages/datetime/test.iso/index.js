988c3c88f7973257cf103fbb536bd9c300c4ccbb
codebling <worsecodes@gmail.com>
2017-02-01 14:07:51 -0500

    Add test for server-side/isomorphic rendering (#381)
    * Move "create config" function to its own file in utils
    
    * Add isometric/server rendering tests to Gulp
    
    * Add core, datetime and table tests
    
    * Run both "karma" and "isotest" targets when "test" target is invoked
    
    * Keep CircleCI "karma" and "isotest" test categories separate for visibility
    
    * Remove unnecessary constants in 'datetime' module isotests
    
    * Remove unused constants
    
    * Rename 'create-config.js' to 'karma-config.js' for clarity
    
    * Use exact version of gulp-mocha (3.0.1)

---
+++ packages/datetime/test.iso/index.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015 Palantir Technologies, Inc. All rights reserved.
+ * Licensed under the BSD-3 License as modified (the “License”); you may obtain a copy
+ * of the license at https://github.com/palantir/blueprint/blob/master/LICENSE
+ * and https://github.com/palantir/blueprint/blob/master/PATENTS
+ */
+
+const Components = require("../dist");
+const ReactTestUtils = require("react-addons-test-utils");
+const React = require("react");
+
+/*
+ * Determines if the passed Component is a React Class or not
+ * `ReactTestUtils.isElement()`
+ */
+const isReactClass = (Component) => {
+    return typeof Component !== "undefined"
+        && typeof Component.prototype !== "undefined"
+        && typeof Component.prototype.constructor !== "undefined"
+        && typeof Component.prototype.render !== "undefined"
+    ;
+};
+
+const customProps = {
+};
+
+const customChildren = {
+};
+
+const shallowRenderer = ReactTestUtils.createRenderer();
+
+Object.keys(Components).forEach((ComponentKey) => {
+    describe(ComponentKey, () => {
+        const Component = Components[ComponentKey];
+
+        if (isReactClass(Component)) {
+            it("can be server-rendered", () => {
+                const element = React.createElement(
+                    Component,
+                    customProps[ComponentKey],
+                    customChildren[ComponentKey]
+                );
+                shallowRenderer.render(element);
+            });
+        }
+    });
+});
