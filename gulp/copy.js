e52a0f1361d63f92ad76a1309395926f33bee8ab
Gilad Gray <ggray@palantir.com>
2017-02-02 21:38:18 -0800

    copy-files => copy + remove `copy: false`

--- gulp/copy.js
+++ gulp/copy.js
@@ -7,23 +7,20 @@ module.exports = (blueprint, gulp, plugins) => {
     var mergeStream = require("merge-stream");
     var path = require("path");
 
-    blueprint.task("copy", "files", [], (project) => {
-        // allow for no-op on project dependencies
-        if (project.copy === false) {
-            return;
-        }
-
-        // copy options is a map of file globs to array of dest directories.
-        // given: "copy": { "path/to/file.txt": {to: ["foo/bar"], base: "path"} }
-        // the file at currProject/path/to/file.txt is copied to currProject/build/foo/bar/to/file.txt
-        return mergeStream(Object.keys(project.copy).map((key) => {
-            var dests = project.copy[key].to;
-            var base = project.copy[key].base || "";
-            var stream = gulp.src(path.join(project.cwd, key), { base: path.join(project.cwd, base) });
-            dests.forEach((dest) => {
-                stream = stream.pipe(gulp.dest(blueprint.destPath(project, dest)));
-            });
-            return stream;
-        })).pipe(plugins.count(`${project.id}: <%= files %> copied`));
+    blueprint.taskGroup({ block: "copy" }, (project, taskName) => {
+        gulp.task(taskName, () => {
+            // copy options is a map of file globs to array of dest directories.
+            // given: "copy": { "path/to/file.txt": {to: ["foo/bar"], base: "path"} }
+            // the file at currProject/path/to/file.txt is copied to currProject/build/foo/bar/to/file.txt
+            return mergeStream(Object.keys(project.copy).map((key) => {
+                var dests = project.copy[key].to;
+                var base = project.copy[key].base || "";
+                var stream = gulp.src(path.join(project.cwd, key), { base: path.join(project.cwd, base) });
+                dests.forEach((dest) => {
+                    stream = stream.pipe(gulp.dest(blueprint.destPath(project, dest)));
+                });
+                return stream;
+            })).pipe(plugins.count(`${project.id}: <%= files %> copied`));
+        });
     });
 };
