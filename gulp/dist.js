5bcd8b0e569ee1c515e64be0ce6bf3f98b57a0c7
Gilad Gray <ggray@palantir.com>
2017-02-02 23:01:57 -0800

    remove bump-*-version tasks
    replaced by lerna in scripts/prepRelease.sh

--- gulp/dist.js
+++ gulp/dist.js
@@ -3,7 +3,7 @@
  */
 "use strict";
 
-module.exports = (blueprint, gulp, plugins) => {
+module.exports = (blueprint, gulp) => {
     const fs = require("fs");
     const path = require("path");
     const mergeStream = require("merge-stream");
@@ -19,26 +19,6 @@ module.exports = (blueprint, gulp, plugins) => {
         });
     });
     gulp.task("bundle", blueprint.taskMapper("typescript", "bundle"));
-
-    [
-        "major",
-        "minor",
-        "patch",
-    ].forEach((versionBumpType) => {
-        gulp.task(`bump-${versionBumpType}-version`, () => {
-            const streams = blueprint.projects.map((project) => (
-                gulp.src(path.join(project.cwd, "package.json"))
-                    .pipe(plugins.bump({ type: versionBumpType }))
-                    .pipe(gulp.dest(project.cwd))
-            ));
-            streams.push(
-                gulp.src("package.json")
-                    .pipe(plugins.bump({ type: versionBumpType }))
-                    .pipe(gulp.dest("."))
-            );
-            return mergeStream(streams);
-        });
-    });
 
     // asserts that all main fields in package.json reference existing files
     function testDist(project) {
