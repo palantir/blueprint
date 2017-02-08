7988c8cc45732e7a66aa243879f1dba48d77fcff
Chris Lewis <clewis@palantir.com>
2017-02-08 12:58:58 -0800

    Show a date-range picker popover on focus - Add isOpen to state - Add handleInputClick to address popoover immediate-close bug - Add default min and max dates - Set to Position.TOP_RIGHT for now, since the example is at the bottom of the page

--- packages/datetime/src/dateRangeInput.tsx
+++ packages/datetime/src/dateRangeInput.tsx
@@ -14,12 +14,19 @@ import {
     IInputGroupProps,
     InputGroup,
     IProps,
+    Popover,
+    Position,
     Utils,
 } from "@blueprintjs/core";
 
 import {
+    getDefaultMaxDate,
+    getDefaultMinDate,
     IDatePickerBaseProps,
 } from "./datePickerCore";
+import {
+    DateRangePicker,
+} from "./dateRangePicker";
 
 export interface IDateRangeInputProps extends IDatePickerBaseProps, IProps {
     startInputProps?: IInputGroupProps;
@@ -27,6 +34,7 @@ export interface IDateRangeInputProps extends IDatePickerBaseProps, IProps {
 }
 
 export interface IDateRangeInputState {
+    isOpen?: boolean,
     selectedEnd?: moment.Moment;
     selectedStart?: moment.Moment;
 };
@@ -34,6 +42,8 @@ export interface IDateRangeInputState {
 export class DateRangeInput extends AbstractComponent<IDateRangeInputProps, IDateRangeInputState> {
     public static defaultProps: IDateRangeInputProps = {
         endInputProps: {},
+        maxDate: getDefaultMaxDate(),
+        minDate: getDefaultMinDate(),
         startInputProps: {},
     };
 
@@ -55,6 +65,7 @@ export class DateRangeInput extends AbstractComponent<IDateRangeInputProps, IDat
     public constructor(props: IDateRangeInputProps, context?: any) {
         super(props, context);
         this.state = {
+            isOpen: false,
             selectedEnd: null,
             selectedStart: null,
         };
@@ -64,18 +75,47 @@ export class DateRangeInput extends AbstractComponent<IDateRangeInputProps, IDat
         // allow custom props for each input group, but pass them in an order
         // that guarantees only some props are overridable.
         return (
-            <div className={Classes.CONTROL_GROUP}>
-                <InputGroup
-                    placeholder="Start date"
-                    {...this.props.startInputProps}
-                    inputRef={this.refHandlers.startInputRef}
-                />
-                <InputGroup
-                    placeholder="End date"
-                    {...this.props.endInputProps}
-                    inputRef={this.refHandlers.endInputRef}
-                />
-            </div>
+            <Popover
+                autoFocus={false}
+                content={<DateRangePicker maxDate={this.props.maxDate} minDate={this.props.minDate} />}
+                enforceFocus={false}
+                inline={true}
+                isOpen={this.state.isOpen}
+                onClose={this.handlePopoverClose}
+                position={Position.TOP_LEFT}
+                useSmartArrowPositioning={false}
+            >
+                <div className={Classes.CONTROL_GROUP}>
+                    <InputGroup
+                        placeholder="Start date"
+                        {...this.props.startInputProps}
+                        inputRef={this.refHandlers.startInputRef}
+                        onClick={this.handleInputClick}
+                        onFocus={this.handleInputFocus}
+                    />
+                    <InputGroup
+                        placeholder="End date"
+                        {...this.props.endInputProps}
+                        inputRef={this.refHandlers.endInputRef}
+                        onClick={this.handleInputClick}
+                        onFocus={this.handleInputFocus}
+                    />
+                </div>
+            </Popover>
         );
+    }
+
+    private handleInputClick = (e: React.MouseEvent<HTMLInputElement>) => {
+        // unless we stop propagation on this event, a click within an input
+        // will close the popover almost as soon as it opens.
+        e.stopPropagation();
+    }
+
+    private handleInputFocus = () => {
+        this.setState({ isOpen: true });
+    }
+
+    private handlePopoverClose = () => {
+        this.setState({ isOpen: false });
     }
 }
