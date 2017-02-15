e88ad4d895591bb7905040fde036ffefedfcdb44
Gilad Gray <ggray@palantir.com>
2017-02-15 16:13:48 -0500

    script to convert scss docs to md files

---
+++ mdify.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const glob = require("glob");
+const fs = require("fs");
+
+// captures: [title, body, reference]
+const BLOCK_REGEX = /\/\*\n(.+)\n\n((?:.|\n)+?)\nStyleguide:? (.+)\n\*\//g;
+
+const HEADINGS = "######";
+
+glob.sync("packages/core/src/components/*/*.scss")
+    .forEach((comp) => {
+        const scss = fs.readFileSync(comp, "utf-8").replace(/^ */gm, "");
+        const contents = ["---\nparent: components\n---"];
+
+        /** @type {RegExpExecArray} */
+        let match;
+        // tslint:disable-next-line:no-conditional-assignment
+        while ((match = BLOCK_REGEX.exec(scss)) !== null) {
+            const [title, body, reference] = match.slice(1);
+            const depth = reference.split(".").length - 1;
+
+            contents.push(
+                `${HEADINGS.slice(0, depth)} ${title}`,
+                body.trim().replace(/@\w+(-\w+)/g, (tag, ext) => tag.replace(ext, ext[1].toUpperCase() + ext.slice(2)))
+            );
+        }
+        fs.writeFileSync(comp.replace(".scss", ".md").replace("/_", "/"), contents.join("\n\n") + "\n");
+    });
