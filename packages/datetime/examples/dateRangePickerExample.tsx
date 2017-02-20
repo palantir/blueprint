c25892f167873e6be72112ef6d54f43e0465b00e
Antoine Llorca <llorca@users.noreply.github.com>
2017-02-20 12:03:03 -0800

    Contrain -> Constrain

--- packages/datetime/examples/dateRangePickerExample.tsx
+++ packages/datetime/examples/dateRangePickerExample.tsx
@@ -64,7 +64,7 @@ export class DateRangePickerExample extends BaseExample<IDateRangePickerExampleS
                 <Switch
                     checked={this.state.contiguousCalendarMonths}
                     key="Contiguous"
-                    label="Contrain to contiguous months"
+                    label="Constrain to contiguous months"
                     onChange={this.toggleContiguousCalendarMonths}
                 />,
                 <Switch
