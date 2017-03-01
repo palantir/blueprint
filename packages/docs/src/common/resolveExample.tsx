12f1f11160d20d19be1d1e35b53ef0d4df12c742
Gilad Gray <ggray@palantir.com>
2017-03-01 15:33:42 -0800

    refactor resolvers to return JSX.Elements

--- packages/docs/src/common/resolveExample.tsx
+++ packages/docs/src/common/resolveExample.tsx
@@ -11,51 +11,64 @@ import * as CoreExamples from "@blueprintjs/core/examples";
 import * as DateExamples from "@blueprintjs/datetime/examples";
 import * as TableExamples from "@blueprintjs/table/examples";
 
+import { getTheme } from "./theme";
+
+export type ExampleComponentClass = React.ComponentClass<{ getTheme: () => string }>;
+
+// construct a map of package name to all examples defined in that package.
+// packageName must match directory name as it is used to generate sourceUrl.
+type ExampleMap = {
+    [packageName: string]: {
+        [componentName: string]: ExampleComponentClass;
+    };
+};
+
 const Examples: ExampleMap = {
     core: CoreExamples as any,
     datetime: DateExamples as any,
     table: TableExamples as any,
 };
 
-export type ExampleComponentClass = React.ComponentClass<{ getTheme: () => string }>;
-
-// construct a map of package name to all examples defined in that package.
-// packageName must match directory name as it is used to generate sourceUrl.
-export type ExampleMap = {
-    [packageName: string]: {
-        [componentName: string]: ExampleComponentClass;
-    };
-};
-
-export interface IResolvedExample {
-    component: ExampleComponentClass;
-    sourceUrl: string;
-}
-
 /**
  * Searches the given examples for a component with the given name and returns the class
  * and name of package in which it was found.
  */
-export function getExample(componentName: string, examples: ExampleMap) {
+function getExample(componentName: string, examples: ExampleMap) {
     for (const packageName of Object.keys(examples)) {
         const component = examples[packageName][componentName];
         if (component != null) {
             return { component, packageName };
         }
     }
-    return { component: null, packageName: null };
+    return { component: undefined, packageName: undefined };
 }
 
 const SRC_HREF_BASE = "https://github.com/palantir/blueprint/blob/master/packages";
+
+export interface IExampleProps {
+    component: ExampleComponentClass;
+    sourceUrl: string;
+}
+
+const Example: React.SFC<IExampleProps> = (props) => (
+    <div className="kss-example-wrapper">
+        <div className="kss-example">
+            {React.createElement(props.component, { getTheme })}
+        </div>
+        <a className="view-example-source" href={props.sourceUrl} target="_blank">
+            <span className="pt-icon-standard pt-icon-code">&nbsp;</span>View source on GitHub
+        </a>
+    </div>
+);
 
 /**
  * Given the name of an example component, like `"AlertExample"`, attempts to resolve
  * it to an actual example component exported by one of the packages. Also returns
  * the URL of the source code on GitHub.
  */
-export function resolveExample(exampleName: string): IResolvedExample {
+export function resolveExample(exampleName: string, key: React.Key) {
     if (exampleName == null) {
-        return { component: null, sourceUrl: "" };
+        return undefined;
     }
 
     const { component, packageName } = getExample(exampleName, Examples);
@@ -63,8 +76,9 @@ export function resolveExample(exampleName: string): IResolvedExample {
         throw new Error(`Unknown example component: ${exampleName}`);
     }
     const fileName = exampleName.charAt(0).toLowerCase() + exampleName.slice(1) + ".tsx";
-    return {
-        component,
-        sourceUrl: [SRC_HREF_BASE, packageName, "examples", fileName].join("/"),
-    };
+    return <Example
+        component={component}
+        key={key}
+        sourceUrl={[SRC_HREF_BASE, packageName, "examples", fileName].join("/")}
+    />;
 }
