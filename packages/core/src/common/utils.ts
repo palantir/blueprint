41251504c47758796f147c2a3f975cb0c4d105d8
Gilad Gray <giladgray@gmail.com>
2017-03-03 11:38:36 -0800

    add deprecation warning to Tabs constructor (#767)

--- packages/core/src/common/utils.ts
+++ packages/core/src/common/utils.ts
@@ -5,6 +5,14 @@
  * and https://github.com/palantir/blueprint/blob/master/PATENTS
  */
 
+// only accessible within this file, so use `Utils.isNodeEnv` from the outside.
+declare var process: { env: any };
+
+/** Returns whether `process.env.NODE_ENV` exists and equals `env`. */
+export function isNodeEnv(env: string) {
+    return typeof process !== "undefined" && process.env && process.env.NODE_ENV === env;
+}
+
 /** Returns whether the value is a function. Acts as a type guard. */
 export function isFunction(value: any): value is Function {
     return typeof value === "function";
