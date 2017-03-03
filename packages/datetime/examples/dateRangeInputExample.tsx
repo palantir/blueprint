02d4e9b404c69ed69a2bc431b606ae4df91e54bf
Chris Lewis <clewis@palantir.com>
2017-03-02 20:26:20 -0800

    Add format selector to example

--- packages/datetime/examples/dateRangeInputExample.tsx
+++ packages/datetime/examples/dateRangeInputExample.tsx
@@ -5,13 +5,41 @@
  * and https://github.com/palantir/blueprint/blob/master/PATENTS
  */
 
-import BaseExample from "@blueprintjs/core/examples/common/baseExample";
+import { Radio, RadioGroup } from "@blueprintjs/core";
+import BaseExample, { handleStringChange } from "@blueprintjs/core/examples/common/baseExample";
 import * as React from "react";
 
 import { DateRangeInput } from "../src";
 
-export class DateRangeInputExample extends BaseExample<{}> {
+export interface IDateRangeInputExampleState {
+    format?: string;
+}
+
+export class DateRangeInputExample extends BaseExample<IDateRangeInputExampleState> {
+    public state: IDateRangeInputExampleState = {
+        format: "DD/MM/YYYY",
+    };
+
+    private toggleFormat = handleStringChange((format) => this.setState({ format }));
+
     protected renderExample() {
-        return <DateRangeInput />;
+        return <DateRangeInput {...this.state} />;
+    }
+
+    protected renderOptions() {
+        return [
+            [
+                <RadioGroup
+                    key="Format"
+                    label="Date format"
+                    onChange={this.toggleFormat}
+                    selectedValue={this.state.format}
+                >
+                    <Radio label="DD/MM/YYYY" value="DD/MM/YYYY" />
+                    <Radio label="MM-DD-YYYY" value="MM-DD-YYYY" />
+                    <Radio label="YYYY-MM-DD" value="YYYY-MM-DD" />
+                </RadioGroup>,
+            ],
+        ];
     }
 }
