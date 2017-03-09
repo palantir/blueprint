palantir/blueprint

commit 0aaaf0d7cdfd46eb9741c60638d441484c7df776

Refactor css classes into single file for table (#685)
* Refactor css classes into single file for table

Same as the other packages. Fixes #438

* Alphabetize classes, other minor fixes and comment addressing

* final periods.

packages/table/src/locator.ts
@@ -5,6 +5,7 @@
  * and https://github.com/palantir/blueprint/blob/master/PATENTS
  */
 
+import * as Classes from "./common/classes";
 import { Grid } from "./common/grid";
 import { Rect } from "./common/rect";
 import { Utils } from "./common/utils";
@@ -60,8 +61,8 @@ export class Locator implements ILocator {
 
     public getWidestVisibleCellInColumn(columnIndex: number): number {
         const cellClasses = [
-            `.bp-table-cell-col-${columnIndex}`,
-            ".bp-table-column-name",
+            `.${Classes.columnIndexClass(columnIndex)}`,
+            `.${Classes.TABLE_COLUMN_NAME}`,
         ];
         const cells = this.tableElement.querySelectorAll(cellClasses.join(", "));
         let max = 0;

packages/table/src/cell/cell.tsx
@@ -8,8 +8,9 @@
 import * as classNames from "classnames";
 import * as PureRender from "pure-render-decorator";
 import * as React from "react";
+import * as Classes from "../common/classes";
 
-import { Classes, IIntentProps, IProps } from "@blueprintjs/core";
+import { Classes as CoreClasses, IIntentProps, IProps } from "@blueprintjs/core";
 
 import { LoadableContent } from "../common/loadableContent";
 
@@ -49,9 +50,6 @@ export type ICellRenderer = (rowIndex: number, columnIndex: number) => React.Rea
 
 export const emptyCellRenderer = () => <Cell />;
 
-export const CELL_CLASSNAME = "bp-table-cell";
-export const CELL_INTERACTIVE_CLASSNAME = "bp-table-cell-interactive";
-
 @PureRender
 export class Cell extends React.Component<ICellProps, {}> {
     public static defaultProps = {
@@ -62,17 +60,17 @@ export class Cell extends React.Component<ICellProps, {}> {
         const { style, intent, interactive, loading, tooltip, truncated, className } = this.props;
 
         const classes = classNames(
-            CELL_CLASSNAME,
-            Classes.intentClass(intent),
+            Classes.TABLE_CELL,
+            CoreClasses.intentClass(intent),
             {
-                [CELL_INTERACTIVE_CLASSNAME]: interactive,
-                [Classes.LOADING]: loading,
+                [Classes.TABLE_CELL_INTERACTIVE]: interactive,
+                [CoreClasses.LOADING]: loading,
             },
             className,
         );
 
         const content = truncated ?
-            <div className="bp-table-truncated-text">{this.props.children}</div> : this.props.children;
+            <div className={Classes.TABLE_TRUNCATED_TEXT}>{this.props.children}</div> : this.props.children;
 
         return (
             <div className={classes} style={style} title={tooltip}>

packages/table/src/cell/editableCell.tsx
@@ -11,6 +11,7 @@ import * as ReactDOM from "react-dom";
 
 import { EditableText, Utils } from "@blueprintjs/core";
 
+import * as Classes from "../common/classes";
 import { Draggable } from "../interactions/draggable";
 import { Cell, ICellProps } from "./cell";
 
@@ -65,7 +66,7 @@ export class EditableCell extends React.Component<IEditableCellProps, IEditableC
                     stopPropagation={interactive}
                 >
                     <EditableText
-                        className={"bp-table-editable-name"}
+                        className={Classes.TABLE_EDITABLE_NAME}
                         defaultValue={value}
                         intent={intent}
                         minWidth={null}

packages/table/src/cell/formats/jsonFormat.tsx
@@ -8,6 +8,7 @@
 import * as classNames from "classnames";
 import * as PureRender from "pure-render-decorator";
 import * as React from "react";
+import * as Classes from "../../common/classes";
 import { ITruncatedFormatProps, TruncatedFormat, TruncatedPopoverMode } from "./truncatedFormat";
 
 /* istanbul ignore next */
@@ -48,7 +49,7 @@ export class JSONFormat extends React.Component<IJSONFormatProps, {}> {
             showPopover = TruncatedPopoverMode.NEVER;
         }
         const className = classNames(this.props.className, {
-          "bp-table-null": isNully,
+          [Classes.TABLE_NULL]: isNully,
         });
 
         let displayValue = "";

packages/table/src/cell/formats/truncatedFormat.tsx
@@ -5,9 +5,12 @@
  * and https://github.com/palantir/blueprint/blob/master/PATENTS
  */
 
-import { IProps, Popover, Position } from "@blueprintjs/core";
+import { Classes as CoreClasses, IProps, Popover, Position } from "@blueprintjs/core";
+
 import * as classNames from "classnames";
 import * as React from "react";
+
+import * as Classes from "../../common/classes";
 
 export enum TruncatedPopoverMode {
     ALWAYS,
@@ -87,33 +90,39 @@ export class TruncatedFormat extends React.Component<ITruncatedFormatProps, ITru
 
         if (this.shouldShowPopover(content)) {
             const popoverClasses = classNames(
-                "bp-table-truncated-popover",
-                preformatted ? "bp-table-popover-whitespace-pre" : "bp-table-popover-whitespace-normal",
+                Classes.TABLE_TRUNCATED_POPOVER,
+                preformatted ? Classes.TABLE_POPOVER_WHITESPACE_PRE : Classes.TABLE_POPOVER_WHITESPACE_NORMAL,
             );
             const popoverContent = <div className={popoverClasses}>{children}</div>;
-            const className = classNames(this.props.className, "bp-table-truncated-format");
+            const className = classNames(this.props.className, Classes.TABLE_TRUNCATED_FORMAT);
             const constraints = [{
                 attachment: "together",
                 pin: true,
                 to: "window",
             }];
+
+            const iconClasses = classNames(
+                CoreClasses.ICON_STANDARD,
+                CoreClasses.iconClass("more"),
+            );
+
             return (
                 <div className={className}>
-                    <div className="bp-table-truncated-value" ref={this.handleContentDivRef}>{cellContent}</div>
+                    <div className={Classes.TABLE_TRUNCATED_VALUE} ref={this.handleContentDivRef}>{cellContent}</div>
                     <Popover
-                        className="bp-table-truncated-popover-target"
+                        className={Classes.TABLE_TRUNCATED_POPOVER_TARGET}
                         constraints={constraints}
                         content={popoverContent}
                         position={Position.BOTTOM}
                         useSmartArrowPositioning
                         useSmartPositioning
                     >
-                        <span className="pt-icon-standard pt-icon-more"/>
+                        <span className={iconClasses}/>
                     </Popover>
                 </div>
             );
         } else {
-            const className = classNames(this.props.className, "bp-table-truncated-text");
+            const className = classNames(this.props.className, Classes.TABLE_TRUNCATED_TEXT);
             return <div className={className} ref={this.handleContentDivRef}>{cellContent}</div>;
         }
     }

packages/table/src/common/classes.ts
@@ -1,0 +1,100 @@
+/*
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ * Licensed under the BSD-3 License as modified (the “License”); you may obtain a copy
+ * of the license at https://github.com/palantir/blueprint/blob/master/LICENSE
+ * and https://github.com/palantir/blueprint/blob/master/PATENTS
+ */
+
+export const TABLE_BODY = "bp-table-body";
+export const TABLE_BODY_SCROLL_CLIENT = "bp-table-body-scroll-client";
+export const TABLE_BODY_VIRTUAL_CLIENT = "bp-table-body-virtual-client";
+export const TABLE_BOTTOM_CONTAINER = "bp-table-bottom-container";
+export const TABLE_CELL = "bp-table-cell";
+export const TABLE_CELL_CLIENT = "bp-table-cell-client";
+export const TABLE_CELL_GHOST = "bp-table-cell-ghost";
+export const TABLE_CELL_INTERACTIVE = "bp-table-cell-interactive";
+export const TABLE_CELL_LEDGER_EVEN = "bp-table-cell-ledger-even";
+export const TABLE_CELL_LEDGER_ODD = "bp-table-cell-ledger-odd";
+export const TABLE_COLUMN_HEADER_TR = "bp-table-column-header-tr";
+export const TABLE_COLUMN_HEADERS = "bp-table-column-headers";
+export const TABLE_COLUMN_NAME = "bp-table-column-name";
+export const TABLE_COLUMN_NAME_TEXT = "bp-table-column-name-text";
+export const TABLE_CONTAINER = "bp-table-container";
+export const TABLE_DRAGGABLE = "bp-table-draggable";
+export const TABLE_DRAGGING = "bp-table-dragging";
+export const TABLE_EDITABLE_NAME = "bp-table-editable-name";
+export const TABLE_HEADER = "bp-table-header";
+export const TABLE_HEADER_ACTIVE = "bp-table-header-active";
+export const TABLE_HEADER_CONTENT = "bp-table-header-content";
+export const TABLE_HEADER_SELECTED = "bp-table-header-selected";
+export const TABLE_HORIZONTAL_CELL_DIVIDER = "bp-table-horizontal-cell-divider";
+export const TABLE_HORIZONTAL_GUIDE = "bp-table-horizontal-guide";
+export const TABLE_INTERACTION_BAR = "bp-table-interaction-bar";
+export const TABLE_LAST_IN_COLUMN = "bp-table-last-in-column";
+export const TABLE_LAST_IN_ROW = "bp-table-last-in-row";
+export const TABLE_MENU = "bp-table-menu";
+export const TABLE_NO_HORIZONTAL_SCROLL = "bp-table-no-horizontal-scroll";
+export const TABLE_NO_LAYOUT = "bp-table-no-layout";
+export const TABLE_NO_VERTICAL_SCROLL = "bp-table-no-vertical-scroll";
+export const TABLE_NULL = "bp-table-null";
+export const TABLE_OVERLAY = "bp-table-overlay";
+export const TABLE_OVERLAY_LAYER = "bp-table-overlay-layer";
+export const TABLE_POPOVER_WHITESPACE_NORMAL = "bp-table-popover-whitespace-normal";
+export const TABLE_POPOVER_WHITESPACE_PRE = "bp-table-popover-whitespace-pre";
+export const TABLE_REGION = "bp-table-region";
+export const TABLE_RESIZE_GUIDES = "bp-table-resize-guides";
+export const TABLE_RESIZE_HANDLE = "bp-table-resize-handle";
+export const TABLE_RESIZE_HANDLE_TARGET = "bp-table-resize-handle-target";
+export const TABLE_RESIZE_HORIZONTAL = "bp-table-resize-horizontal";
+export const TABLE_RESIZE_SENSOR = "bp-table-resize-sensor";
+export const TABLE_RESIZE_SENSOR_EXPAND = "bp-table-resize-sensor-expand";
+export const TABLE_RESIZE_SENSOR_SHRINK = "bp-table-resize-sensor-shrink";
+export const TABLE_RESIZE_VERTICAL = "bp-table-resize-vertical";
+export const TABLE_ROUNDED_LAYOUT = "bp-table-rounded-layout";
+export const TABLE_ROW_HEADERS = "bp-table-row-headers";
+export const TABLE_ROW_NAME = "bp-table-row-name";
+export const TABLE_ROW_NAME_TEXT = "bp-table-row-name-text";
+export const TABLE_SELECTION_ENABLED = "bp-table-selection-enabled";
+export const TABLE_SELECTION_REGION = "bp-table-selection-region";
+export const TABLE_TH_MENU = "bp-table-th-menu";
+export const TABLE_THEAD = "bp-table-thead";
+export const TABLE_TOP_CONTAINER = "bp-table-top-container";
+export const TABLE_TRUNCATED_FORMAT = "bp-table-truncated-format";
+export const TABLE_TRUNCATED_POPOVER = "bp-table-truncated-popover";
+export const TABLE_TRUNCATED_POPOVER_TARGET = "bp-table-truncated-popover-target";
+export const TABLE_TRUNCATED_TEXT = "bp-table-truncated-text";
+export const TABLE_TRUNCATED_VALUE = "bp-table-truncated-value";
+export const TABLE_VERTICAL_GUIDE = "bp-table-vertical-guide";
+
+/** Common code for row and column index class generator functions, since they're essentially the same. */
+function dimensionIndexClass(classPrefix: string, index: string | number) {
+    if (index == null) {
+        return undefined;
+    }
+
+    if (typeof index === "number") {
+        return `${classPrefix}${index}`;
+    }
+
+    return index.indexOf(classPrefix) === 0 ? index : `${classPrefix}${index}`;
+}
+
+/** Return CSS class for table colummn index, whether or not 'bp-table-col-' prefix is included. */
+export function columnIndexClass(columnIndex: string | number) {
+    return dimensionIndexClass("bp-table-col-", columnIndex);
+}
+
+/** Return CSS class for table row index, whether or not 'bp-table-row-' prefix is included. */
+export function rowIndexClass(rowIndex: string | number) {
+    return dimensionIndexClass("bp-table-row-", rowIndex);
+}
+
+/** Return CSS class for table colummn cell index, whether or not 'bp-table-cell-col-' prefix is included. */
+export function columnCellIndexClass(columnIndex: string | number) {
+    return dimensionIndexClass("bp-table-cell-col-", columnIndex);
+}
+
+/** Return CSS class for table row cell index, whether or not 'bp-table-cell-row-' prefix is included. */
+export function rowCellIndexClass(rowIndex: string | number) {
+    return dimensionIndexClass("bp-table-cell-row-", rowIndex);
+}

packages/table/src/common/grid.ts
@@ -8,6 +8,7 @@
 import { CSSProperties } from "react";
 
 import { IRegion, RegionCardinality, Regions } from "../regions";
+import * as Classes from "./classes";
 import { Rect } from "./rect";
 import { Utils } from "./utils";
 
@@ -24,11 +25,6 @@ export interface IColumnIndices {
     columnIndexStart: number;
     columnIndexEnd: number;
 }
-
-const EXTREMA_LAST_IN_ROW = ["bp-table-last-in-row"];
-const EXTREMA_LAST_IN_COLUMN = ["bp-table-last-in-column"];
-const EXTREMA_LAST_IN_ROW_AND_COLUMN = ["bp-table-last-in-column", "bp-table-last-in-row"];
-const EXTREMA_NONE: string[] = [];
 
 /**
  * This class manages the sizes of grid cells using arrays of individual row/column sizes.
@@ -298,15 +294,15 @@ export class Grid {
 
     public getExtremaClasses(rowIndex: number, columnIndex: number, rowEnd: number, columnEnd: number) {
         if (rowIndex === rowEnd && columnIndex === columnEnd) {
-            return EXTREMA_LAST_IN_ROW_AND_COLUMN;
+            return [Classes.TABLE_LAST_IN_COLUMN, Classes.TABLE_LAST_IN_ROW];
         }
         if (rowIndex === rowEnd) {
-            return EXTREMA_LAST_IN_COLUMN;
+            return [Classes.TABLE_LAST_IN_COLUMN];
         }
         if (columnIndex === columnEnd) {
-            return EXTREMA_LAST_IN_ROW;
+            return [Classes.TABLE_LAST_IN_ROW];
         }
-        return EXTREMA_NONE;
+        return [];
     }
 
     public getRegionStyle(region: IRegion): CSSProperties {

packages/table/src/common/roundSize.tsx
@@ -6,6 +6,7 @@
  */
 
 import * as React from "react";
+import * as Classes from "./classes";
 
 /**
  * A React component that measures and rounds the size of its only child. This
@@ -19,8 +20,8 @@ export class RoundSize extends React.Component<{}, {}> {
 
     public render() {
         return (
-            <div className="bp-table-rounded-layout" ref={this.setContainerRef}>
-                <div className="bp-table-no-layout" ref={this.setInternalRef}>
+            <div className={Classes.TABLE_ROUNDED_LAYOUT} ref={this.setContainerRef}>
+                <div className={Classes.TABLE_NO_LAYOUT} ref={this.setInternalRef}>
                     {React.Children.only(this.props.children)}
                 </div>
             </div>

commit 08f085738f7c09f47b64e3e35346598e1c1ea1b2

Fix column auto resize
Fix bug I accidentally introduced in #685

packages/table/src/locator.ts
@@ -61,7 +61,7 @@ export class Locator implements ILocator {
 
     public getWidestVisibleCellInColumn(columnIndex: number): number {
         const cellClasses = [
-            `.${Classes.columnIndexClass(columnIndex)}`,
+            `.${Classes.columnCellIndexClass(columnIndex)}`,
             `.${Classes.TABLE_COLUMN_NAME}`,
         ];
         const cells = this.tableElement.querySelectorAll(cellClasses.join(", "));