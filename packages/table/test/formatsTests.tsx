43888d873c2c0c9b1f5d392dc1b5deb9b71e6065
Geoff Cameron <gcameron@palantir.com>
2017-03-09 13:46:09 -0500

    Add test for horizontal truncation with truncated text

--- packages/table/test/formatsTests.tsx
+++ packages/table/test/formatsTests.tsx
@@ -51,6 +51,35 @@ describe("Formats", () => {
             expect(comp.find(`.${Classes.TABLE_TRUNCATED_POPOVER_TARGET}`).element).to.exist;
         });
 
+        it("can automatically truncate and show popover when truncated and word wrapped", () => {
+            const str = `
+                We are going to die, and that makes us the lucky ones. Most
+                people are never going to die because they are never going to
+                be born. The potential people who could have been here in my
+                place but who will in fact never see the light of day
+                outnumber the sand grains of Arabia. Certainly those unborn
+                ghosts include greater poets than Keats, scientists greater
+                than Newton. We know this because the set of possible people
+                allowed by our DNA so massively outnumbers the set of actual
+                people. In the teeth of these stupefying odds it is you and I,
+                in our ordinariness, that are here. We privileged few, who won
+                the lottery of birth against all odds, how dare we whine at
+                our inevitable return to that prior state from which the vast
+                majority have never stirred?
+            `;
+
+            const style = { height: "200px" };
+
+            const comp = harness.mount(
+                <div className={Classes.TABLE_TRUNCATED_TEXT} style={style}>
+                    <TruncatedFormat>{str}</TruncatedFormat>
+                </div>,
+            );
+            const textElement = comp.element.query(`.${Classes.TABLE_TRUNCATED_VALUE}`);
+            expect(textElement.scrollHeight).to.be.greaterThan(textElement.clientHeight);
+            expect(comp.find(`.${Classes.TABLE_TRUNCATED_POPOVER_TARGET}`).element).to.exist;
+        });
+
         it("can manually truncate and show popover when truncated", () => {
             const str = `
                 As Gregor Samsa awoke one morning from uneasy dreams he found
