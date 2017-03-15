4a49e344bc537108c3b8084a63267104da7c5d1c
Gilad Gray <ggray@palantir.com>
2017-03-14 19:16:00 -0700

    BaseExample requires id prop, adds data-example-id attribute
    IBaseExample interface re-used in docs example renderer.
    BaseExample is the .kss-example, reducing element count by 1!

--- packages/core/examples/index.ts
+++ packages/core/examples/index.ts
@@ -5,6 +5,8 @@
  * and https://github.com/palantir/blueprint/blob/master/PATENTS
  */
 
+export * from "./common/baseExample";
+
 export * from "./alertExample";
 export * from "./buttonsExample";
 export * from "./collapseExample";
