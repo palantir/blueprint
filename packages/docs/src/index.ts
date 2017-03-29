f29d78e09436c0aaa73673e13cd54a5908c39ca9
Bill Dwyer <bdwyer@palantir.com>
2017-03-29 12:27:04 -0700

    copyright headers

--- packages/docs/src/index.ts
+++ packages/docs/src/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ * Copyright 2017-present Palantir Technologies, Inc. All rights reserved.
  * Licensed under the BSD-3 License as modified (the “License”); you may obtain a copy
  * of the license at https://github.com/palantir/blueprint/blob/master/LICENSE
  * and https://github.com/palantir/blueprint/blob/master/PATENTS
@@ -10,5 +10,4 @@ import "dom4";
 export * from "./components/baseExample";
 export * from "./components/styleguide";
 export * from "./common";
-
 export * from "./tags";
--- packages/docs/src/tags/heading.tsx
+++ packages/docs/src/tags/heading.tsx
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2017-present Palantir Technologies, Inc. All rights reserved.
+ * Licensed under the BSD-3 License as modified (the “License”); you may obtain a copy
+ * of the license at https://github.com/palantir/blueprint/blob/master/LICENSE
+ * and https://github.com/palantir/blueprint/blob/master/PATENTS
+ */
+
 import { IHeadingTag } from "documentalist/dist/client";
 import * as React from "react";
 import { TagRenderer } from "./";
