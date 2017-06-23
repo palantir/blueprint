3d85cba1977582c50fe01b5cd9033de44516343c
Adi Dahiya <adi.dahiya14@gmail.com>
2017-06-23 18:35:09 -0400

    Fix wget url, --no-check-certificate

--- scripts/yarn.sh
+++ scripts/yarn.sh
@@ -7,7 +7,7 @@ INSTALL_DIR="/opt/yarn"
 sudo mkdir -p "${INSTALL_DIR}"
 sudo chown ubuntu "${INSTALL_DIR}"
 cd "${INSTALL_DIR}"
-wget https://registry.npmjs.org/yarn/yarn-${VERSION}.tar.gz
+wget https://registry.npmjs.org/yarn/-/yarn-${VERSION}.tar.gz --no-check-certificate
 tar zxf yarn-${VERSION}.tar.gz
 sudo ln -s "${INSTALL_DIR}/dist/bin/yarn" /usr/local/bin/yarn
 sudo ln -s "${INSTALL_DIR}/dist/bin/yarn.js" /usr/local/bin/yarn.js
