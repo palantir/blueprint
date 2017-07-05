807b375a11662f0d46420a623f6e357c4f1d76c6
Antoine Llorca <allorca@palantir.com>
2017-07-05 12:15:01 -0700

    Add ulRef prop

--- packages/labs/src/components/select/select.tsx
+++ packages/labs/src/components/select/select.tsx
@@ -18,6 +18,7 @@ import {
     InputGroup,
     IPopoverProps,
     Keys,
+    Menu,
     Popover,
     Position,
     Utils,
@@ -164,9 +165,9 @@ export class Select<T> extends AbstractComponent<ISelectProps<T>, ISelectState<T
                 </div>
                 <div onKeyDown={handleKeyDown} onKeyUp={handleKeyUp}>
                     {filterable ? input : undefined}
-                    <ul className={Classes.MENU} ref={listProps.itemsParentRef}>
+                    <Menu ulRef={listProps.itemsParentRef}>
                         {this.renderItems(listProps)}
-                    </ul>
+                    </Menu>
                 </div>
             </Popover>
         );
