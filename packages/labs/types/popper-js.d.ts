30e848710d27639d34a36d94d172f49be821e25d
Gilad Gray <ggray@palantir.com>
2017-07-19 11:51:31 -0700

    fix lint errors

--- packages/labs/types/popper-js.d.ts
+++ packages/labs/types/popper-js.d.ts
@@ -1,3 +1,6 @@
+// tslint:disable interface-name
+// tslint:disable max-line-length
+
 // Type definitions for popper.js 1.10
 // Project: https://github.com/FezVrasta/popper.js/
 // Definitions by: ggray <https://github.com/giladgray>, rhysd <https://rhysd.github.io>, joscha <https://github.com/joscha>, seckardt <https://github.com/seckardt>, marcfallows <https://github.com/marcfallows>
@@ -5,28 +8,28 @@
 // This file only declares the public portions of the API.
 // It should not define internal pieces such as utils or modifier details.
 
-declare module 'popper.js' {
-  export type Position = 'top' | 'right' | 'bottom' | 'left';
+declare module "popper.js" {
+  export type Position = "top" | "right" | "bottom" | "left";
 
-  export type Placement = 'auto-start'
-    | 'auto'
-    | 'auto-end'
-    | 'top-start'
-    | 'top'
-    | 'top-end'
-    | 'right-start'
-    | 'right'
-    | 'right-end'
-    | 'bottom-end'
-    | 'bottom'
-    | 'bottom-start'
-    | 'left-end'
-    | 'left'
-    | 'left-start';
+  export type Placement = "auto-start"
+    | "auto"
+    | "auto-end"
+    | "top-start"
+    | "top"
+    | "top-end"
+    | "right-start"
+    | "right"
+    | "right-end"
+    | "bottom-end"
+    | "bottom"
+    | "bottom-start"
+    | "left-end"
+    | "left"
+    | "left-start";
 
-  export type Boundary = 'scrollParent' | 'viewport' | 'window';
+  export type Boundary = "scrollParent" | "viewport" | "window";
 
-  export type ModifierFn = (data: PopperData, options: Object) => PopperData;
+  export type ModifierFn = (data: PopperData, options: object) => PopperData;
 
   export interface BaseModifier {
     order?: number;
@@ -49,14 +52,14 @@ declare module 'popper.js' {
       element?: string | Element,
     };
     flip?: BaseModifier & {
-      behavior?: 'flip' | 'clockwise' | 'counterclockwise' | Position[],
+      behavior?: "flip" | "clockwise" | "counterclockwise" | Position[],
       padding?: number,
       boundariesElement?: Boundary | Element,
     };
     inner?: BaseModifier;
     hide?: BaseModifier;
     applyStyle?: BaseModifier & {
-      onLoad?: Function,
+      onLoad?: () => void,
       gpuAcceleration?: boolean,
     };
     [name: string]: BaseModifier & Record<string, any>;
@@ -77,7 +80,7 @@ declare module 'popper.js' {
     hide: boolean;
     arrowElement: Element;
     styles: CSSStyleDeclaration;
-    boundaries: Object;
+    boundaries: object;
     offsets: {
       popper: Offset,
       reference: Offset,
@@ -104,16 +107,16 @@ declare module 'popper.js' {
   }
 
   export default class Popper {
-    static modifiers: (BaseModifier & { name: string })[];
-    static placements: Placement[];
-    static Defaults: PopperOptions;
+    public static modifiers: Array<BaseModifier & { name: string }>;
+    public static placements: Placement[];
+    public static Defaults: PopperOptions;
 
     constructor(reference: Element | ReferenceObject, popper: Element, options?: PopperOptions);
 
-    destroy(): void;
-    update(): void;
-    scheduleUpdate(): void;
-    enableEventListeners(): void;
-    disableEventListeners(): void;
+    public destroy(): void;
+    public update(): void;
+    public scheduleUpdate(): void;
+    public enableEventListeners(): void;
+    public disableEventListeners(): void;
   }
 }
