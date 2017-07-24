2a82bb022afe461a57278e38b454813e01ae888b
Gilad Gray <ggray@palantir.com>
2017-07-24 13:27:28 -0700

    Update stylelint dependencies
    stylelint@8! postcss-url@7 supports copying assets so we can remove
    postcss-copy-assets entirely. (Note: resource paths are now
    dist/resources, not dist/assets)

--- gulp/sass.js
+++ gulp/sass.js
@@ -7,7 +7,6 @@ module.exports = (blueprint, gulp, plugins) => {
     const autoprefixer = require("autoprefixer");
     const path = require("path");
     const packageImporter = require("node-sass-package-importer");
-    const postcssCopyAssets = require("postcss-copy-assets");
     const postcssImport = require("postcss-import");
     const postcssUrl = require("postcss-url");
     const COPYRIGHT_HEADER = require("./util/text").COPYRIGHT_HEADER;
@@ -65,24 +64,19 @@ module.exports = (blueprint, gulp, plugins) => {
             sassCompiler.on("error", plugins.sass.logError);
         }
 
-        const postcssOptions = {
-            map: { inline: false },
-            to : blueprint.destPath(project, "dist.css"),
-        };
         const postcssPlugins = project.sass === "bundle" ? [
             // inline all imports
             postcssImport(),
-            // rebase all urls due to inlining
-            postcssUrl({ url: "rebase" }),
-            // copy assets to dist folder, respecting rebase
-            postcssCopyAssets({
-                pathTransform: (newPath, origPath) => {
-                    return path.resolve(
-                        blueprint.destPath(project),
-                        "assets",
-                        path.basename(origPath)
-                    );
-                },
+            // copy all url() assets into dist/resources/...
+            postcssUrl({
+                assetsPath: "./assets",
+                basePath: [
+                    // search current package and core resources
+                    "../resources",
+                    "../../core/resources",
+                    // if new resources are added to packages, they'll need to be listed here
+                ],
+                url: "copy",
             }),
         ] : [];
         // always run autoprefixer
@@ -91,7 +85,9 @@ module.exports = (blueprint, gulp, plugins) => {
         return gulp.src(config.srcGlob(project, true))
             .pipe(plugins.sourcemaps.init())
             .pipe(sassCompiler)
-            .pipe(plugins.postcss(postcssPlugins, postcssOptions))
+            .pipe(plugins.postcss(postcssPlugins, {
+                to : blueprint.destPath(project, "dist.css"),
+            }))
             .pipe(plugins.stripCssComments({ preserve: /^\*/ }))
             .pipe(plugins.replace(/\n{3,}/g, "\n\n"))
             // see https://github.com/floridoo/vinyl-sourcemaps-apply/issues/11#issuecomment-231220574
