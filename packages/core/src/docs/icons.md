61e1cb8a71e47d5a6efe1a0e964ab8b73694bc56
Gilad Gray <ggray@palantir.com>
2017-08-08 18:26:07 -0700

    add test suite, fix others

--- packages/core/src/docs/icons.md
+++ packages/core/src/docs/icons.md
@@ -4,7 +4,7 @@ Blueprint provides over 300 UI icons in an icon font. They come in two sizes, 16
 be used anywhere text is used. It's easy to change their color or apply effects like text shadows
 via standard CSS properties.
 
-There are two ways of using Blueprint UI icons, described in more details in the
+There are two ways of using Blueprint UI icons, described in more detail in the
 [**Icon component documentation**](#core/components/icon):
 1. React component: `<Icon iconName="more" />`
 2. CSS classes: `<span className="pt-icon-standard pt-icon-more />`
---
+++ packages/core/test/icon/iconTests.tsx
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ * Licensed under the BSD-3 License as modified (the “License”); you may obtain a copy
+ * of the license at https://github.com/palantir/blueprint/blob/master/LICENSE
+ * and https://github.com/palantir/blueprint/blob/master/PATENTS
+ */
+
+import { assert } from "chai";
+import { shallow } from "enzyme";
+import * as React from "react";
+
+import { Classes, Icon, IconClasses, IIconProps, Intent } from "../../src/index";
+
+describe("<Icon>", () => {
+    it("iconSize=16 renders standard size", () => assertIconClass(
+        <Icon iconName="vertical-distribution" iconSize={16} />,
+        Classes.ICON_STANDARD,
+    ));
+
+    it("iconSize=20 renders large size", () => assertIconClass(
+        <Icon iconName="vertical-distribution" iconSize={20} />,
+        Classes.ICON_LARGE,
+    ));
+
+    it("iconSize=inherit renders auto-size", () => assertIconClass(
+        <Icon iconName="vertical-distribution" iconSize="inherit" />,
+        Classes.ICON,
+    ));
+
+    it("renders intent class", () => assertIconClass(
+        <Icon iconName="add" intent={Intent.DANGER} />,
+        Classes.INTENT_DANGER,
+    ));
+
+    it("renders iconName class", () => assertIconClass(
+        <Icon iconName="vertical-distribution" />,
+        IconClasses.VERTICAL_DISTRIBUTION,
+    ));
+
+    it("iconName=undefined renders nothing", () => {
+        const icon = shallow(<Icon iconName={undefined} />);
+        assert.isTrue(icon.isEmptyRender());
+    });
+
+    it("supports prefixed iconName with warning", () => {
+        const warnSpy = sinon.spy(console, "warn");
+        shallow(<Icon iconName="pt-icon-add-column-right" />);
+        assert.isTrue(warnSpy.calledOnce);
+        assert.match(warnSpy.args[0][0], /deprecated/);
+        warnSpy.restore();
+    });
+
+    /** Asserts that rendered icon has given className. */
+    function assertIconClass(icon: React.ReactElement<IIconProps>, className: string) {
+        const wrapper = shallow(icon);
+        assert.isTrue(wrapper.hasClass(className));
+        return wrapper;
+    }
+});
