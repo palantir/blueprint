bcce57aad93b6f52a3d868dcca66d4e55f47b237
Francisco Fabián González <franciscofabian@me.com>
2017-08-23 23:50:16 +0200

    change var to const

--- packages/core/src/components/tabs2/tabs2.tsx
+++ packages/core/src/components/tabs2/tabs2.tsx
@@ -154,8 +154,8 @@ export class Tabs2 extends AbstractComponent<ITabs2Props, ITabs2State> {
         if (this.state.selectedTabId !== prevState.selectedTabId) {
             this.moveSelectionIndicator();
         } else if (prevState.selectedTabId !== undefined) {
-            var selectedTabIdIndex = this.props.children.map(child => child.props.id).indexOf(this.state.selectedTabId);
-            var prevSelectedTabIdIndex = _prevProps.children.map(child => child.props.id).indexOf(prevState.selectedTabId);
+            const selectedTabIdIndex = this.props.children.map(child => child.props.id).indexOf(this.state.selectedTabId);
+            const prevSelectedTabIdIndex = _prevProps.children.map(child => child.props.id).indexOf(prevState.selectedTabId);
             if (selectedTabIdIndex !== prevSelectedTabIdIndex) {
                 this.moveSelectionIndicator();
             }
