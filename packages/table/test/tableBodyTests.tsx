8d2e2125954364a93af6b05e2cb78df510c1bdc8
Chris Lewis <clewis@palantir.com>
2017-08-25 17:27:54 -0700

    Write tests

--- packages/table/test/tableBodyTests.tsx
+++ packages/table/test/tableBodyTests.tsx
@@ -6,7 +6,15 @@
  */
 
 import { expect } from "chai";
+import { mount } from "enzyme";
+import * as React from "react";
+
+import { Cell } from "../src/cell/cell";
+import { Batcher } from "../src/common/batcher";
 import * as Classes from "../src/common/classes";
+import { Grid } from "../src/common/grid";
+import { Rect } from "../src/common/rect";
+import { RenderOptimizationMode } from "../src/common/renderOptimizationMode";
 import { TableBody } from "../src/tableBody";
 
 describe("TableBody", () => {
@@ -22,4 +30,69 @@ describe("TableBody", () => {
         ]);
     });
 
+    describe("renderOptimizationMode", () => {
+        // use enough rows that batching won't render all of them in one pass.
+        // and careful: if this value is too big (~100), the batcher's reliance
+        // on `requestIdleCallback` may cause the tests to run multiple times.
+        const LARGE_NUM_ROWS = Batcher.DEFAULT_ADD_LIMIT * 2;
+        const NUM_COLUMNS = 1;
+
+        const COLUMN_WIDTH = 100;
+        const ROW_HEIGHT = 20;
+
+        it("renders all cells immediately if renderOptimizationMode === RenderOptimizationMode.NONE", () => {
+            const tableBody = mountTableBody(RenderOptimizationMode.NONE);
+
+            // expect all cells to have rendered in one pass
+            expect(tableBody.find(Cell).length).to.equal(LARGE_NUM_ROWS);
+        });
+
+        it("uses batch rendering if renderOptimizationMode === RenderOptimizationMode.BATCH", () => {
+            const tableBody = mountTableBody(RenderOptimizationMode.BATCH);
+
+            // run this assertion immediately, expecting that the batching hasn't finished yet.
+            expect(tableBody.find(Cell).length).to.equal(Batcher.DEFAULT_ADD_LIMIT);
+        });
+
+        function mountTableBody(renderOptimizationMode: RenderOptimizationMode) {
+            const rowHeights = Array(LARGE_NUM_ROWS).fill(ROW_HEIGHT);
+            const columnWidths = Array(NUM_COLUMNS).fill(COLUMN_WIDTH);
+
+            const grid = new Grid(rowHeights, columnWidths);
+            const viewportRect = new Rect(0, 0, NUM_COLUMNS * COLUMN_WIDTH, LARGE_NUM_ROWS * ROW_HEIGHT);
+
+            return mount(
+                <TableBody
+                    cellRenderer={renderCell}
+                    grid={grid}
+                    loading={false}
+                    locator={null}
+                    renderOptimizationMode={renderOptimizationMode}
+                    viewportRect={viewportRect}
+
+                    // ISelectableProps
+                    allowMultipleSelection={true}
+                    onFocus={noop}
+                    onSelection={noop}
+                    selectedRegions={[]}
+
+                    // IRowIndices
+                    rowIndexStart={0}
+                    rowIndexEnd={LARGE_NUM_ROWS - 1}
+
+                    // IColumnIndices
+                    columnIndexStart={0}
+                    columnIndexEnd={NUM_COLUMNS - 1}
+                />,
+            );
+        }
+
+        function renderCell() {
+            return <Cell>gg</Cell>;
+        }
+
+        function noop() {
+            return;
+        }
+    });
 });
