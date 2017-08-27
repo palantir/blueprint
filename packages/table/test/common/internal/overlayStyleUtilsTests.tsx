086d3a543ebad4bfd7dbbc5dfd74c4357338e62b
Chris Lewis <clewis@palantir.com>
2017-08-27 14:05:18 -0700

    Add test stubs for overlayStyleUtils.ts

---
+++ packages/table/test/common/internal/overlayStyleUtilsTests.tsx
@@ -0,0 +1,216 @@
+/**
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ * Licensed under the BSD-3 License as modified (the “License”); you may obtain a copy
+ * of the license at https://github.com/palantir/blueprint/blob/master/LICENSE
+ * and https://github.com/palantir/blueprint/blob/master/PATENTS
+ */
+
+import { expect } from "chai";
+import * as OverlayStyleUtils from "../../../src/common/internal/overlayStyleUtils";
+
+describe("OverlayStyleUtils", () => {
+
+    describe("getBodyOverlayStyle", () => {
+
+        describe("MAIN quadrant", () => {
+
+            describe("CELLS region", () => {
+                it("within quadrant", () => {
+                    // TODO
+                });
+                it("at quadrant boundary", () => {
+                    // TODO
+                });
+                it("beyond boundary", () => {
+                    // TODO
+                });
+            });
+
+            describe("FULL_ROWS region", () => {
+                it("within quadrant", () => {
+                    // TODO
+                });
+                it("at quadrant boundary", () => {
+                    // TODO
+                });
+                it("beyond boundary", () => {
+                    // TODO
+                });
+            });
+
+            describe("FULL_COLUMNS region", () => {
+                it("within quadrant", () => {
+                    // TODO
+                });
+                it("at quadrant boundary", () => {
+                    // TODO
+                });
+                it("beyond boundary", () => {
+                    // TODO
+                });
+            });
+
+            describe("FULL_TABLE region", () => {
+                it("within quadrant", () => {
+                    // TODO
+                });
+                it("at quadrant boundary", () => {
+                    // TODO
+                });
+                it("beyond boundary", () => {
+                    // TODO
+                });
+            });
+        });
+
+        describe("LEFT quadrant", () => {
+            describe("CELLS region", () => {
+                it("within quadrant", () => {
+                    // TODO
+                });
+                it("at quadrant boundary", () => {
+                    // TODO
+                });
+                it("beyond boundary", () => {
+                    // TODO
+                });
+            });
+
+            describe("FULL_ROWS region", () => {
+                it("within quadrant", () => {
+                    // TODO
+                });
+                it("at quadrant boundary", () => {
+                    // TODO
+                });
+                it("beyond boundary", () => {
+                    // TODO
+                });
+            });
+
+            describe("FULL_COLUMNS region", () => {
+                it("within quadrant", () => {
+                    // TODO
+                });
+                it("at quadrant boundary", () => {
+                    // TODO
+                });
+                it("beyond boundary", () => {
+                    // TODO
+                });
+            });
+
+            describe("FULL_TABLE region", () => {
+                it("within quadrant", () => {
+                    // TODO
+                });
+                it("at quadrant boundary", () => {
+                    // TODO
+                });
+                it("beyond boundary", () => {
+                    // TODO
+                });
+            });
+        });
+
+        describe("TOP quadrant", () => {
+            describe("CELLS region", () => {
+                it("within quadrant", () => {
+                    // TODO
+                });
+                it("at quadrant boundary", () => {
+                    // TODO
+                });
+                it("beyond boundary", () => {
+                    // TODO
+                });
+            });
+
+            describe("FULL_ROWS region", () => {
+                it("within quadrant", () => {
+                    // TODO
+                });
+                it("at quadrant boundary", () => {
+                    // TODO
+                });
+                it("beyond boundary", () => {
+                    // TODO
+                });
+            });
+
+            describe("FULL_COLUMNS region", () => {
+                it("within quadrant", () => {
+                    // TODO
+                });
+                it("at quadrant boundary", () => {
+                    // TODO
+                });
+                it("beyond boundary", () => {
+                    // TODO
+                });
+            });
+
+            describe("FULL_TABLE region", () => {
+                it("within quadrant", () => {
+                    // TODO
+                });
+                it("at quadrant boundary", () => {
+                    // TODO
+                });
+                it("beyond boundary", () => {
+                    // TODO
+                });
+            });
+        });
+
+        describe("TOP_LEFT quadrant", () => {
+            describe("CELLS region", () => {
+                it("within quadrant", () => {
+                    // TODO
+                });
+                it("at quadrant boundary", () => {
+                    // TODO
+                });
+                it("beyond boundary", () => {
+                    // TODO
+                });
+            });
+
+            describe("FULL_ROWS region", () => {
+                it("within quadrant", () => {
+                    // TODO
+                });
+                it("at quadrant boundary", () => {
+                    // TODO
+                });
+                it("beyond boundary", () => {
+                    // TODO
+                });
+            });
+
+            describe("FULL_COLUMNS region", () => {
+                it("within quadrant", () => {
+                    // TODO
+                });
+                it("at quadrant boundary", () => {
+                    // TODO
+                });
+                it("beyond boundary", () => {
+                    // TODO
+                });
+            });
+
+            describe("FULL_TABLE region", () => {
+                it("within quadrant", () => {
+                    // TODO
+                });
+                it("at quadrant boundary", () => {
+                    // TODO
+                });
+                it("beyond boundary", () => {
+                    // TODO
+                });
+            });
+        });
+    });
+});
