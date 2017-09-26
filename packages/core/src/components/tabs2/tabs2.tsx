7f81d618604f76f601b3375325f3d2e37ef65ec6
Francisco Fabián González <franciscofabian@me.com>
2017-09-27 00:44:56 +0200

    use React.children to handle children props

--- packages/core/src/components/tabs2/tabs2.tsx
+++ packages/core/src/components/tabs2/tabs2.tsx
@@ -167,7 +167,7 @@ export class Tabs2 extends AbstractComponent<ITabs2Props, ITabs2State> {
         if (this.state.selectedTabId !== prevState.selectedTabId) {
             this.moveSelectionIndicator();
         } else if (prevState.selectedTabId != null) {
-            if (!Utils.shallowCompareKeys(prevProps.children, this.props.children)) {
+            if (!Utils.arraysEqual(prevProps.children, React.Children.map(this.props.children, thisArg => thisArg), Utils.shallowCompareKeys) {
                 this.moveSelectionIndicator();
             }
         }
