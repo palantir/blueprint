5ed0e04476c7da917466aff6dadfd537fd434801
Adi Dahiya <adi.dahiya14@gmail.com>
2017-09-26 09:31:30 -0400

    fix datetime lint errors

--- packages/datetime/test/common/dateTestUtils.ts
+++ packages/datetime/test/common/dateTestUtils.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ * Licensed under the BSD-3 License as modified (the “License”); you may obtain a copy
+ * of the license at https://github.com/palantir/blueprint/blob/master/LICENSE
+ * and https://github.com/palantir/blueprint/blob/master/PATENTS
+ */
+
 import { assert } from "chai";
 import { padWithZeroes } from "../../src/common/utils";
 
