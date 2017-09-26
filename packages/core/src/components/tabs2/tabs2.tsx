da45dece5d810e135753406ff6593861b7c178e1
Francisco Fabián González <franciscofabian@me.com>
2017-09-27 00:46:54 +0200

    fix props children

--- packages/core/src/components/tabs2/tabs2.tsx
+++ packages/core/src/components/tabs2/tabs2.tsx
@@ -167,7 +167,7 @@ export class Tabs2 extends AbstractComponent<ITabs2Props, ITabs2State> {
         if (this.state.selectedTabId !== prevState.selectedTabId) {
             this.moveSelectionIndicator();
         } else if (prevState.selectedTabId != null) {
-            if (!Utils.arraysEqual(prevProps.children, React.Children.map(this.props.children, thisArg => thisArg), Utils.shallowCompareKeys) {
+            if (!Utils.arraysEqual(prevProps.children, React.Children.map(this.props.children, thisArg => {return thisArg}), Utils.shallowCompareKeys) {
                 this.moveSelectionIndicator();
             }
         }
