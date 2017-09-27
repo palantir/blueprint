fff9df3efd502bb6646405fa9ce2e0109f10285d
Chris Lewis <clewis@palantir.com>
2017-09-26 19:40:30 -0700

    Add more explanation in the @deprecated comment

--- packages/table/src/headers/columnHeaderCell.tsx
+++ packages/table/src/headers/columnHeaderCell.tsx
@@ -45,7 +45,8 @@ export interface IColumnNameProps {
      *
      * @default false
      * @deprecated since @blueprintjs/table v1.27.0; pass this prop to `Table`
-     * instead.
+     * instead. (If you don't, the `Table` will not be able to properly resize
+     * the column header when `useInteractionBar` is toggled on and off.)
      */
     useInteractionBar?: boolean;
 }
