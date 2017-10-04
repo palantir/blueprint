41b78df8982e443cf3005881d7d1d80657f0e9d6
Chris Lewis <cmslewis@gmail.com>
2017-10-04 12:37:46 -0700

    [Table] Fix TS errors in features.tsx (#1652)
    * Fix TS errors in features.tsx
    
    * Improve comment

--- packages/table/preview/features.tsx
+++ packages/table/preview/features.tsx
@@ -264,13 +264,16 @@ class EditableTable extends React.Component<{}, IEditableTableState> {
     private setArrayState<T>(key: string, index: number, value: T) {
         const values = (this.state as any)[key].slice() as T[];
         values[index] = value;
-        this.setState({ [key]: values });
+        // TS doesn't know how to type-check parameterized string-literal keys,
+        // so we have to cast to `any`. TS issue:
+        // https://github.com/Microsoft/TypeScript/issues/15534
+        this.setState({ [key]: values } as any);
     }
 
     private setSparseState<T>(stateKey: string, dataKey: string, value: T) {
         const stateData = (this.state as any)[stateKey] as { [key: string]: T };
         const values = { ...stateData, [dataKey]: value };
-        this.setState({ [stateKey]: values });
+        this.setState({ [stateKey]: values } as any);
     }
 }
 
