8ca52597e8df763e7ff0653f1d9e2e658a2f8ee1
Geoff Cameron <gscshoyru@gmail.com>
2017-10-06 18:20:36 -0400

    [Table] Add toggle for custom column headers to preview (#1671)
    * Add toggle for custom column headers to preview
    
    * sentence casing fixes
    
    * Make toggles dependent on each other

--- packages/table/preview/mutableTable.tsx
+++ packages/table/preview/mutableTable.tsx
@@ -607,8 +607,9 @@ export class MutableTable extends React.Component<{}, IMutableTableState> {
                 {this.renderNumberSelectMenu("Num. frozen columns", "numFrozenCols", FROZEN_COLUMN_COUNTS)}
                 {this.renderSwitch("Loading state", "showColumnHeadersLoading")}
                 {this.renderSwitch("Menus", "showColumnMenus")}
+                {this.renderSwitch("Custom headers", "enableColumnCustomHeaders")}
                 <h6>Interactions</h6>
-                {this.renderSwitch("Editing", "enableColumnNameEditing")}
+                {this.renderSwitch("Editing", "enableColumnNameEditing", "enableColumnCustomHeaders", false)}
                 {this.renderSwitch("Reordering", "enableColumnReordering")}
                 {this.renderSwitch("Resizing", "enableColumnResizing")}
                 {this.renderSwitch("Selection", "enableColumnSelection")}
@@ -636,9 +637,9 @@ export class MutableTable extends React.Component<{}, IMutableTableState> {
                 <h6>Text Layout</h6>
                 {this.renderSwitch("Truncation", "enableCellTruncation", "enableCellEditing", false)}
                 <div className="sidebar-indented-group">{truncatedPopoverModeMenu}</div>
-                {this.renderSwitch("Fixed Truncation", "enableCellTruncationFixed", "enableCellTruncation", true)}
+                {this.renderSwitch("Fixed truncation", "enableCellTruncationFixed", "enableCellTruncation", true)}
                 <div className="sidebar-indented-group">{truncatedLengthMenu}</div>
-                {this.renderSwitch("Wrap Text", "enableCellWrap")}
+                {this.renderSwitch("Wrap text", "enableCellWrap")}
 
                 <h4>Page</h4>
                 <h6>Display</h6>
@@ -657,11 +658,11 @@ export class MutableTable extends React.Component<{}, IMutableTableState> {
         return (
             <Button
                 intent={Intent.PRIMARY}
-                iconName={"undo"}
+                iconName="undo"
                 className={classNames(Classes.FILL)}
                 onClick={this.handleDefaultsButtonClick}
             >
-                Reset All
+                Reset all
             </Button>
         );
     }
@@ -986,6 +987,10 @@ export class MutableTable extends React.Component<{}, IMutableTableState> {
     private syncDependentBooleanStates = () => {
         if (this.state.enableCellEditing && this.state.enableCellTruncation) {
             this.setState({ enableCellTruncation: false });
+        }
+
+        if (this.state.enableColumnNameEditing && this.state.enableColumnCustomHeaders) {
+            this.setState({ enableColumnNameEditing: false });
         }
     };
 
