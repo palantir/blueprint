310cb209caccb1bb894002aaaec46c5928b6beef
Chris Lewis <clewis@palantir.com>
2017-10-10 16:13:49 -0700

    Write tests for PlatformUtils

--- packages/table/src/common/internal/platformUtils.ts
+++ packages/table/src/common/internal/platformUtils.ts
@@ -9,8 +9,9 @@
  * Returns `true` if `navigator.platform` matches a known Mac platform, or
  * `false` otherwise.
  */
-export function isMac() {
-    const platform = typeof navigator !== "undefined" ? navigator.platform : undefined;
+export function isMac(platformOverride?: string) {
+    const platformActual = typeof navigator !== "undefined" ? navigator.platform : undefined;
+    const platform = platformOverride != null ? platformOverride : platformActual;
     return platform == null ? false : /Mac|iPod|iPhone|iPad/.test(platform);
 }
 
@@ -18,7 +19,7 @@ export function isMac() {
  * Returns `true` if (1) the platform is Mac and the keypress includes the `cmd`
  * key, or (2) the platform is non-Mac and the keypress includes the `ctrl` key.
  */
-export const isModKeyPressed = (event: KeyboardEvent) => {
-    const isMacPlatform = isMac();
+export const isModKeyPressed = (event: KeyboardEvent, platformOverride?: string) => {
+    const isMacPlatform = isMac(platformOverride);
     return (isMacPlatform && event.metaKey) || (!isMacPlatform && event.ctrlKey);
 };
