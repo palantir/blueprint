d6845b2a2fad45b66998867795cf154e8256cfb0
Chris Lewis <clewis@palantir.com>
2017-10-12 00:42:27 -0700

    Code cleanup in movementDelta.ts

--- packages/table/src/common/movementDelta.ts
+++ packages/table/src/common/movementDelta.ts
@@ -6,13 +6,9 @@
  */
 
 export interface IMovementDelta {
-    /**
-     * The number of rows by which to move.
-     */
+    /** The number of rows by which to move. */
     rows: number;
 
-    /**
-     * The number of columns by which to move.
-     */
+    /** The number of columns by which to move. */
     cols: number;
 }
