25e28d56324176a757804255bb0d79c7e23ddd3b
Adi Dahiya <adahiya@palantir.com>
2017-11-07 22:42:06 +0000

    site-docs webpack dev server kinda works

--- packages/site-landing/webpack.config.js
+++ packages/site-landing/webpack.config.js
@@ -1,65 +1,38 @@
-"use strict";
+/**
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ * Licensed under the terms of the LICENSE file distributed with this project.
+ */
 
+const baseConfig = require("@blueprintjs/webpack-build-scripts/webpack.config.base");
 const path = require("path");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const SRC = path.resolve("./src");
-const DEST = path.resolve("./dist");
-const NAME = "blueprint-landing.js";
-
-module.exports = {
-    entry: [
-        `${SRC}/index.tsx`,
-    ],
-
-    module: {
-        rules: [
-            {
-                include: SRC,
-                test: /\.tsx?$/,
-                use: [{
-                    loader: "ts-loader",
-                    options: {
-                        compilerOptions: { declaration: false },
-                    },
-                }],
-            }, {
-                include: SRC,
-                loader: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: ["css-loader", "postcss-loader", "sass-loader"],
-                }),
-                test: /\.s[ac]ss$/,
-            }, {
-                test: /\.(eot|ttf|woff|woff2)$/,
-                use: [{
-                    loader: "file-loader",
-                    options: {
-                        name: "assets/fonts/[name].[ext]"
-                    }
-                }],
-            }, {
-                test: /resources\/img\/.*\.(svg|png)$/,
-                use: [{
-                    loader: "file-loader",
-                    options: {
-                        name: "assets/img/[name].[ext]"
-                    }
-                }],
-            },
+module.exports = Object.assign({}, baseConfig, {
+    entry: {
+        "blueprint-landing": [
+            "./src/index.tsx",
         ],
     },
 
     output: {
-        filename: NAME,
-        path: DEST,
+        filename: "[name].js",
+        path: path.resolve(__dirname, "./dist"),
+        publicPath: "/",
     },
 
-    plugins: [
-        new ExtractTextPlugin(NAME.replace(/\.js$/, ".css")),
-    ],
-
-    resolve: {
-        extensions: [".js", ".ts", ".tsx"],
-    },
-};
+    devServer: {
+        contentBase: "./",
+        disableHostCheck: true,
+        historyApiFallback: true,
+        https: false,
+        // hot: true,
+        index: path.resolve(__dirname, "index.html"),
+        inline: true,
+        stats: "errors-only",
+        open: true,
+        overlay: {
+            warnings: true,
+            errors: true,
+        },
+        port: 9000,
+    }
+});
