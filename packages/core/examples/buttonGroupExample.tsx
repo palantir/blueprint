d66fd9dabde57bef0aa6db346fa1256d381a9bd5
Chris Lewis <clewis@palantir.com>
2017-11-11 01:02:33 -0800

    ButtonGroup component

---
+++ packages/core/examples/buttonGroupExample.tsx
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the terms of the LICENSE file distributed with this project.
+ */
+
+import * as React from "react";
+
+import { AnchorButton, Button, ButtonGroup, Classes, Icon, Switch } from "@blueprintjs/core";
+import { BaseExample, handleBooleanChange } from "@blueprintjs/docs";
+
+export interface IButtonGroupExampleState {
+    fill?: boolean;
+    minimal?: boolean;
+    large?: boolean;
+    vertical?: boolean;
+}
+
+export class ButtonGroupExample extends BaseExample<IButtonGroupExampleState> {
+    public state: IButtonGroupExampleState = {
+        fill: false,
+        large: false,
+        minimal: false,
+        vertical: false,
+    };
+
+    private handleFillChange = handleBooleanChange(fill => this.setState({ fill }));
+    private handleLargeChange = handleBooleanChange(large => this.setState({ large }));
+    private handleMinimalChange = handleBooleanChange(minimal => this.setState({ minimal }));
+    private handleVerticalChange = handleBooleanChange(vertical => this.setState({ vertical }));
+
+    protected renderExample() {
+        // have the container take up the full-width if `fill` is true;
+        // otherwise, disable full-width styles to keep a vertical button group
+        // from taking up the full width.
+        const style: React.CSSProperties = { flexGrow: this.state.fill ? 1 : undefined };
+        return (
+            <ButtonGroup style={style} {...this.state}>
+                <Button iconName="database">Queries</Button>
+                <Button iconName="function">Functions</Button>
+                <AnchorButton iconName="function">
+                    Options
+                    <Icon iconName="caret-down" className={Classes.ALIGN_RIGHT} />
+                </AnchorButton>
+            </ButtonGroup>
+        );
+    }
+
+    protected renderOptions() {
+        return [
+            [
+                <Switch checked={this.state.fill} key="fill" label="Fill" onChange={this.handleFillChange} />,
+                <Switch checked={this.state.large} key="large" label="Large" onChange={this.handleLargeChange} />,
+                <Switch
+                    checked={this.state.minimal}
+                    key="minimal"
+                    label="Minimal"
+                    onChange={this.handleMinimalChange}
+                />,
+                <Switch
+                    checked={this.state.vertical}
+                    key="vertical"
+                    label="Vertical"
+                    onChange={this.handleVerticalChange}
+                />,
+            ],
+        ];
+    }
+}
---
+++ packages/core/src/components/button/buttonGroup.tsx
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the terms of the LICENSE file distributed with this project.
+ */
+
+import * as classNames from "classnames";
+import * as PureRender from "pure-render-decorator";
+import * as React from "react";
+import * as Classes from "../../common/classes";
+import { IProps } from "../../common/props";
+
+export interface IButtonGroupProps extends IProps {
+    /**
+     * Whether the child buttons should appear with full-width styling.
+     * @default false
+     */
+    fill?: boolean;
+
+    /**
+     * Whether the child buttons should appear with minimal styling.
+     * @default false
+     */
+    minimal?: boolean;
+
+    /**
+     * Whether the child buttons should appear with large styling.
+     * @default false
+     */
+    large?: boolean;
+
+    /**
+     * CSS styles to apply to the alert.
+     */
+    style?: React.CSSProperties;
+
+    /**
+     * Whether the child buttons should appear with vertical styling.
+     * @default false
+     */
+    vertical?: boolean;
+}
+
+@PureRender
+export class ButtonGroup extends React.Component<IButtonGroupProps, {}> {
+    public static displayName = "Blueprint.ButtonGroup";
+
+    public render() {
+        const className = classNames(
+            Classes.BUTTON_GROUP,
+            {
+                [Classes.FILL]: this.props.fill,
+                [Classes.MINIMAL]: this.props.minimal,
+                [Classes.LARGE]: this.props.large,
+                [Classes.VERTICAL]: this.props.vertical,
+            },
+            this.props.className,
+        );
+        return (
+            <div className={className} style={this.props.style}>
+                {this.props.children}
+            </div>
+        );
+    }
+}
