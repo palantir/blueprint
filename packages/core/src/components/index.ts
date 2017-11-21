5801301d147d7b4dc2129771fe3b9d23a15a123d
Chris Lewis <clewis@palantir.com>
2017-11-20 23:18:33 -0800

    Add ControlGroup component

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -25,6 +25,7 @@ export * from "./collapsible-list/collapsibleList";
 export * from "./context-menu/contextMenuTarget";
 export * from "./dialog/dialog";
 export * from "./editable-text/editableText";
+export * from "./forms/controlGroup";
 export * from "./forms/controls";
 export * from "./forms/formGroup";
 export * from "./forms/inputGroup";
