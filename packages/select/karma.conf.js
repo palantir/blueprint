9466ec1ebe54ef91e013466489ecef53867c599a
Xiaohan Zhang <xzhang@palantir.com>
2017-12-11 15:40:43 -0800

    commit top level package files

---
+++ packages/select/karma.conf.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
+ */
+
+const { createKarmaConfig } = require("@blueprintjs/karma-build-scripts");
+const path = require("path");
+
+module.exports = function (config) {
+    const baseConfig = createKarmaConfig({
+        dirname: __dirname,
+    });
+    config.set(baseConfig);
+    config.set({
+        webpack: Object.assign({}, baseConfig.webpack, {
+            entry: {
+                core: path.resolve(__dirname, "test/index.ts"),
+            },
+        }),
+    })
+};
