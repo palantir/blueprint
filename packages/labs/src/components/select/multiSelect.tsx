85d94add0a14c5e33c5977241fac5b0d4bee5793
Chris Lewis <clewis@palantir.com>
2017-12-11 12:27:24 -0800

    Fix reference to TagInput in MultiSelect

--- packages/labs/src/components/select/multiSelect.tsx
+++ packages/labs/src/components/select/multiSelect.tsx
@@ -7,11 +7,10 @@ import * as classNames from "classnames";
 import * as PureRender from "pure-render-decorator";
 import * as React from "react";
 
-import { HTMLInputProps, IPopoverProps, Keys, Menu, Popover, Position, Utils } from "@blueprintjs/core";
+import { HTMLInputProps, IPopoverProps, ITagInputProps, Keys, Menu, Popover, Position, TagInput, Utils } from "@blueprintjs/core";
 import * as Classes from "../../common/classes";
 import { IListItemsProps, IQueryListRendererProps, QueryList } from "../query-list/queryList";
 import { ISelectItemRendererProps } from "../select/select";
-import { ITagInputProps, TagInput } from "../tag-input/tagInput";
 
 export interface IMultiSelectProps<T> extends IListItemsProps<T> {
     /** Controlled selected values. */
