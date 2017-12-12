ba90e417dda431478a2fd42c04167bf80cdd7b5e
Xiaohan Zhang <xzhang@palantir.com>
2017-12-11 16:15:18 -0800

    move tests from labs to select

--- packages/labs/test/index.ts
+++ packages/labs/test/index.ts
@@ -2,10 +2,6 @@
  * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
  */
 
-import "./multiSelectTests";
 import "./popover2Tests";
-import "./queryListTests";
-import "./selectTests";
-import "./suggestTests";
 import "./tagInputTests";
 import "./timezonePickerTests";
