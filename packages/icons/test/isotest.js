85e2e7700213f028726a2f4d7e8c822b3c87b553
Adi Dahiya <adi.dahiya14@gmail.com>
2017-12-15 08:25:32 -0800

    Fix inter-package dependencies, simplify test job config (#1932)

--- packages/icons/test/isotest.js
+++ packages/icons/test/isotest.js
@@ -3,7 +3,7 @@
  * Licensed under the terms of the LICENSE file distributed with this project.
  */
 
-const { generateIsomorphicTests } = require("@blueprintjs/node-build-scripts");
+const { generateIsomorphicTests } = require("@blueprintjs/test-commons");
 const React = require("react");
 const Icons = require("../dist");
 
