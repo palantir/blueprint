33baa2eb0b526380485798fdf6c47c40820dbcfb
Kent <kblake@palantir.com>
2017-12-20 13:51:52 -0800

    Adding className consistency test

--- packages/test-commons/src/generateIsomorphicTests.ts
+++ packages/test-commons/src/generateIsomorphicTests.ts
@@ -6,6 +6,7 @@
 // TODO: make this an executable script that takes configuration from CLI arguments so we don't need
 // to use the `mocha` CLI and write an isotest.js file in every project
 
+import { assert } from "chai";
 import * as Enzyme from "enzyme";
 import * as Adapter from "enzyme-adapter-react-16";
 import * as React from "react";
@@ -31,20 +32,21 @@ function isReactClass(Component: any): boolean {
  * @param Components  main export from package
  * @param props custom props per component
  * @param children custom children per component
- * @param skipList array of component names to skip
+ * @param renderSkipList array of component names to skip
  */
 export function generateIsomorphicTests(
     Components: { [name: string]: any },
     props: { [name: string]: any },
     children: { [name: string]: React.ReactNode },
-    skipList: string[] = [],
+    renderSkipList: string[] = [],
+    classNameSkipList: string[] = ["Alert", "Dialog", "MenuItem", "Popover2", "Portal", "Toaster", "Tooltip2"],
 ) {
     Object.keys(Components)
         .sort()
         .forEach(componentName => {
             const Component = Components[componentName];
             if (isReactClass(Component)) {
-                if (skipList.includes(componentName)) {
+                if (renderSkipList.includes(componentName)) {
                     it.skip(`<${componentName}>`);
                 } else {
                     it(`<${componentName}>`, () => {
@@ -53,6 +55,24 @@ export function generateIsomorphicTests(
                         // we care merely that `render()` succeeds: it can be server-rendered.
                         // errors will fail the test and log full stack traces to the console. nifty!
                         Enzyme.render(element);
+                    });
+                }
+                if (classNameSkipList.includes(componentName)) {
+                    it.skip(`<${componentName}>`);
+                } else {
+                    it(`<${componentName}>  className is applied to outer element`, () => {
+                        const element = React.createElement(
+                            Component,
+                            {
+                                ...props[componentName],
+                                className: "test",
+                                iconName: "pt-icon-build",
+                                inline: true,
+                                lazy: false,
+                            } as any,
+                            children[componentName],
+                        );
+                        assert.isTrue(Enzyme.render(element).hasClass("test"));
                     });
                 }
             }
