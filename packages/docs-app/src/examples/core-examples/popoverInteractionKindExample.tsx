f144675e2779794ced403315a9ae2b2f46927bbf
Chris Lewis <clewis@palantir.com>
2017-12-22 17:08:16 -0800

    Derp, just use enforceFocus={false} in the PopoverInteractionKindExample too

--- packages/docs-app/src/examples/core-examples/popoverInteractionKindExample.tsx
+++ packages/docs-app/src/examples/core-examples/popoverInteractionKindExample.tsx
@@ -6,26 +6,12 @@
 
 import * as React from "react";
 
-import {
-    Button,
-    IButtonProps,
-    Intent,
-    IPopoverProps,
-    Popover,
-    PopoverInteractionKind,
-    Position,
-    Toaster,
-} from "@blueprintjs/core";
+import { Button, Intent, Popover, PopoverInteractionKind, Position } from "@blueprintjs/core";
 import { BaseExample } from "@blueprintjs/docs";
 import { FileMenu } from "./common/fileMenu";
 
 export class PopoverInteractionKindExample extends BaseExample<{}> {
     protected className = "docs-popover-interaction-kind-example";
-
-    private toaster: Toaster;
-    private refHandlers = {
-        toaster: (ref: Toaster) => (this.toaster = ref),
-    };
 
     protected renderExample() {
         return (
@@ -33,55 +19,24 @@ export class PopoverInteractionKindExample extends BaseExample<{}> {
                 {this.renderPopover("HOVER", PopoverInteractionKind.HOVER)}
                 {this.renderPopover("HOVER_TARGET_ONLY", PopoverInteractionKind.HOVER_TARGET_ONLY)}
                 {this.renderPopover("CLICK", PopoverInteractionKind.CLICK)}
-                {this.renderPopover(
-                    "CLICK_TARGET_ONLY",
-                    PopoverInteractionKind.CLICK_TARGET_ONLY,
-                    { onClick: this.handleClickTargetOnlyButtonClick },
-                    { popoverWillClose: this.handleClickTargetOnlyPopoverWillClose },
-                )}
-                <Toaster {...this.state} ref={this.refHandlers.toaster} />
+                {this.renderPopover("CLICK_TARGET_ONLY", PopoverInteractionKind.CLICK_TARGET_ONLY)}
             </div>
         );
     }
 
-    private renderPopover(
-        buttonLabel: string,
-        interactionKind: PopoverInteractionKind,
-        buttonProps?: IButtonProps,
-        popoverProps?: IPopoverProps,
-    ) {
+    private renderPopover(buttonLabel: string, interactionKind: PopoverInteractionKind) {
         // MenuItem's default shouldDismissPopover={true} behavior is confusing
         // in this example, since it introduces an additional way popovers can
         // close. set it to false here for clarity.
         return (
             <Popover
-                {...popoverProps}
                 content={<FileMenu shouldDismissPopover={false} />}
+                enforceFocus={false}
                 position={Position.BOTTOM_LEFT}
                 interactionKind={interactionKind}
             >
-                <Button intent={Intent.PRIMARY} {...buttonProps}>
-                    {buttonLabel}
-                </Button>
+                <Button intent={Intent.PRIMARY}>{buttonLabel}</Button>
             </Popover>
         );
     }
-
-    private handleClickTargetOnlyButtonClick = () => {
-        this.toaster.show({
-            iconName: "info-sign",
-            intent: Intent.PRIMARY,
-            message: (
-                <>
-                    Click the <code>CLICK_TARGET_ONLY</code> button again to close the popover and regain interactivity
-                    in the rest of the app.
-                </>
-            ),
-            timeout: -1,
-        });
-    };
-
-    private handleClickTargetOnlyPopoverWillClose = () => {
-        this.toaster.clear();
-    };
 }
