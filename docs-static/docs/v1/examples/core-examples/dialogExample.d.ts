b2aed5fb700803a7b08cf6c69f0d88221a368289
Adi Dahiya <adahiya@palantir.com>
2018-01-16 17:23:04 -0500

    Commit v1 docs to docs-static/ folder

---
+++ docs-static/docs/v1/examples/core-examples/dialogExample.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+import { OverlayExample } from "./overlayExample";
+export declare class DialogExample extends OverlayExample {
+    protected renderExample(): JSX.Element;
+    protected renderOptions(): JSX.Element[][];
+}
