2b5bbee98fc469fb23e2d0f63d021079da7bff3e
Adi Dahiya <adi.dahiya14@gmail.com>
2018-01-17 15:52:28 -0500

    Prepare release 2.0.0-beta.1 (#2015)

--- docs-static/docs/v1/examples/core-examples/dialogExample.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { OverlayExample } from "./overlayExample";
-export declare class DialogExample extends OverlayExample {
-    protected renderExample(): JSX.Element;
-    protected renderOptions(): JSX.Element[][];
-}
