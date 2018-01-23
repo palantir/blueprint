85e41b394753ec92bcf832228a094d230ce968b1
Gilad Gray <ggray@palantir.com>
2018-01-23 15:59:10 -0800

    generate and export IconSvgs

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -5,5 +5,6 @@
  */
 
 export { IconClasses } from "./generated/iconClasses";
-export { IconName } from "./generated/iconName";
+export { IconName, LegacyIconName } from "./generated/iconName";
 export { IconContents } from "./generated/iconStrings";
+export { IconSvgs } from "./generated/iconSvgs";
