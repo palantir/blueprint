b325887335e1864a09b25643a9d4e45fb6de32bd
Gilad Gray <ggray@palantir.com>
2018-01-25 12:18:32 -0800

    simple filter for ApiBrowser

--- packages/docs-theme/src/components/typescript/apiBrowser.tsx
+++ packages/docs-theme/src/components/typescript/apiBrowser.tsx
@@ -4,33 +4,49 @@
  * Licensed under the terms of the LICENSE file distributed with this project.
  */
 
-import { Classes } from "@blueprintjs/core";
+import { Classes, InputGroup } from "@blueprintjs/core";
 import * as classNames from "classnames";
 import * as React from "react";
 import { DocumentationContextTypes, hasTypescriptData, IDocumentationContext } from "../../common/context";
 import { TypescriptExample } from "../../tags/typescript";
+import { handleStringChange } from "../baseExample";
 import { ApiLink } from "./apiLink";
 
 export interface IApiBrowserProps {
     section: string;
 }
 
-export class ApiBrowser extends React.PureComponent<IApiBrowserProps> {
+export interface IApiBrowserState {
+    query: string;
+}
+
+export class ApiBrowser extends React.PureComponent<IApiBrowserProps, IApiBrowserState> {
     public static contextTypes = DocumentationContextTypes;
     public context: IDocumentationContext;
 
+    public state: IApiBrowserState = { query: "" };
+    private handleQueryChange = handleStringChange(query => this.setState({ query }));
+
     public render() {
         const { section } = this.props;
+        const { query } = this.state;
         const data = this.context.getDocsData();
         if (!hasTypescriptData(data)) {
             return <h1>No Typescript data found.</h1>;
         }
+        const filteredLinks = Object.keys(data.typescript)
+            .filter(name => name.toLowerCase().indexOf(query.toLowerCase()) >= 0)
+            .map(name => <ApiLink className={classNames(Classes.MENU_ITEM, "docs-code")} key={name} name={name} />);
         return (
             <div className="docs-api-browser">
                 <div className="docs-api-list">
-                    {Object.keys(data.typescript).map(name => (
-                        <ApiLink className={classNames(Classes.MENU_ITEM, "docs-code")} key={name} name={name} />
-                    ))}
+                    <InputGroup
+                        leftIconName="search"
+                        placeholder="Filter..."
+                        value={query}
+                        onChange={this.handleQueryChange}
+                    />
+                    {filteredLinks}
                 </div>
                 <div className="docs-api-content">
                     <TypescriptExample tag="typescript" value={section} />
