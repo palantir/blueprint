697e48c29d74a84c5d972454ff51483515ddee81
Gilad Gray <ggray@palantir.com>
2018-01-25 11:30:23 -0800

    class CssExample

--- packages/docs-theme/src/tags/css.tsx
+++ packages/docs-theme/src/tags/css.tsx
@@ -4,51 +4,57 @@
  * Licensed under the terms of the LICENSE file distributed with this project.
  */
 
-import { IKssModifier, IKssPluginData, ITag } from "documentalist/dist/client";
+import { IKssPluginData, ITag } from "documentalist/dist/client";
 import * as React from "react";
 import { DocumentationContextTypes, IDocumentationContext } from "../common/context";
 import { ModifierTable } from "../components/modifierTable";
 
-export const CssExample: React.SFC<ITag> = ({ value }, { getDocsData }: IDocumentationContext) => {
-    const { css } = getDocsData() as IKssPluginData;
-    if (css == null || css[value] == null) {
-        return null;
-    }
-    const { markup, markupHtml, modifiers, reference } = css[value];
-    return (
-        <div>
-            {modifiers.length > 0 ? <ModifierTable modifiers={modifiers} /> : undefined}
-            <div className="docs-example-wrapper" data-reference={reference}>
-                {renderMarkupForModifier(markup, DEFAULT_MODIFIER)}
-                {modifiers.map(mod => renderMarkupForModifier(markup, mod))}
-            </div>
-            <div className="docs-markup" dangerouslySetInnerHTML={{ __html: markupHtml }} />
-        </div>
-    );
-};
-CssExample.contextTypes = DocumentationContextTypes;
-CssExample.displayName = "Docs.CssExample";
+export class CssExample extends React.PureComponent<ITag> {
+    public static contextTypes = DocumentationContextTypes;
+    public static displayName = "Docs.CssExample";
 
-function renderMarkupForModifier(markup: string, { name }: IKssModifier) {
-    const html = markup.replace(MODIFIER_PLACEHOLDER, (_, prefix) => {
-        if (prefix && name.charAt(0) === prefix) {
-            return name.slice(1);
-        } else if (!prefix) {
-            return name;
-        } else {
-            return "";
+    public context: IDocumentationContext;
+
+    public render() {
+        const { value } = this.props;
+        const { css } = this.context.getDocsData() as IKssPluginData;
+        if (css == null || css[value] == null) {
+            return null;
         }
-    });
-    return (
-        <div className="docs-example" data-modifier={name} key={name}>
-            <code>{name}</code>
-            <div dangerouslySetInnerHTML={{ __html: html }} />
-        </div>
-    );
+        const { markup, markupHtml, modifiers, reference } = css[value];
+        return (
+            <div>
+                {modifiers.length > 0 ? <ModifierTable modifiers={modifiers} /> : undefined}
+                <div className="docs-example-wrapper" data-reference={reference}>
+                    {this.renderMarkupExample(markup)}
+                    {modifiers.map(mod => this.renderMarkupExample(markup, mod.name))}
+                </div>
+                <div className="docs-markup" dangerouslySetInnerHTML={{ __html: markupHtml }} />
+            </div>
+        );
+    }
+
+    private renderMarkupExample(markup: string, modifierName = "default") {
+        return (
+            <div className="docs-example" data-modifier={modifierName} key={modifierName}>
+                <code>{modifierName}</code>
+                {this.renderMarkupForModifier(markup, modifierName)}
+            </div>
+        );
+    }
+
+    private renderMarkupForModifier(markup: string, modifierName: string) {
+        const html = markup.replace(MODIFIER_PLACEHOLDER_REGEXP, (_, prefix: string) => {
+            if (prefix && modifierName.charAt(0) === prefix) {
+                return modifierName.slice(1);
+            } else if (!prefix) {
+                return modifierName;
+            } else {
+                return "";
+            }
+        });
+        return <div dangerouslySetInnerHTML={{ __html: html }} />;
+    }
 }
 
-const MODIFIER_PLACEHOLDER = /\{\{([.:]?)modifier\}\}/g;
-const DEFAULT_MODIFIER: IKssModifier = {
-    documentation: "Default",
-    name: "default",
-};
+const MODIFIER_PLACEHOLDER_REGEXP = /\{\{([.:]?)modifier\}\}/g;
