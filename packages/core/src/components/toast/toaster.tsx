4a1ccb2f2ac54f5f5816d1a146cb023b644fecb7
Gilad Gray <ggray@palantir.com>
2018-01-29 11:59:24 -0800

    Toaster renders children after state.toasts

--- packages/core/src/components/toast/toaster.tsx
+++ packages/core/src/components/toast/toaster.tsx
@@ -27,6 +27,7 @@ export type ToasterPosition =
     | Position.BOTTOM_LEFT
     | Position.BOTTOM_RIGHT;
 
+/** Instance methods available on a `<Toaster>` component instance. */
 export interface IToaster {
     /**
      * Shows a new toast to the user, or updates an existing toast corresponding to the provided key (optional).
@@ -45,6 +46,10 @@ export interface IToaster {
     getToasts(): IToastOptions[];
 }
 
+/**
+ * Props supported by the `<Toaster>` component.
+ * These props can be passed as an argument to the static `Toaster.create(props?, container?)` method.
+ */
 export interface IToasterProps extends IProps {
     /**
      * Whether a toast should acquire application focus when it first opens.
@@ -165,6 +170,7 @@ export class Toaster extends AbstractPureComponent<IToasterProps, IToasterState>
                 transitionName="pt-toast"
             >
                 {this.state.toasts.map(this.renderToast, this)}
+                {this.props.children}
             </Overlay>
         );
     }
