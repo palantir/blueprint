a2ab9522268d980da742376d481462f1cb493430
Gilad Gray <ggray@palantir.com>
2018-01-31 18:09:18 -0800

    examples/core-examples/common/IconSelect
    .pt-label supports .pt-popover-wrapper

--- packages/docs-app/src/examples/core-examples/iconExample.tsx
+++ packages/docs-app/src/examples/core-examples/iconExample.tsx
@@ -6,23 +6,20 @@
 
 import * as React from "react";
 
-import { Classes, Icon, MenuItem, Slider } from "@blueprintjs/core";
-import { BaseExample, handleStringChange } from "@blueprintjs/docs-theme";
-import { IconClasses, IconName } from "@blueprintjs/icons";
-import { ItemRenderer, Suggest } from "@blueprintjs/select";
-import * as classNames from "classnames";
+import { Classes, Icon, Slider } from "@blueprintjs/core";
+import { BaseExample } from "@blueprintjs/docs-theme";
+import { IconName } from "@blueprintjs/icons";
+import { IconSelect } from "./common/iconSelect";
 
 export interface IIconExampleState {
     iconName: IconName;
     iconSize: number;
-    query: string;
 }
 
 export class IconExample extends BaseExample<IIconExampleState> {
     public state: IIconExampleState = {
         iconName: "calendar",
         iconSize: Icon.SIZE_STANDARD,
-        query: "calendar",
     };
 
     protected renderExample() {
@@ -34,29 +31,9 @@ export class IconExample extends BaseExample<IIconExampleState> {
     }
 
     protected renderOptions() {
-        const { iconName, iconSize, query } = this.state;
-        const suggestInputProps = {
-            leftIconName: query === iconName ? iconName : "blank",
-            // control suggest value so it can have initial value from state
-            onChange: this.handleQueryChange,
-            value: query,
-        };
+        const { iconName, iconSize } = this.state;
         return [
-            [
-                <label className={Classes.LABEL} key="icon-name-label">
-                    Icon name
-                </label>,
-                <IconSuggest
-                    key="icon-name"
-                    inputProps={suggestInputProps}
-                    items={ICON_NAMES.filter(item => item.indexOf(query) >= 0)}
-                    itemRenderer={renderIconItem}
-                    inputValueRenderer={this.renderIconValue}
-                    noResults={<MenuItem disabled={true} text="No results" />}
-                    onItemSelect={this.handleIconNameChange}
-                    popoverProps={{ minimal: true }}
-                />,
-            ],
+            [<IconSelect key="icon-name" iconName={iconName} onChange={this.handleIconNameChange} />],
             [
                 <label className={Classes.LABEL} key="icon-size-label">
                     Icon size
@@ -74,28 +51,9 @@ export class IconExample extends BaseExample<IIconExampleState> {
         ];
     }
 
-    private renderIconValue(icon: IconName) {
-        return icon;
-    }
-
     private handleIconSizeChange = (iconSize: number) => this.setState({ iconSize });
 
-    private handleIconNameChange = (iconName: IconName) => this.setState({ iconName, query: iconName });
-
-    // tslint:disable-next-line:member-ordering
-    private handleQueryChange = handleStringChange((query: string) => this.setState({ query }));
+    private handleIconNameChange = (iconName: IconName) => this.setState({ iconName });
 }
 
-const IconSuggest = Suggest.ofType<IconName>();
-const ICON_NAMES = Object.keys(IconClasses).map(
-    (name: keyof typeof IconClasses) => IconClasses[name].replace("pt-icon-", "") as IconName,
-);
 const MAX_ICON_SIZE = 100;
-
-const renderIconItem: ItemRenderer<IconName> = (icon, { handleClick, modifiers: { active } }) => {
-    const classes = classNames({
-        [Classes.ACTIVE]: active,
-        [Classes.INTENT_PRIMARY]: active,
-    });
-    return <MenuItem className={classes} iconName={icon} key={icon} onClick={handleClick} text={icon} />;
-};
