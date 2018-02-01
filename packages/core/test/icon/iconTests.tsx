de3013b1ed0518c4092b193e79b4db6e9a72b2f4
Gilad Gray <ggray@palantir.com>
2018-01-31 17:01:09 -0800

    fix tests

--- packages/core/test/icon/iconTests.tsx
+++ packages/core/test/icon/iconTests.tsx
@@ -8,25 +8,23 @@ import { assert } from "chai";
 import { shallow } from "enzyme";
 import * as React from "react";
 
-import { IconClasses } from "@blueprintjs/icons";
+import { IconClasses, IconName } from "@blueprintjs/icons";
 
 import { Classes, Icon, IIconProps, Intent } from "../../src/index";
 
 describe("<Icon>", () => {
     it("iconSize=16 renders standard size", () =>
-        assertIconSize(<Icon iconName="vertical-distribution" iconSize={Icon.SIZE_STANDARD} />, Icon.SIZE_STANDARD));
+        assertIconSize(<Icon iconName="graph" iconSize={Icon.SIZE_STANDARD} />, Icon.SIZE_STANDARD));
 
     it("iconSize=20 renders large size", () =>
-        assertIconSize(<Icon iconName="vertical-distribution" iconSize={Icon.SIZE_LARGE} />, Icon.SIZE_LARGE));
+        assertIconSize(<Icon iconName="graph" iconSize={Icon.SIZE_LARGE} />, Icon.SIZE_LARGE));
 
     it("renders intent class", () =>
-        assertIconClass(<Icon iconName="add" intent={Intent.DANGER} />, Classes.INTENT_DANGER));
+        assert.isTrue(shallow(<Icon iconName="add" intent={Intent.DANGER} />).hasClass(Classes.INTENT_DANGER)));
 
-    it("renders iconName class", () =>
-        assertIconClass(<Icon iconName="vertical-distribution" />, IconClasses.VERTICAL_DISTRIBUTION));
+    it("renders iconName class", () => assertIcon(<Icon iconName="calendar" />, "calendar"));
 
-    it("supports prefixed iconName", () =>
-        assertIconClass(<Icon iconName={IconClasses.AIRPLANE} />, IconClasses.AIRPLANE));
+    it("supports prefixed iconName", () => assertIcon(<Icon iconName={IconClasses.AIRPLANE} />, "airplane"));
 
     it("iconName=undefined renders nothing", () => {
         const icon = shallow(<Icon iconName={undefined} />);
@@ -34,10 +32,8 @@ describe("<Icon>", () => {
     });
 
     /** Asserts that rendered icon has given className. */
-    function assertIconClass(icon: React.ReactElement<IIconProps>, className: string) {
-        const wrapper = shallow(icon);
-        assert.isTrue(wrapper.hasClass(className));
-        return wrapper;
+    function assertIcon(icon: React.ReactElement<IIconProps>, iconName: IconName) {
+        assert.strictEqual(shallow(icon).text(), iconName);
     }
 
     /** Asserts that rendered icon has width/height equal to size. */
@@ -45,6 +41,5 @@ describe("<Icon>", () => {
         const wrapper = shallow(icon);
         assert.strictEqual(wrapper.prop("width"), size);
         assert.strictEqual(wrapper.prop("height"), size);
-        return wrapper;
     }
 });
