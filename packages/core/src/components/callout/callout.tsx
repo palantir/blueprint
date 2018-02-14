fe9b37df61ff28457c4f319525aa6314c104ad14
Gilad Gray <ggray@palantir.com>
2018-02-14 11:56:37 -0800

    Callout uses h4, fix icon v-align

--- packages/core/src/components/callout/callout.tsx
+++ packages/core/src/components/callout/callout.tsx
@@ -25,7 +25,7 @@ export interface ICalloutProps extends IIntentProps, IProps {
      * String content of optional title element.
      *
      * Due to a conflict with the HTML prop types, to provide JSX content simply pass
-     * `<h5>JSX title content<h5>` as first `children` element instead of using this prop.
+     * `<h4>JSX title content</h4>` as first `children` element instead of using this prop.
      */
     title?: string;
 }
@@ -42,7 +42,7 @@ export class Callout extends React.PureComponent<ICalloutProps & React.HTMLAttri
                         <Icon icon={iconName} iconSize={Icon.SIZE_LARGE} />
                     </span>
                 )}
-                {title && <h5>{title}</h5>}
+                {title && <h4>{title}</h4>}
                 {children}
             </div>
         );
