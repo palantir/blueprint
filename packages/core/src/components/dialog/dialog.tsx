c202090ffa091d31a4b0bb62ce55b944ac37aaa1
Antoine Llorca <allorca@palantir.com>
2018-02-15 11:16:01 -0800

    Change dialog header to h4

--- packages/core/src/components/dialog/dialog.tsx
+++ packages/core/src/components/dialog/dialog.tsx
@@ -115,7 +115,7 @@ export class Dialog extends AbstractPureComponent<IDialogProps, {}> {
         return (
             <div className={Classes.DIALOG_HEADER}>
                 <Icon icon={icon} iconSize={Icon.SIZE_LARGE} />
-                <h5>{title}</h5>
+                <h4>{title}</h4>
                 {this.maybeRenderCloseButton()}
             </div>
         );
