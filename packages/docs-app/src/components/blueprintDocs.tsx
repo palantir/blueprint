a24d743dbb3877e57fcc6afbfd930cc31dcfc0c4
Gilad Gray <ggray@palantir.com>
2018-02-16 13:53:42 -0800

    move Banner into docs-theme

--- packages/docs-app/src/components/blueprintDocs.tsx
+++ packages/docs-app/src/components/blueprintDocs.tsx
@@ -6,7 +6,7 @@
 
 import { Icon, Menu, MenuItem, Popover, Position, setHotkeysDialogProps } from "@blueprintjs/core";
 import { IPackageInfo } from "@blueprintjs/docs-data";
-import { Documentation, IDocumentationProps } from "@blueprintjs/docs-theme";
+import { Banner, Documentation, IDocumentationProps } from "@blueprintjs/docs-theme";
 import { ITsDocBase } from "documentalist/dist/client";
 import * as React from "react";
 import { NavbarActions } from "./navbarActions";
@@ -50,10 +50,10 @@ export class BlueprintDocs extends React.Component<IBlueprintDocsProps, { themeN
         );
         return (
             <>
-                <a className="docs-banner" target="_blank" href="http://blueprintjs.com/docs/v1/">
+                <Banner href="http://blueprintjs.com/docs/v1/">
                     This documentation is for Blueprint v2, which is currently under development. Click here to go to
                     the v1 docs.
-                </a>
+                </Banner>
                 <Documentation
                     {...this.props}
                     className={this.state.themeName}
