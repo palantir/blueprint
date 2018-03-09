a05dd3d6b50e76bd2c0b7fdc6c3c9ecd45812913
Gilad Gray <ggray@palantir.com>
2018-03-09 11:30:35 -0800

    Updates

--- docs/v2/examples/core-examples/spinnerExample.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import { ProgressExample } from "./progressExample";
-export declare class SpinnerExample extends ProgressExample {
-    private handleSizeChange;
-    protected renderExample(): JSX.Element;
-    protected renderOptions(): JSX.Element[][];
-}
--- svgs.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const init: (elem: HTMLElement) => void;
