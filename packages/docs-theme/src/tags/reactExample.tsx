12ae3494698a691c71fe8513a099cfdc46321793
Gilad Gray <ggray@palantir.com>
2018-03-19 20:22:56 -0700

    switch View Source link to minimal button (clickable)

--- packages/docs-theme/src/tags/reactExample.tsx
+++ packages/docs-theme/src/tags/reactExample.tsx
@@ -4,8 +4,7 @@
  * Licensed under the terms of the LICENSE file distributed with this project.
  */
 
-import { Classes, Icon, Intent, Tag } from "@blueprintjs/core";
-import classNames from "classnames";
+import { AnchorButton, Classes, Intent } from "@blueprintjs/core";
 import { ITag } from "documentalist/dist/client";
 import * as React from "react";
 
@@ -25,15 +24,17 @@ export interface IExampleProps {
     name: string;
 }
 
-const viewSourceClasses = classNames(Classes.LARGE, Classes.MINIMAL);
 export const ReactExample: React.SFC<IExampleProps> = props => (
     <div className="docs-example-wrapper">
         {props.example.render({ id: props.name })}
-        <a className="view-example-source" href={props.example.sourceUrl} target="_blank">
-            <Tag className={viewSourceClasses} intent={Intent.PRIMARY} interactive={true}>
-                <Icon icon="code" iconSize={Icon.SIZE_LARGE} />View source on GitHub
-            </Tag>
-        </a>
+        <AnchorButton
+            className={Classes.MINIMAL}
+            href={props.example.sourceUrl}
+            icon="code"
+            intent={Intent.PRIMARY}
+            target="_blank"
+            text="View source on GitHub"
+        />
     </div>
 );
 ReactExample.displayName = "Docs2.ReactExample";
