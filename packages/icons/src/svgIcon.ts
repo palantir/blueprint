d7d5a2ff42d32f57fb9af976e9dd540cbdc9f93a
reiv <metareiv@gmail.com>
2018-04-04 00:33:34 +0200

    Generate svgIcons.ts with individual icon exports

---
+++ packages/icons/src/svgIcon.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2018 Palantir Technologies, Inc. All rights reserved.
+ */
+
+/**
+ * Compact representation of an SVG icon.
+ * - `[0]`: name
+ * - `[1]`: 16px SVG path
+ * - `[2]`: 20px SVG path
+ */
+export type SVGIcon = [string, string[], string[]];
