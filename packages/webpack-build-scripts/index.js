69b9bd8438f2830d8f3b42a95a781294a19f41ef
Gilad Gray <ggray@palantir.com>
2018-04-03 13:37:40 -0700

    use expanded externals syntax

--- packages/webpack-build-scripts/index.js
+++ packages/webpack-build-scripts/index.js
@@ -5,26 +5,10 @@
 
 const baseConfig = require("./webpack.config.base");
 const karmaConfig = require("./webpack.config.karma");
+const COMMON_EXTERNALS = require("./externals");
 
 module.exports = {
     baseConfig,
     karmaConfig,
-    COMMON_EXTERNALS: {
-        "@blueprintjs/core": "var Blueprint.Core",
-        "@blueprintjs/icons": "var Blueprint.Icons",
-        "@blueprintjs/datetime": "var Blueprint.Datetime",
-        "@blueprintjs/labs": "var Blueprint.Labs",
-        "@blueprintjs/select": "var Blueprint.Select",
-        "@blueprintjs/table": "var Blueprint.Table",
-        "@blueprintjs/timezone": "var Blueprint.Timezone",
-        "classnames": "classNames",
-        "dom4": "window",
-        "jquery": "$",
-        "moment": "moment",
-        "moment-timezone": "moment",
-        "react": "React",
-        "react-transition-group": "ReactTransitionGroup",
-        "react-day-picker": "DayPicker",
-        "react-dom": "ReactDOM",
-    },
+    COMMON_EXTERNALS,
 };
