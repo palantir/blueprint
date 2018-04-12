3300c6a89f92afb7044f194fff7bb1d7c92a3388
danielkatz125 <danielkatz125@gmail.com>
2018-04-12 14:35:53 -0400

    fix indentation

--- packages/table/src/table.tsx
+++ packages/table/src/table.tsx
@@ -423,19 +423,19 @@ export class Table extends AbstractComponent<ITableProps, ITableState> {
 
     public static childContextTypes: React.ValidationMap<
         IColumnInteractionBarContextTypes
-        > = columnInteractionBarContextTypes;
+    > = columnInteractionBarContextTypes;
 
     // these default values for `resizeRowsByApproximateHeight` have been
     // fine-tuned to work well with default Table font styles.
     private static resizeRowsByApproximateHeightDefaults: Record<
         keyof IResizeRowsByApproximateHeightOptions,
         number
-        > = {
-            getApproximateCharWidth: 8,
-            getApproximateLineHeight: 18,
-            getCellHorizontalPadding: 2 * Locator.CELL_HORIZONTAL_PADDING,
-            getNumBufferLines: 1,
-        };
+    > = {
+        getApproximateCharWidth: 8,
+        getApproximateLineHeight: 18,
+        getCellHorizontalPadding: 2 * Locator.CELL_HORIZONTAL_PADDING,
+        getNumBufferLines: 1,
+    };
 
     private static SHALLOW_COMPARE_PROP_KEYS_BLACKLIST = [
         "selectedRegions", // (intentionally omitted; can be deeply compared to save on re-renders in controlled mode)
@@ -1219,13 +1219,13 @@ export class Table extends AbstractComponent<ITableProps, ITableState> {
         const nextScrollTop =
             tableBottom < viewportRect.top + viewportRect.height
                 ? // scroll the last row into view
-                Math.max(0, tableBottom - viewportRect.height)
+                    Math.max(0, tableBottom - viewportRect.height)
                 : viewportRect.top;
 
         const nextScrollLeft =
             tableRight < viewportRect.left + viewportRect.width
                 ? // scroll the last column into view
-                Math.max(0, tableRight - viewportRect.width)
+                    Math.max(0, tableRight - viewportRect.width)
                 : viewportRect.left;
 
         this.syncViewportPosition(nextScrollLeft, nextScrollTop);
