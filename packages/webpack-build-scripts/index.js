fd70406ab398d975b4377c3cdd09fb2a29efdc1a
Gilad Gray <giladgray@gmail.com>
2018-04-16 15:22:58 -0700

    add all local packages to common externals (#2328)
    * add all local packages to common externals
    so they never get bundled in each other
    
    * use expanded externals syntax
    
    * whitespace

--- packages/webpack-build-scripts/index.js
+++ packages/webpack-build-scripts/index.js
@@ -5,24 +5,10 @@
 
 const baseConfig = require("./webpack.config.base");
 const karmaConfig = require("./webpack.config.karma");
+const COMMON_EXTERNALS = require("./externals");
 
 module.exports = {
     baseConfig,
     karmaConfig,
-    COMMON_EXTERNALS: {
-        "@blueprintjs/core": "var Blueprint.Core",
-        "@blueprintjs/datetime": "var Blueprint.Datetime",
-        "@blueprintjs/labs": "var Blueprint.Labs",
-        "@blueprintjs/table": "var Blueprint.Table",
-        "classnames": "classNames",
-        "dom4": "window",
-        "jquery": "$",
-        "moment": "moment",
-        "moment-timezone": "moment",
-        "react": "React",
-        "react-transition-group": "ReactTransitionGroup",
-        "react-day-picker": "DayPicker",
-        "react-dom": "ReactDOM",
-        "tether": "Tether",
-    },
+    COMMON_EXTERNALS,
 };
