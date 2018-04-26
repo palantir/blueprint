c951fc9832e2d740cc2114cf5d696de062ef88c5
Hammadi Agharass <hammadi.agharass@usmba.ac.ma>
2018-04-26 14:47:41 +0100

    Remove newline to pass CircleCI test
    The previous commit didn't pass the test due the newline insertion, I've removed the newline and I hope it would pass it.

--- packages/core/test/editable-text/editableTextTests.tsx
+++ packages/core/test/editable-text/editableTextTests.tsx
@@ -138,7 +138,6 @@ describe("<EditableText>", () => {
 
         it("calls onEdit when entering edit mode and passes the initial value to the callback", () => {
             const editSpy = spy();
-            
             const INIT_VALUE = "hello";
             
             mount(<EditableText onEdit={editSpy} defaultValue={INIT_VALUE} />)
