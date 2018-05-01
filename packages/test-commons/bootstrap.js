d844b0f7ba9101a9ef4cb9e8ca314c4614b2909c
Gilad Gray <ggray@palantir.com>
2018-05-01 13:03:42 -0700

    remove console color from enzyme message
    seems to bork karma-junit-reorter
    https://github.com/karma-runner/karma-junit-reporter/issues/116

--- packages/test-commons/bootstrap.js
+++ packages/test-commons/bootstrap.js
@@ -12,4 +12,4 @@ const Adapter = require(`enzyme-adapter-react-${process.env.REACT || 16}`);
 Enzyme.configure({ adapter: new Adapter() });
 
 // tslint:disable-next-line:no-console
-console.info(`Enzyme configured with \x1b[35m${Adapter.name}\x1b[0m`);
+console.info(`Enzyme configured with *${Adapter.name}*`);
