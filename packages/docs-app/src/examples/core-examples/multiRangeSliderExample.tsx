bbdc8ff045abf4059bf6fc5323480f16f8e92c78
Jonah Scheinerman <jonah@scheinerman.net>
2018-05-01 18:32:09 -0400

    Improvements to the example

--- packages/docs-app/src/examples/core-examples/multiRangeSliderExample.tsx
+++ packages/docs-app/src/examples/core-examples/multiRangeSliderExample.tsx
@@ -9,22 +9,31 @@ import * as React from "react";
 import { Intent, MultiRangeSlider, SliderHandle, Switch } from "@blueprintjs/core";
 import { BaseExample, handleBooleanChange } from "@blueprintjs/docs-theme";
 
+export interface ISliderValues {
+    outerStart: number;
+    innerStart: number;
+    innerEnd: number;
+    outerEnd: number;
+}
+
 export interface IMultiRangeSliderExampleState {
     hideOuterBounds?: boolean;
     twoTailed?: boolean;
-    values?: number[];
+    values?: ISliderValues;
     vertical?: boolean;
 }
 
-interface IBoundValuesUpdate {
-    [index: number]: number;
-}
-
+// tslint:disable:object-literal-sort-keys
 export class MultiRangeSliderExample extends BaseExample<IMultiRangeSliderExampleState> {
     public state: IMultiRangeSliderExampleState = {
         hideOuterBounds: false,
         twoTailed: true,
-        values: [12, 36, 72, 90],
+        values: {
+            outerStart: 12,
+            innerStart: 36,
+            innerEnd: 72,
+            outerEnd: 90,
+        },
         vertical: false,
     };
 
@@ -44,10 +53,10 @@ export class MultiRangeSliderExample extends BaseExample<IMultiRangeSliderExampl
                     vertical={this.state.vertical}
                     defaultTrackIntent={Intent.SUCCESS}
                 >
-                    {this.renderOuterStartHandle()}
-                    {this.renderInnerStartHandle()}
                     {this.renderInnerEndHandle()}
                     {this.renderOuterEndHandle()}
+                    {this.renderOuterStartHandle()}
+                    {this.renderInnerStartHandle()}
                 </MultiRangeSlider>
             </div>
         );
@@ -78,11 +87,11 @@ export class MultiRangeSliderExample extends BaseExample<IMultiRangeSliderExampl
         if (hideOuterBounds) {
             return null;
         }
-        return <SliderHandle trackIntentBefore={Intent.DANGER} type="start" value={values[0]} />;
+        return <SliderHandle trackIntentBefore={Intent.DANGER} type="start" value={values.outerStart} />;
     }
 
     private renderInnerStartHandle() {
-        return <SliderHandle trackIntentBefore={Intent.WARNING} type="start" value={this.state.values[1]} />;
+        return <SliderHandle trackIntentBefore={Intent.WARNING} type="start" value={this.state.values.innerStart} />;
     }
 
     private renderInnerEndHandle() {
@@ -90,7 +99,7 @@ export class MultiRangeSliderExample extends BaseExample<IMultiRangeSliderExampl
         if (!twoTailed) {
             return null;
         }
-        return <SliderHandle trackIntentAfter={Intent.WARNING} type="end" value={values[2]} />;
+        return <SliderHandle trackIntentAfter={Intent.WARNING} type="end" value={values.innerEnd} />;
     }
 
     private renderOuterEndHandle() {
@@ -98,45 +107,32 @@ export class MultiRangeSliderExample extends BaseExample<IMultiRangeSliderExampl
         if (hideOuterBounds || !twoTailed) {
             return null;
         }
-        return <SliderHandle trackIntentAfter={Intent.DANGER} type="end" value={values[3]} />;
+        return <SliderHandle trackIntentAfter={Intent.DANGER} type="end" value={values.outerEnd} />;
     }
 
     private handleChange = (newValues: number[]) => {
         const updatedValues = this.getUpdatedValues(newValues);
-        const values = this.state.values.slice();
-        for (let index = 0; index < 4; index++) {
-            if (index in updatedValues) {
-                values[index] = updatedValues[index];
-            }
-        }
-        values.sort((a, b) => a - b);
+        const values = { ...this.state.values, ...updatedValues };
         this.setState({ values });
     };
 
-    private getUpdatedValues(values: number[]): IBoundValuesUpdate {
+    private getUpdatedValues(values: number[]): Partial<ISliderValues> {
         const { hideOuterBounds, twoTailed } = this.state;
         if (hideOuterBounds) {
             if (twoTailed) {
-                const [newInnerStart, newInnerEnd] = values;
-                return {
-                    [1]: newInnerStart,
-                    [2]: newInnerEnd,
-                };
+                const [innerStart, innerEnd] = values;
+                return { innerStart, innerEnd };
             } else {
-                const [newInnerStart] = values;
-                return {
-                    [1]: newInnerStart,
-                };
+                const [innerStart] = values;
+                return { innerStart };
             }
         } else {
             if (twoTailed) {
-                return values;
+                const [outerStart, innerStart, innerEnd, outerEnd] = values;
+                return { outerStart, innerStart, innerEnd, outerEnd };
             } else {
-                const [newOuterStart, newInnerStart] = values;
-                return {
-                    [0]: newOuterStart,
-                    [1]: newInnerStart,
-                };
+                const [outerStart, outerEnd] = values;
+                return { outerStart, outerEnd };
             }
         }
     }
