8331335fd8328cc067de1c798b2df69e6efa17c7
Gilad Gray <ggray@palantir.com>
2018-05-15 16:20:30 -0700

    SpinnerExample no longer extends ProgressExample

--- packages/docs-app/src/examples/core-examples/spinnerExample.tsx
+++ packages/docs-app/src/examples/core-examples/spinnerExample.tsx
@@ -6,9 +6,9 @@
 
 import * as React from "react";
 
-import { Classes, Code, Spinner } from "@blueprintjs/core";
-import { handleStringChange } from "@blueprintjs/docs-theme";
-import { ProgressExample } from "./progressExample";
+import { Classes, Intent, Label, Slider, Spinner, Switch } from "@blueprintjs/core";
+import { Example, handleBooleanChange, handleStringChange, IExampleProps } from "@blueprintjs/docs-theme";
+import { IntentSelect } from "./common/intentSelect";
 
 const SIZES = [
     { label: "Default", value: "" },
@@ -16,26 +16,67 @@ const SIZES = [
     { label: "Large", value: Classes.LARGE },
 ];
 
-export class SpinnerExample extends ProgressExample {
-    private handleSizeChange = handleStringChange(className => this.setState({ className }));
+export interface ISpinnerExampleState {
+    hasValue: boolean;
+    intent?: Intent;
+    size: string;
+    value: number;
+}
 
-    protected renderExample() {
-        const { className, hasValue, intent, value } = this.state;
-        return <Spinner className={className} intent={intent} value={hasValue ? value : null} />;
+export class SpinnerExample extends React.PureComponent<IExampleProps, ISpinnerExampleState> {
+    public state: ISpinnerExampleState = {
+        hasValue: false,
+        size: "",
+        value: 0.7,
+    };
+
+    private handleIndeterminateChange = handleBooleanChange(hasValue => this.setState({ hasValue }));
+    private handleModifierChange = handleStringChange((intent: Intent) => this.setState({ intent }));
+    private handleSizeChange = handleStringChange(size => this.setState({ size }));
+
+    public render() {
+        const { size, hasValue, intent, value } = this.state;
+        return (
+            <Example options={this.renderOptions()} {...this.props}>
+                <Spinner
+                    intent={intent}
+                    large={size === Classes.LARGE}
+                    small={size === Classes.SMALL}
+                    value={hasValue ? value : null}
+                />
+            </Example>
+        );
     }
 
-    protected renderOptions() {
-        const options = super.renderOptions();
-        options.push([
-            <label className={Classes.LABEL} key="size">
-                Size (via <Code>className</Code>)
-                <div className={Classes.SELECT}>
-                    <select value={this.state.className} onChange={this.handleSizeChange}>
-                        {SIZES.map((opt, i) => <option key={i} {...opt} />)}
-                    </select>
-                </div>
-            </label>,
-        ]);
-        return options;
+    private renderOptions() {
+        const { size, hasValue, intent, value } = this.state;
+        return (
+            <>
+                <Switch checked={hasValue} label="Known Value" onChange={this.handleIndeterminateChange} />
+                <Slider
+                    disabled={!hasValue}
+                    labelStepSize={1}
+                    min={0}
+                    max={1}
+                    onChange={this.handleValueChange}
+                    labelRenderer={this.renderLabel}
+                    stepSize={0.1}
+                    showTrackFill={false}
+                    value={value}
+                />
+                <IntentSelect intent={intent} onChange={this.handleModifierChange} />,
+                <Label text="Size">
+                    <div className={Classes.SELECT}>
+                        <select value={size} onChange={this.handleSizeChange}>
+                            {SIZES.map((opt, i) => <option key={i} {...opt} />)}
+                        </select>
+                    </div>
+                </Label>,
+            </>
+        );
     }
+
+    private renderLabel = (value: number) => value.toFixed(1);
+
+    private handleValueChange = (value: number) => this.setState({ value });
 }
