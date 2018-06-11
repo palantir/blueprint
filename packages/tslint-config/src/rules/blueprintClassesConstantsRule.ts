41f772812489ae8b4c957f6643996d7940ed4021
Gilad Gray <ggray@palantir.com>
2018-06-11 16:57:32 -0700

    blueprint-classes-constants rule only detects leading pt- *prefix*

--- packages/tslint-config/src/rules/blueprintClassesConstantsRule.ts
+++ packages/tslint-config/src/rules/blueprintClassesConstantsRule.ts
@@ -7,7 +7,8 @@
 import * as Lint from "tslint";
 import * as ts from "typescript";
 
-const PATTERN = /\bpt-[\w-]+\b/;
+// detect "pt-" *prefix*: not preceded by letter or dash
+const PATTERN = /[^\w-]pt-[\w-]+/;
 
 export class Rule extends Lint.Rules.AbstractRule {
     public static metadata: Lint.IRuleMetadata = {
@@ -33,7 +34,8 @@ function walk(ctx: Lint.WalkContext<void>): void {
         if (ts.isStringLiteral(node)) {
             const match = PATTERN.exec(node.getFullText());
             if (match != null) {
-                ctx.addFailureAt(node.getFullStart() + match.index, match[0].length, Rule.FAILURE_STRING);
+                // ignore first character of match: negated character class
+                ctx.addFailureAt(node.getFullStart() + match.index + 1, match[0].length - 1, Rule.FAILURE_STRING);
             }
         }
         return ts.forEachChild(node, cb);
