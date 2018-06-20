f0527816fca7ad9aead0bf596c481588106ec768
Gilad Gray <giladgray@gmail.com>
2018-06-20 15:06:13 -0700

    upgrade documentalist, add "edit this page" links (#2615)
    * documentalist 1.4.0
    
    * compile-docs-data: use NpmPlugin instead of releases.json and versions.json
    
    * refactor BlueprintDocs to use npm plugin data
    
    * add hasNpmData typeguard
    
    * remove unused type
    
    * add Documentation renderPageActions prop to put actions in upper-right corner of page
    
    * put Edit link in the actions! (hardcoded github url)
    
    * url constants
    
    * script comment

--- packages/docs-data/compile-docs-data.js
+++ packages/docs-data/compile-docs-data.js
@@ -22,22 +22,18 @@ const GENERATED_SRC_DIR = path.resolve(process.cwd(), "./src/generated");
 const BUILD_DIR = path.resolve(process.cwd(), "build");
 const NAV_PAGE_NAME = "_nav";
 
-const DOCS_DATA_FILENAME = "docs.json";
-const DOCS_RELEASES_FILENAME = "releases.json";
-const DOCS_VERSIONS_FIELENAME = "versions.json";
+const DOCS_DATA_PATH = path.join(GENERATED_SRC_DIR, "docs.json");
 
 (async () => {
     try {
         if (!fs.existsSync(GENERATED_SRC_DIR)) {
             fs.mkdirSync(GENERATED_SRC_DIR);
         }
-
         await generateDocumentalistData();
-        generateReleasesData();
-        generateVersionsData();
     } catch (err) {
-        console.error(`[docs-data] Failed to generate JSON data:`);
-        console.error(err);
+        // console.error messages get swallowed by lerna but console.log is emitted to terminal.
+        console.log(`[docs-data] ERROR when generating JSON docs data:`);
+        console.log(err);
         throw err;
     }
 })();
@@ -48,6 +44,7 @@ const DOCS_VERSIONS_FIELENAME = "versions.json";
 function generateDocumentalistData() {
     return new dm.Documentalist({
         markdown: { renderer: docsUtils.markedRenderer },
+        sourceBaseDir: ROOT_DIR,
         // must mark our @Decorator APIs as reserved so we can use them in code samples
         reservedTags: ["import", "ContextMenuTarget", "HotkeysTarget"],
     })
@@ -61,12 +58,25 @@ function generateDocumentalistData() {
             }),
         )
         .use(".scss", new dm.KssPlugin())
-        .documentGlobs("../*/src/**/*.{scss,md}", "../*/src/index.{ts,tsx}")
+        .use("package.json", new dm.NpmPlugin())
+        .documentGlobs("../*/src/**/*.{scss,md}", "../*/src/index.{ts,tsx}", "../*/package.json")
         .then(docs => JSON.stringify(docs, transformDocumentalistData, 2))
-        .then(content => fs.writeFileSync(path.join(GENERATED_SRC_DIR, DOCS_DATA_FILENAME), content));
+        .then(content => fs.writeFileSync(DOCS_DATA_PATH, content));
 }
 
 function transformDocumentalistData(key, value) {
+    if (key === "versions" && Array.isArray(value)) {
+        // one major version per release
+        const majors = new Map();
+        for (version of value) {
+            const major = semver.major(version)
+            if (!majors.has(major) || semver.gt(version, majors.get(major))) {
+                majors.set(major, version);
+            }
+        }
+        // reverse the list so highest version is first (easier indexing)
+        return Array.from(majors.values()).reverse();
+    }
     // remove all class declarations as they're currently unused and only increase bundle size
     if (value != null && value.kind !== "class") {
         return replaceNS(value);
@@ -74,73 +84,9 @@ function transformDocumentalistData(key, value) {
     return undefined;
 }
 
-/**
- * Create a JSON file containing latest released version of each project
- */
-function generateReleasesData() {
-    const packageDirectories = fs
-        .readdirSync(PACKAGES_DIR)
-        .map(name => path.join(PACKAGES_DIR, name))
-        .filter(source => fs.lstatSync(source).isDirectory());
-
-    const releases = packageDirectories
-        .filter(packagePath => fs.existsSync(path.resolve(packagePath, "package.json")))
-        .map(packagePath => require(path.resolve(packagePath, "package.json")))
-        // only include non-private projects
-        .filter(project => !project.private)
-        // just these two fields from package.json:
-        .map(({ name, version }) => ({ name, version }));
-
-    fs.writeFileSync(path.join(GENERATED_SRC_DIR, DOCS_RELEASES_FILENAME), JSON.stringify(releases, null, 2));
-}
-
-/**
- * Create a JSON file containing published versions of the documentation
- */
-function generateVersionsData() {
-    let stdout = "";
-    const child = spawn("git", ["tag"]);
-    child.stdout.setEncoding("utf8");
-    child.stdout.on("data", data => {
-        stdout += data;
-    });
-    child.on("close", () => {
-        /** @type {Map<string, string>} */
-        const majorVersionMap = stdout
-            .split("\n")
-            // turn @blueprintjs/core@* tags into version numbers
-            .filter(val => /\@blueprintjs\/core\@[1-9]\d*\.\d+\.\d+.*/.test(val))
-            .map(val => val.slice("@blueprintjs/core@".length))
-            .reduce((map, version) => {
-                const major = semver.major(version);
-                if (!map.has(major) || semver.gt(version, map.get(major))) {
-                    map.set(major, version);
-                }
-                return map;
-            }, new Map());
-        // sort in reverse order (so latest is first)
-        const majorVersions = Array.from(majorVersionMap.values()).sort(semver.rcompare);
-
-        console.info("[docs-data] Major versions found:", majorVersions.join(", "));
-
-        fs.writeFileSync(
-            path.join(GENERATED_SRC_DIR, DOCS_VERSIONS_FIELENAME),
-            JSON.stringify(strMapToObj(majorVersionMap), null, 2),
-        );
-    });
-}
-
-function strMapToObj(strMap) {
-    const obj = Object.create(null);
-    for (const [k, v] of strMap) {
-        // We don’t escape the key '__proto__'
-        // which can cause problems on older engines
-        obj[k] = v;
-    }
-    return obj;
-}
-
 /** @param {any} text replace `#{$ns}` with Blueprint class namespace. if not a string, simply returns `text`. */
 function replaceNS(text) {
-    return typeof text === "string" ? text.replace(/#{\$ns}|@ns/g, Classes.getClassNamespace()) : text;
+    return typeof text === "string"
+        ? text.replace(/#{\$ns}|@ns/g, Classes.getClassNamespace())
+        : text;
 }
