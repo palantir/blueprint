b03e0fd1d273daa4d527d4e5ea51e2bb88f1e4dc
Gilad Gray <ggray@palantir.com>
2018-07-03 17:31:33 -0700

    initial isOpen state respects popoverProps

--- packages/select/src/components/select/suggest.tsx
+++ packages/select/src/components/select/suggest.tsx
@@ -69,7 +69,7 @@ export class Suggest<T> extends React.PureComponent<ISuggestProps<T>, ISuggestSt
     }
 
     public state: ISuggestState<T> = {
-        isOpen: false,
+        isOpen: (this.props.popoverProps && this.props.popoverProps.isOpen) || false,
         isTyping: false,
         query: "",
     };
