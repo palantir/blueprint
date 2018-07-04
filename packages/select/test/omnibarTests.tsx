fbceefbbf1531ed16cfeb7bc1f2fb80e8413c878
Gilad Gray <ggray@palantir.com>
2018-07-03 17:44:38 -0700

    add Omnibar tests!

---
+++ packages/select/test/omnibarTests.tsx
@@ -0,0 +1,14 @@
+/*
+* Copyright 2018 Palantir Technologies, Inc. All rights reserved.
+*
+* Licensed under the terms of the LICENSE file distributed with this project.
+*/
+
+import { mount } from "enzyme";
+import * as React from "react";
+import { Omnibar } from "../src";
+import { selectComponentSuite } from "./selectComponentSuite";
+
+describe("<Omnibar>", () => {
+    selectComponentSuite(props => mount(<Omnibar {...props} isOpen={true} />));
+});
