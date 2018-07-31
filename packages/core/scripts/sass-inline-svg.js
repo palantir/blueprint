9a9dd7d3b6c973383650d92ec0991857ff9a7cdf
Gilad Gray <giladgray@gmail.com>
2018-07-31 12:14:24 -0700

    [Breadcrumbs] inline SVG icons (#2742)
    * rename icon inliner function to svg-icon()
    
    * inline breadcrumbs icons
    
    trickier because color gets embedded in icon so it requires re-embedding for dark theme.
    skipped that and just used colors that look good enough in both.

--- packages/core/scripts/sass-inline-svg.js
+++ packages/core/scripts/sass-inline-svg.js
@@ -2,8 +2,8 @@ const inliner = require('sass-inline-svg');
 
 module.exports = {
     // Sass function to inline a UI Icon svg and change its path color:
-    // svg("16px/icon-name.svg", (path: (fill: $color)) )
-    'svg': inliner('../../resources/icons', {
+    // svg-icon("16px/icon-name.svg", (path: (fill: $color)) )
+    'svg-icon': inliner('../../resources/icons', {
         // run through SVGO first
         optimize: true,
         // minimal "uri" encoding is smaller than base64
