0c09726bdbbd4be4892c97e67363dc0e8caefb71
Gilad Gray <giladgray@gmail.com>
2018-08-03 14:34:18 -0700

    [Menu] supports arbitrary HTML attributes (#2757)
    * Menu supports HTML attributes
    
    * remove invalid html props

--- packages/core/src/components/menu/menu.tsx
+++ packages/core/src/components/menu/menu.tsx
@@ -12,7 +12,7 @@ import { DISPLAYNAME_PREFIX, IProps } from "../../common/props";
 import { MenuDivider } from "./menuDivider";
 import { MenuItem } from "./menuItem";
 
-export interface IMenuProps extends IProps {
+export interface IMenuProps extends IProps, React.HTMLAttributes<HTMLUListElement> {
     /** Whether the menu items in this menu should use a large appearance. */
     large?: boolean;
 
@@ -27,10 +27,11 @@ export class Menu extends React.Component<IMenuProps, {}> {
     public static Item = MenuItem;
 
     public render() {
-        const classes = classNames(Classes.MENU, { [Classes.LARGE]: this.props.large }, this.props.className);
+        const { className, children, large, ulRef, ...htmlProps } = this.props;
+        const classes = classNames(Classes.MENU, { [Classes.LARGE]: large }, className);
         return (
-            <ul className={classes} ref={this.props.ulRef}>
-                {this.props.children}
+            <ul {...htmlProps} className={classes} ref={ulRef}>
+                {children}
             </ul>
         );
     }
