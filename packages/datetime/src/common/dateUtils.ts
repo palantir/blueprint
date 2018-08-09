0aca854635bd3d062324d2d1774b448d795a18d7
Gilad Gray <ggray@palantir.com>
2018-08-09 16:32:07 -0700

    areSameDay calls areSameMonth

--- packages/datetime/src/common/dateUtils.ts
+++ packages/datetime/src/common/dateUtils.ts
@@ -37,13 +37,7 @@ export function areRangesEqual(dateRange1: DateRange, dateRange2: DateRange) {
 }
 
 export function areSameDay(date1: Date, date2: Date) {
-    return (
-        date1 != null &&
-        date2 != null &&
-        date1.getDate() === date2.getDate() &&
-        date1.getMonth() === date2.getMonth() &&
-        date1.getFullYear() === date2.getFullYear()
-    );
+    return areSameMonth(date1, date2) && date1.getDate() === date2.getDate();
 }
 
 export function areSameMonth(date1: Date, date2: Date) {
