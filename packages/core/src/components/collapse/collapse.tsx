5a3658d71aac1fffa1d557b194732bd9c1ecbd32
Ulysse Mizrahi <odusseys@gmail.com>
2018-09-07 11:23:22 +0200

    fix documentation

--- packages/core/src/components/collapse/collapse.tsx
+++ packages/core/src/components/collapse/collapse.tsx
@@ -66,7 +66,13 @@ export enum AnimationStates {
  * OPEN
  * When in this state, the collapse height is set to auto, and the body Y is set to 0 (so the element can be seen
  * as normal).
+ * 
+ * OPEN START
+ * When in this state, the body is re-rendered, height is set to the measured body height and the body Y is set to 0.
  *
+ * OPENING
+ * Animation begins, height is set to auto. This is all animated, and on complete, the state changes to OPEN.
+ * 
  * CLOSING_START
  * When in this state, height has been changed from auto to the measured height of the body to prepare for the
  * closing animation in CLOSING_END.
@@ -75,12 +81,11 @@ export enum AnimationStates {
  * When in this state, the height is set to 0 and the body Y is at -height. Both of these properties are transformed,
  * and then after the animation is complete, the state changes to CLOSED.
  *
- * OPENING
- * When in this state, the body is re-rendered, height is set to the measured body height and the body Y is set to 0.
- * This is all animated, and on complete, the state changes to OPEN.
+
+
  *
  * When changing the isOpen prop, the following happens to the states:
- * isOpen = true : CLOSED -> OPENING -> OPEN
+ * isOpen = true : CLOSED -> OPEN_START -> OPENING -> OPEN
  * isOpen = false: OPEN -> CLOSING_START -> CLOSING_END -> CLOSED
  * These are all animated.
  */
