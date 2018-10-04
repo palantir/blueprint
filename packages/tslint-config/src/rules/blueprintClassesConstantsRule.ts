4372b49b2b4323c95a93b1b2f9a2c65d3d425fc1
Gilad Gray <giladgray@gmail.com>
2018-10-04 15:45:47 -0700

    [tslint] refactor fixer code (#2993)
    * pull string literal replacement logic to separate function
    
    much more legible, only one push().
    add doc comments.
    
    * further refactor into two helper methods
    
    replacement logic is dead simple now
    
    * remove a helper function
    
    * deconstruct, pattern at the top
    
    * small edits
    
    * amend regex to ignore pt-icon strings
    
    * pull out match iteration logic

--- packages/tslint-config/src/rules/blueprintClassesConstantsRule.ts
+++ packages/tslint-config/src/rules/blueprintClassesConstantsRule.ts
@@ -9,6 +9,9 @@ import * as utils from "tsutils";
 import * as ts from "typescript";
 import { addImportToFile } from "./utils/addImportToFile";
 
+// find all pt- prefixed classes, except those that begin with pt-icon (handled by other rules)
+const BLUEPRINT_CLASSNAME_PATTERN = /[^\w-<.](pt-(?!icon-?)[\w-]+)/g;
+
 export class Rule extends Lint.Rules.AbstractRule {
     public static metadata: Lint.IRuleMetadata = {
         ruleName: "blueprint-classes-constants",
@@ -28,40 +31,33 @@ export class Rule extends Lint.Rules.AbstractRule {
     }
 }
 
-interface IMatchingString {
-    match: string;
-    index: number;
-}
-
 function walk(ctx: Lint.WalkContext<void>) {
     let shouldFixImports = true;
     return ts.forEachChild(ctx.sourceFile, callback);
 
     function callback(node: ts.Node): void {
         if (ts.isStringLiteralLike(node) || ts.isTemplateExpression(node)) {
-            const ptMatches: IMatchingString[] = [];
-            const fullText = node.getFullText();
-            let currentMatch: RegExpExecArray | null;
-            // tslint:disable-next-line:no-conditional-assignment
-            while ((currentMatch = BLUEPRINT_CLASSNAME_PATTERN.exec(fullText)) != null) {
-                const fullBlueprintName = currentMatch[1]; // e.g. pt-breadcrumb
-                const blueprintClassName = getBlueprintClassName(fullBlueprintName); // e.g. breadcrumb
+            const prefixMatches = getAllMatches(node.getFullText());
+            if (prefixMatches.length > 0) {
+                const ptClassStrings = prefixMatches.map(m => m.match);
+                const replacementText = utils.isStringLiteral(node)
+                    ? // "string literal" likely becomes `${template} string` so we may need to change how it is assigned
+                      wrapForParent(getLiteralReplacement(node.getText(), ptClassStrings), node)
+                    : getTemplateReplacement(node.getText(), ptClassStrings);
 
-                // See if we should ignore this class or not.
-                if (blueprintClassName == null || shouldIgnoreBlueprintClass(blueprintClassName)) {
-                    continue;
-                } else {
-                    ptMatches.push({ match: fullBlueprintName, index: currentMatch.index });
+                const replacements = [new Lint.Replacement(node.getStart(), node.getWidth(), replacementText)];
+                if (shouldFixImports) {
+                    // add an import statement for `Classes` constants at most once.
+                    replacements.unshift(addImportToFile(ctx.sourceFile, ["Classes"], "@blueprintjs/core"));
+                    shouldFixImports = false;
                 }
-            }
-            if (ptMatches.length > 0) {
+
                 ctx.addFailureAt(
-                    node.getFullStart() + ptMatches[0].index + 1,
-                    ptMatches[0].match.length,
+                    node.getFullStart() + prefixMatches[0].index + 1,
+                    prefixMatches[0].match.length,
                     Rule.FAILURE_STRING,
-                    getReplacement(node, ptMatches.map(p => p.match), ctx.sourceFile, shouldFixImports),
+                    replacements,
                 );
-                shouldFixImports = false;
             }
         }
 
@@ -69,86 +65,64 @@ function walk(ctx: Lint.WalkContext<void>) {
     }
 }
 
-function getReplacement(
-    node: ts.StringLiteralLike | ts.TemplateExpression,
-    ptClassStrings: string[],
-    file: ts.SourceFile,
-    shouldFixImport: boolean,
-) {
-    const replacements: Lint.Replacement[] = [];
-
-    // We may need to add a blueprint import to the top of the file. We only want to do this once per file, otherwise,
-    // we'll keep stacking imports and mess things up.
-    if (shouldFixImport) {
-        replacements.push(addImportToFile(file, ["Classes"], "@blueprintjs/core"));
+/** Returns array of all invalid string matches detected in the given className string. */
+function getAllMatches(className: string) {
+    const ptMatches = [];
+    let currentMatch: RegExpMatchArray | null;
+    // tslint:disable-next-line:no-conditional-assignment
+    while ((currentMatch = BLUEPRINT_CLASSNAME_PATTERN.exec(className)) != null) {
+        ptMatches.push({ match: currentMatch[1], index: currentMatch.index });
     }
-
-    if (utils.isStringLiteral(node)) {
-        // remove all illegal classnames, then slice off the quotes, and trim any remaining white space
-        const stringWithoutPtClasses = ptClassStrings
-            .reduce((value, cssClass) => {
-                return value.replace(cssClass, "");
-            }, node.getText())
-            .slice(1, -1)
-            .trim();
-        const templateStrings = ptClassStrings.map(n => `\${${convertPtClassName(n)}}`).join(" ");
-        if (stringWithoutPtClasses.length > 0) {
-            const replacement = `\`${templateStrings} ${stringWithoutPtClasses}\``;
-            replacements.push(
-                new Lint.Replacement(node.getStart(), node.getWidth(), wrapForParent(replacement, node, node.parent)),
-            );
-        } else {
-            const replacement =
-                ptClassStrings.length === 1 ? convertPtClassName(ptClassStrings[0]) : `\`${templateStrings}\``;
-            replacements.push(
-                new Lint.Replacement(node.getStart(), node.getWidth(), wrapForParent(replacement, node, node.parent)),
-            );
-        }
-    } else if (utils.isTemplateExpression(node) || utils.isNoSubstitutionTemplateLiteral(node)) {
-        let replacementText = node.getText();
-        ptClassStrings.forEach(classString => {
-            const classReplacement = `\${${convertPtClassName(classString)}}`;
-            replacementText = replacementText.replace(classString, classReplacement);
-        });
-        replacements.push(new Lint.Replacement(node.getStart(), node.getWidth(), replacementText));
-    }
-    return replacements;
+    return ptMatches;
 }
 
-function wrapForParent(statement: string, node: ts.Node, parentNode: ts.Node | undefined) {
-    if (parentNode === undefined) {
+/** Produce replacement text for a string literal that contains invalid classes. */
+function getLiteralReplacement(className: string, ptClassStrings: string[]) {
+    // remove all illegal classnames, then slice off the quotes, and trim any remaining white space
+    const stringWithoutPtClasses = ptClassStrings
+        .reduce((value, cssClass) => value.replace(cssClass, ""), className)
+        .slice(1, -1)
+        .trim();
+    // special case: only one invalid class name
+    if (stringWithoutPtClasses.length === 0 && ptClassStrings.length === 1) {
+        return convertPtClassName(ptClassStrings[0]);
+    }
+    // otherwise produce a `template string`
+    const templateStrings = ptClassStrings.map(n => `\${${convertPtClassName(n)}}`).join(" ");
+    return `\`${[templateStrings, stringWithoutPtClasses].join(" ").trim()}\``;
+}
+
+/** Produce replacement text for a `template string` that contains invalid classes. */
+function getTemplateReplacement(className: string, ptClassStrings: string[]) {
+    return ptClassStrings.reduce(
+        (value, cssClass) => value.replace(cssClass, `\${${convertPtClassName(cssClass)}}`),
+        className,
+    );
+}
+
+/** Wrap the given statement based on the type of the parent node: JSX props, expressions, etc. */
+function wrapForParent(statement: string, node: ts.Node) {
+    const { parent } = node;
+    if (parent === undefined) {
         return statement;
-    } else if (utils.isJsxAttribute(parentNode)) {
+    } else if (utils.isJsxAttribute(parent)) {
         return `{${statement}}`;
-    } else if (utils.isExpressionStatement(parentNode)) {
+    } else if (utils.isExpressionStatement(parent)) {
         return `[${statement}]`;
         // If we're changing the key, it will be child index 0 and we need to wrap it.
         // Else, we're changing a value, and there's no need to wrap
-    } else if (utils.isPropertyAssignment(parentNode) && parentNode.getChildAt(0) === node) {
+    } else if (utils.isPropertyAssignment(parent) && parent.getChildAt(0) === node) {
         return `[${statement}]`;
     } else {
         return statement;
     }
 }
 
+/** Converts a `pt-class-name` literal to `Classes.CLASS_NAME` constant. */
 function convertPtClassName(text: string) {
     const className = text
         .replace("pt-", "")
         .replace(/-/g, "_")
         .toUpperCase();
     return `Classes.${className}`;
-}
-
-const BLUEPRINT_CLASSNAME_PATTERN = /[^\w-<.](pt-[\w-]+)/g;
-
-function getBlueprintClassName(fullClassName: string): string | undefined {
-    if (fullClassName.length < 3) {
-        return undefined;
-    } else {
-        return fullClassName.slice(3);
-    }
-}
-
-function shouldIgnoreBlueprintClass(blueprintClassName: string): boolean {
-    return blueprintClassName.startsWith("icon");
 }
