0927e2d7dfbf174815a3f9c7efc188857a4cf1a5
Piotr Kwiatkowski <piotrk@palantir.com>
2018-10-10 13:04:40 +0100

    changed viewport units

--- packages/docs-app/src/examples/core-examples/drawerExample.tsx
+++ packages/docs-app/src/examples/core-examples/drawerExample.tsx
@@ -76,63 +76,6 @@ export class DrawerExample extends React.PureComponent<IExampleProps<IBlueprintE
                                 can build upon. And the enterprise data foundation goes where the business drives it.
                             </p>
                             <p>Start the revolution. Unleash the power of data integration with Palantir Foundry.</p>
-                            <p>
-                                Palantir Foundry radically reimagines the way enterprises interact with data by
-                                amplifying amplifying and extending the power of data integration. With Foundry, anyone
-                                can source, fuse, and transform data into any shape they desire. Business analysts
-                                become data engineers — and leaders in their organization’s data revolution.
-                            </p>
-                            <p>
-                                Foundry’s back end includes a suite of best-in-class data integration capabilities: data
-                                provenance, git-style versioning semantics, granular access controls, branching,
-                                transformation authoring, and more. But these powers are not limited to the back-end IT
-                                shop.
-                            </p>
-                            <p>
-                                In Foundry, tables, applications, reports, presentations, and spreadsheets operate as
-                                data integrations in their own right. Access controls, transformation logic, and data
-                                quality flow from original data source to intermediate analysis to presentation in real
-                                time. Every end product created in Foundry becomes a new data source that other users
-                                can build upon. And the enterprise data foundation goes where the business drives it.
-                            </p>
-                            <p>
-                                Palantir Foundry radically reimagines the way enterprises interact with data by
-                                amplifying and extending the power of data integration. With Foundry, anyone can source,
-                                fuse, and transform data into any shape they desire. Business analysts become data
-                                engineers — and leaders in their organization’s data revolution.
-                            </p>
-                            <p>
-                                Foundry’s back end includes a suite of best-in-class data integration capabilities: data
-                                provenance, git-style versioning semantics, granular access controls, branching,
-                                transformation authoring, and more. But these powers are not limited to the back-end IT
-                                shop.
-                            </p>
-                            <p>
-                                In Foundry, tables, applications, reports, presentations, and spreadsheets operate as
-                                data integrations in their own right. Access controls, transformation logic, and data
-                                quality flow from original data source to intermediate analysis to presentation in real
-                                time. Every end product created in Foundry becomes a new data source that other users
-                                can build upon. And the enterprise data foundation goes where the business drives it.
-                            </p>
-                            <p>
-                                Palantir Foundry radically reimagines the way enterprises interact with data by
-                                amplifying and extending the power of data integration. With Foundry, anyone can source,
-                                fuse, and transform data into any shape they desire. Business analysts become data
-                                engineers — and leaders in their organization’s data revolution.
-                            </p>
-                            <p>
-                                Foundry’s back end includes a suite of best-in-class data integration capabilities: data
-                                provenance, git-style versioning semantics, granular access controls, branching,
-                                transformation authoring, and more. But these powers are not limited to the back-end IT
-                                shop.
-                            </p>
-                            <p>
-                                In Foundry, tables, applications, reports, presentations, and spreadsheets operate as
-                                data integrations in their own right. Access controls, transformation logic, and data
-                                quality flow from original data source to intermediate analysis to presentation in real
-                                time. Every end product created in Foundry becomes a new data source that other users
-                                can build upon. And the enterprise data foundation goes where the business drives it.
-                            </p>
                         </div>
                     </div>
                     <div className={Classes.DRAWER_FOOTER}>Footer</div>
