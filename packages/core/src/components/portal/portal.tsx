d94c5c8e5593acc29a36fdf624e3f3a1ab2903f3
KochiyaOcean <root@kochiyaocean.org>
2018-10-18 04:42:19 +0900

    [Portal] add container prop

--- packages/core/src/components/portal/portal.tsx
+++ packages/core/src/components/portal/portal.tsx
@@ -21,6 +21,11 @@ export interface IPortalProps extends IProps {
      * Callback invoked when the children of this `Portal` have been added to the DOM.
      */
     onChildrenMount?: () => void;
+
+    /**
+     * The HTML element that children will be mounted to. Default value is `document.body`.
+     */
+    container?: HTMLElement;
 }
 
 export interface IPortalState {
@@ -49,6 +54,9 @@ const REACT_CONTEXT_TYPES: ValidationMap<IPortalContext> = {
 export class Portal extends React.Component<IPortalProps, IPortalState> {
     public static displayName = `${DISPLAYNAME_PREFIX}.Portal`;
     public static contextTypes = REACT_CONTEXT_TYPES;
+    public static defaultProps: IPortalProps = {
+        container: typeof document !== "undefined" ? document.body : null,
+    };
 
     public context: IPortalContext;
     public state: IPortalState = { hasMounted: false };
@@ -68,7 +76,7 @@ export class Portal extends React.Component<IPortalProps, IPortalState> {
 
     public componentDidMount() {
         this.portalElement = this.createContainerElement();
-        document.body.appendChild(this.portalElement);
+        this.props.container.appendChild(this.portalElement);
         this.setState({ hasMounted: true }, this.props.onChildrenMount);
         if (cannotCreatePortal) {
             this.unstableRenderNoPortal();
