4e2177339e7609e73996b986b69f1054f1860861
Gilad Gray <ggray@palantir.com>
2018-12-05 11:41:04 -0800

    add notes about types

--- packages/core/src/common/utils/safeInvokeMember.ts
+++ packages/core/src/common/utils/safeInvokeMember.ts
@@ -6,26 +6,41 @@
 
 import { isFunction } from "../utils";
 
+/*
+Understanding the types here:
+
+`<Object, Key, ...Args, Return>`
+
+`{ [k in K]?: (a: A) => R }`: This is a MAPPED TYPE that enforces that keys `K`
+are all optional member functions with the given signature. The type `k` is only
+used in the mapping definition.
+
+`K extends keyof T`: A subset of the keys of the object `T`. The mapped type
+above then imposes a restriction on the _values_ of these keys in `T`. Note that
+`safeInvokeMember` only supports a single key, so the subset here has exactly
+one item.
+*/
+
 /**
  * Safely invoke the member function with no arguments, if the object
  * exists and the given key is indeed a function, and return its value.
- * Otherwise, return undefined.
+ * Otherwise, return `undefined`.
  */
-export function safeInvokeMember<T extends { [P in K]?: () => R }, K extends keyof T, R = void>(
+export function safeInvokeMember<T extends { [k in K]?: () => R }, K extends keyof T, R = void>(
     obj: T | undefined,
     key: K,
 ): R | undefined;
 /**
  * Safely invoke the member function with one argument, if the object
  * exists and the given key is indeed a function, and return its value.
- * Otherwise, return undefined.
+ * Otherwise, return `undefined`.
  *
  * ```js
  * // example usage
  * safeInvokeMember(this.props.inputProps, "onChange", evt);
  * ```
  */
-export function safeInvokeMember<T extends { [P in K]?: (a: A) => R }, K extends keyof T, A, R = void>(
+export function safeInvokeMember<T extends { [k in K]?: (a: A) => R }, K extends keyof T, A, R = void>(
     obj: T | undefined,
     key: K,
     arg1: A,
@@ -33,9 +48,9 @@ export function safeInvokeMember<T extends { [P in K]?: (a: A) => R }, K extends
 /**
  * Safely invoke the member function with two arguments, if the object
  * exists and the given key is indeed a function, and return its value.
- * Otherwise, return undefined.
+ * Otherwise, return `undefined`.
  */
-export function safeInvokeMember<T extends { [P in K]?: (a: A, b: B) => R }, K extends keyof T, A, B, R = void>(
+export function safeInvokeMember<T extends { [k in K]?: (a: A, b: B) => R }, K extends keyof T, A, B, R = void>(
     obj: T | undefined,
     key: K,
     arg1: A,
@@ -47,7 +62,7 @@ export function safeInvokeMember<T extends { [P in K]?: (a: A, b: B) => R }, K e
  * Otherwise, return undefined.
  */
 export function safeInvokeMember<
-    T extends { [P in K]?: (a: A, b: B, c: C) => R },
+    T extends { [k in K]?: (a: A, b: B, c: C) => R },
     K extends keyof T,
     A,
     B,
