cc2c2f4bb2ace3695300bf1b61f3485562bb58e4
Gilad Gray <ggray@palantir.com>
2018-12-21 18:26:54 -0800

    add handle*Change utils

---
+++ packages/docz-theme/src/theme/common/events.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2018 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the terms of the LICENSE file distributed with this project.
+ */
+
+/** Event handler that exposes the target element's value as a boolean. */
+export function handleBooleanChange(handler: (checked: boolean) => void) {
+    return (event: React.FormEvent<HTMLElement>) => handler((event.target as HTMLInputElement).checked);
+}
+
+/** Event handler that exposes the target element's value as a string. */
+export function handleStringChange(handler: (value: string) => void) {
+    return (event: React.FormEvent<HTMLElement>) => handler((event.target as HTMLInputElement).value);
+}
+
+/** Event handler that exposes the target element's value as a number. */
+export function handleNumberChange(handler: (value: number) => void) {
+    return handleStringChange(value => handler(+value));
+}
