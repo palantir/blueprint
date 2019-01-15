dcf1ee2eb125b9f1ca8882789136ded044c2b7d8
Chris Lewis <clewis@palantir.com>
2019-01-15 15:39:33 -0800

    Update example

---
+++ packages/docs-app/src/examples/timezone-examples/components/index.ts
@@ -0,0 +1,7 @@
+/*
+ * Copyright 2018 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the terms of the LICENSE file distributed with this project.
+ */
+
+export * from "./customTimezoneTarget";
--- packages/docs-app/src/examples/timezone-examples/timezonePickerExample.tsx
+++ packages/docs-app/src/examples/timezone-examples/timezonePickerExample.tsx
@@ -6,12 +6,14 @@
 
 import * as React from "react";
 
-import { H5, Radio, RadioGroup, Switch } from "@blueprintjs/core";
+import { H5, Position, Radio, RadioGroup, Switch } from "@blueprintjs/core";
 import { Example, handleBooleanChange, handleStringChange, IExampleProps } from "@blueprintjs/docs-theme";
 import { TimezoneDisplayFormat, TimezonePicker } from "@blueprintjs/timezone";
+import { CustomTimezoneTarget } from "./components";
 
 export interface ITimezonePickerExampleState {
     disabled: boolean;
+    showCustomTarget: boolean;
     showLocalTimezone: boolean;
     targetDisplayFormat: TimezoneDisplayFormat;
     timezone: string;
@@ -20,6 +22,7 @@ export interface ITimezonePickerExampleState {
 export class TimezonePickerExample extends React.PureComponent<IExampleProps, ITimezonePickerExampleState> {
     public state: ITimezonePickerExampleState = {
         disabled: false,
+        showCustomTarget: false,
         showLocalTimezone: true,
         targetDisplayFormat: TimezoneDisplayFormat.COMPOSITE,
         timezone: "",
@@ -27,18 +30,20 @@ export class TimezonePickerExample extends React.PureComponent<IExampleProps, IT
 
     private handleDisabledChange = handleBooleanChange(disabled => this.setState({ disabled }));
     private handleShowLocalChange = handleBooleanChange(showLocalTimezone => this.setState({ showLocalTimezone }));
+    private handleCustomChildChange = handleBooleanChange(showCustomTarget => this.setState({ showCustomTarget }));
     private handleFormatChange = handleStringChange((targetDisplayFormat: TimezoneDisplayFormat) =>
         this.setState({ targetDisplayFormat }),
     );
 
     public render() {
-        const { timezone, targetDisplayFormat, disabled, showLocalTimezone } = this.state;
+        const { timezone, targetDisplayFormat, disabled, showCustomTarget, showLocalTimezone } = this.state;
 
         const options = (
             <>
                 <H5>Props</H5>
                 <Switch checked={showLocalTimezone} label="Show local timezone" onChange={this.handleShowLocalChange} />
                 <Switch checked={disabled} label="Disabled" onChange={this.handleDisabledChange} />
+                <Switch checked={showCustomTarget} label="Custom target" onChange={this.handleCustomChildChange} />
                 <RadioGroup
                     label="Display format"
                     onChange={this.handleFormatChange}
@@ -58,11 +63,18 @@ export class TimezonePickerExample extends React.PureComponent<IExampleProps, IT
                     value={timezone}
                     onChange={this.handleTimezoneChange}
                     valueDisplayFormat={targetDisplayFormat}
+                    popoverProps={{ position: Position.BOTTOM }}
                     showLocalTimezone={showLocalTimezone}
                     disabled={disabled}
-                />
+                >
+                    {showCustomTarget ? this.renderCustomTarget() : undefined}
+                </TimezonePicker>
             </Example>
         );
+    }
+
+    private renderCustomTarget() {
+        return <CustomTimezoneTarget timezone={this.state.timezone} />;
     }
 
     private handleTimezoneChange = (timezone: string) => this.setState({ timezone });
