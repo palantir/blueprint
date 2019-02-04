1acf0ba77facb34d347ce88c0a2f13854180766c
Gilad Gray <ggray@palantir.com>
2019-02-04 14:07:45 -0800

    click to download sketch files from Resources page

---
+++ packages/docs-app/src/components/resources.tsx
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the terms of the LICENSE file distributed with this project.
+ */
+
+import { Card } from "@blueprintjs/core";
+import download from "downloadjs";
+import * as React from "react";
+
+const URL_BASE = "https://github.palantir.build/design/blueprint/blob/develop/resources/sketch";
+
+const RESOURCES: IResourceProps[] = [
+    { fileName: "Core Kit.sketch", lastUpdated: "August 22, 2018" },
+    { fileName: "Blueprint Colors.sketchpalette", lastUpdated: "March 22, 2016" },
+];
+
+export const Resources: React.SFC = () => (
+    <>
+        {RESOURCES.map(resource => (
+            <ResourceCard key={resource.fileName} {...resource} />
+        ))}
+    </>
+);
+
+interface IResourceProps {
+    fileName: string;
+    lastUpdated: string;
+}
+
+class ResourceCard extends React.PureComponent<IResourceProps> {
+    public render() {
+        return (
+            <Card className="blueprint-resource" interactive={true} onClick={this.handleClick}>
+                <span>{this.props.fileName}</span>
+                <small>Last updated {this.props.lastUpdated}</small>
+            </Card>
+        );
+    }
+
+    private handleClick = () => download(`${URL_BASE}/${this.props.fileName}`);
+}
--- packages/docs-app/src/resources.md
+++ packages/docs-app/src/resources.md
@@ -2,11 +2,4 @@
 
 @## Sketch assets
 
-<a class="blueprint-resource @ns-card @ns-interactive" href="https://github.com/palantir/blueprint/tree/develop/resources/sketch/Core%20Kit.sketch" target="_blank">
-    <span>Core Kit.sketch</span>
-    <small>Last updated August 22, 2018</small>
-</a>
-<a class="blueprint-resource @ns-card @ns-interactive" href="https://github.com/palantir/blueprint/tree/develop/resources/sketch/Blueprint%20Colors.sketchpalette" target="_blank">
-    <span>Blueprint Colors.sketchpalette</span>
-    <small>Last updated March 22, 2016</small>
-</a>
+@reactDocs Resources
