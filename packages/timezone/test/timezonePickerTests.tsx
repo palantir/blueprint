67dc88596f01c94121b8fa75f163127a72ec38ae
Adi Dahiya <adahiya@palantir.com>
2019-02-19 16:36:32 -0500

    Update other test suites for new enzyme types

--- packages/timezone/test/timezonePickerTests.tsx
+++ packages/timezone/test/timezonePickerTests.tsx
@@ -5,7 +5,7 @@
  */
 
 import { assert } from "chai";
-import { mount, shallow, ShallowWrapper } from "enzyme";
+import { mount, shallow as untypedShallow, ShallowRendererProps, ShallowWrapper } from "enzyme";
 import * as moment from "moment-timezone";
 import * as React from "react";
 import * as sinon from "sinon";
@@ -30,6 +30,14 @@ import {
 import { ITimezonePickerProps, ITimezonePickerState, TimezoneDisplayFormat, TimezonePicker } from "../src/index";
 
 type TimezonePickerShallowWrapper = ShallowWrapper<ITimezonePickerProps, ITimezonePickerState>;
+
+/**
+ * @see https://github.com/DefinitelyTyped/DefinitelyTyped/issues/26979#issuecomment-465304376
+ */
+// tslint:disable no-unnecessary-callback-wrapper
+const shallow = (el: React.ReactElement, options?: ShallowRendererProps): TimezonePickerShallowWrapper =>
+    untypedShallow<TimezonePicker>(el, options);
+// tslint:enable no-unnecessary-callback-wrapper
 
 const VALUE = "America/Los_Angeles";
 
