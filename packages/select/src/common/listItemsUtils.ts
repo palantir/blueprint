dfbcedd3548829806245b2616d8cc52eb955d86d
Chris Lewis <clewis@palantir.com>
2019-02-26 13:23:35 -0800

    Update function comments

--- packages/select/src/common/listItemsUtils.ts
+++ packages/select/src/common/listItemsUtils.ts
@@ -4,24 +4,33 @@
  * Licensed under the terms of the LICENSE file distributed with this project.
  */
 
-const CREATE_NEW_ITEM_BRAND = "blueprint-create-item";
-
-/** The reserved type of the "Create Item" option in item lists. */
+/**
+ * The reserved type of the "Create Item" option in item lists. This is intended
+ * not to conflict with any custom item type `T` that might be used in  item
+ * list.
+ */
 export interface ICreateNewItem {
-    __blueprintBrand: typeof CREATE_NEW_ITEM_BRAND;
+    __blueprintBrand: "blueprint-create-item";
 }
 
+/** Returns an instance of a "Create Item" object. */
 export function getCreateNewItem(): ICreateNewItem {
-    return { __blueprintBrand: CREATE_NEW_ITEM_BRAND };
+    return { __blueprintBrand: "blueprint-create-item" };
 }
 
+/**
+ * Type guard returning `true` if the provided item (e.g. the current
+ * `activeItem`) is a "Create Item" option.
+ */
 export function isCreateNewItem<T>(item: T | ICreateNewItem | null | undefined): item is ICreateNewItem {
-    return item != null && (item as ICreateNewItem).__blueprintBrand === CREATE_NEW_ITEM_BRAND;
+    return item != null && (item as ICreateNewItem).__blueprintBrand === "blueprint-create-item";
 }
 
+/**
+ * Returns the type of the the current active item. This will be a no-op unless
+ * the `activeItem` is `undefined` or a "Create Item" option, in which case
+ * `null` will be returned instead.
+ */
 export function getActiveItem<T>(activeItem: T | ICreateNewItem | null | undefined): T | null {
-    if (activeItem != null && !isCreateNewItem(activeItem)) {
-        return activeItem;
-    }
-    return null;
+    return activeItem == null || isCreateNewItem(activeItem) ? null : activeItem;
 }
