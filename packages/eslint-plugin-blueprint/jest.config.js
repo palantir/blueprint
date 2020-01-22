580dd4d28514096b6715a302c8e849c1bf09a4b9
Adi Dahiya <adi.dahiya14@gmail.com>
2020-01-22 15:33:49 -0500

    [eslint-plugin-blueprint] chore: run tests in CI

--- packages/eslint-plugin-blueprint/jest.config.js
+++ packages/eslint-plugin-blueprint/jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
     testEnvironment: 'node',
     transform: {
-      '^.+\\.tsx?$': 'ts-jest',
+        '^.+\\.tsx?$': 'ts-jest',
     },
     testRegex: '__tests__\/.+\\.test\\.ts$',
     collectCoverage: false,
     collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     coverageReporters: ['text-summary', 'lcov'],
+    reporters: ['default', 'jest-junit']
 };
