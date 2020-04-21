7233f8fe9b16972a99185fa6dc0ac3c22149ac16
Adi Dahiya <adahiya@palantir.com>
2020-04-21 02:01:50 -0400

    run format

--- packages/docs-data/src/index.d.ts
+++ packages/docs-data/src/index.d.ts
@@ -2,7 +2,13 @@
  * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
  */
 
-import { INpmPluginData, IMarkdownPluginData, IKssExample, IKssPluginData, ITypescriptPluginData } from "@documentalist/client";
+import {
+    INpmPluginData,
+    IMarkdownPluginData,
+    IKssExample,
+    IKssPluginData,
+    ITypescriptPluginData,
+} from "@documentalist/client";
 
 export type IDocsCompleteData = IMarkdownPluginData & INpmPluginData & IKssPluginData & ITypescriptPluginData;
 
