fab2b7ed9f7d85c79c132e9ae4921a219913544f
Adi Dahiya <adi.dahiya14@gmail.com>
2020-04-21 23:00:17 -0400

    [webpack-build-scripts] enable esModule option

--- packages/webpack-build-scripts/webpack.config.base.js
+++ packages/webpack-build-scripts/webpack.config.base.js
@@ -54,11 +54,18 @@ if (!IS_PRODUCTION) {
     );
 }
 
+const extractPlugin = {
+    loader: MiniCssExtractPlugin.loader,
+    options: {
+        esModule: true,
+    },
+};
+
 // Module loaders for .scss files, used in reverse order:
 // compile Sass, apply PostCSS, interpret CSS as modules.
 const scssLoaders = [
     // Only extract CSS to separate file in production mode.
-    IS_PRODUCTION ? MiniCssExtractPlugin.loader : require.resolve("style-loader"),
+    IS_PRODUCTION ? extractPlugin : require.resolve("style-loader"),
     {
         loader: require.resolve("css-loader"),
         options: {
