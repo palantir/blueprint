0435c806c1e9b65b3ab5f06cf00d67578f1e347e
Adi Dahiya <adi.dahiya14@gmail.com>
2020-04-27 00:59:29 -0400

    install eslint-plugin-import, enable no-cycle

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,18 @@
 const config = require("./packages/eslint-config");
 
 const xtends = ["./packages/eslint-config"];
+const plugins = [];
+const rules = {};
 
 // in CI, we don't wan to run eslint-plugin-prettier because it has a ~50% performance penalty.
 // instead, we run yarn format-check at the root to ensure prettier formatting
-if (!process.env.CI) {
+if (process.env.CI) {
+    xtends.push("plugin:import/typescript");
+    plugins.push(
+        "import"
+    );
+    rules["import/no-cycle"] = "error";
+} else {
     xtends.push("plugin:prettier/recommended");
 }
 
@@ -12,6 +20,8 @@ module.exports = {
     ...config,
     root: true,
     extends: xtends,
+    plugins,
+    rules,
     ignorePatterns: [
         "node_modules",
         "dist",
