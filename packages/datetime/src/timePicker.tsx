1671a8c7a6a5f4f1ff06c7a166c8549e6c03c5cd
Oleksii Myronenko <mironenkoalexey@gmail.com>
2020-05-21 18:28:28 +0300

    [datetime] feat(TimePicker): add autoFocus prop (#4124)

--- packages/datetime/src/timePicker.tsx
+++ packages/datetime/src/timePicker.tsx
@@ -50,6 +50,12 @@ export type TimePrecision = typeof TimePrecision[keyof typeof TimePrecision];
 
 export interface ITimePickerProps extends IProps {
     /**
+     * Whether to focus the first input when it opens initially.
+     * @default false
+     */
+    autoFocus?: boolean;
+
+    /**
      * Initial time the `TimePicker` will display.
      * This should not be set if `value` is set.
      */
@@ -146,6 +152,7 @@ export interface ITimePickerState {
 
 export class TimePicker extends React.Component<ITimePickerProps, ITimePickerState> {
     public static defaultProps: ITimePickerProps = {
+        autoFocus: false,
         disabled: false,
         maxTime: getDefaultMaxTime(),
         minTime: getDefaultMinTime(),
@@ -251,6 +258,7 @@ export class TimePicker extends React.Component<ITimePickerProps, ITimePickerSta
 
     private renderInput(className: string, unit: TimeUnit, value: string) {
         const isValid = isTimeUnitValid(unit, parseInt(value, 10));
+        const isHour = unit === TimeUnit.HOUR_12 || unit === TimeUnit.HOUR_24;
 
         return (
             <input
@@ -266,6 +274,7 @@ export class TimePicker extends React.Component<ITimePickerProps, ITimePickerSta
                 onKeyUp={this.getInputKeyUpHandler(unit)}
                 value={value}
                 disabled={this.props.disabled}
+                autoFocus={isHour && this.props.autoFocus}
             />
         );
     }
