88fce69c38460d157787cc5a73908e7ecc5642bb
Adi Dahiya <adi.dahiya14@gmail.com>
2020-05-21 11:48:12 -0400

    [core] fix shape of Utils export
    follow-up to f53aa750ae48f946dbb62b15a792c1a8c01916ae

--- packages/core/src/common/index.ts
+++ packages/core/src/common/index.ts
@@ -30,16 +30,7 @@ export * from "./refs";
 
 import * as Classes from "./classes";
 import * as Keys from "./keys";
-import { getRef } from "./refs";
 import * as Utils from "./utils";
-
-/**
- * Utils.getRef() was added to the public API in @blueprintjs/core@3.27.0,
- * but ref utils were refactored in the next version. We keep this additional
- * export around for backwards compatibility.
- * @see https://github.com/palantir/blueprint/pull/4140
- */
-(Utils as any).getRef = getRef;
 
 export { Classes, Keys, Utils };
 // NOTE: Errors is not exported in public API
