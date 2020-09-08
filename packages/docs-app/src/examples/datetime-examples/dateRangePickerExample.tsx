ff7af00a507a570ec260ee46ba57e5fc2e99a9cb
ejose19 <8742215+ejose19@users.noreply.github.com>
2020-09-08 17:52:03 -0300

    [datetime] docs(DateRangePicker): fix example options (#4319)

--- packages/docs-app/src/examples/datetime-examples/dateRangePickerExample.tsx
+++ packages/docs-app/src/examples/datetime-examples/dateRangePickerExample.tsx
@@ -61,8 +61,12 @@ const MIN_DATE_OPTIONS: IDateOption[] = [
 const MAX_DATE_OPTIONS: IDateOption[] = [
     { label: "None", value: undefined },
     {
-        label: "1 month ago",
-        value: moment().add(-1, "months").toDate(),
+        label: "4 months from now",
+        value: moment().add(4, "months").toDate(),
+    },
+    {
+        label: "1 year from now",
+        value: moment().add(1, "years").toDate(),
     },
 ];
 
