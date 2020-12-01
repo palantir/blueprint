01dc6b9b6828340b361bf7cd26bbeee577e53488
cybae0804 <cybae0804@gmail.com>
2020-12-01 11:12:18 -0800

    Fixes classname not being propagated from multislider to handle bug

--- packages/core/src/components/slider/multiSlider.tsx
+++ packages/core/src/components/slider/multiSlider.tsx
@@ -294,12 +294,12 @@ export class MultiSlider extends AbstractPureComponent2<IMultiSliderProps, ISlid
             return null;
         }
 
-        return handleProps.map(({ value, type }, index) => (
+        return handleProps.map(({ value, type, className }, index) => (
             <Handle
                 className={classNames({
                     [Classes.START]: type === HandleType.START,
                     [Classes.END]: type === HandleType.END,
-                })}
+                }, className)}
                 disabled={disabled}
                 key={`${index}-${handleProps.length}`}
                 label={this.formatLabel(value)}
