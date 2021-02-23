87fa67711d4c678fa458c4207bb671e492e99690
Adi Dahiya <adi.dahiya14@gmail.com>
2021-02-23 16:39:01 +0000

    [docs-app] convert CollapseExample to use hooks

--- packages/docs-app/src/examples/core-examples/collapseExample.tsx
+++ packages/docs-app/src/examples/core-examples/collapseExample.tsx
@@ -14,59 +14,46 @@
  * limitations under the License.
  */
 
-import * as React from "react";
+import React, { useCallback, useState } from "react";
 
 import { Button, Collapse, H5, Pre, Switch } from "@blueprintjs/core";
 import { Example, handleBooleanChange, IExampleProps } from "@blueprintjs/docs-theme";
 
-export interface ICollapseExampleState {
-    isOpen: boolean;
-    keepChildrenMounted: boolean;
-}
+export const CollapseExample: React.FC<IExampleProps> = props => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [keepChildrenMounted, setKeepChildrenMounted] = useState(false);
+    const handleChildrenMountedChange = useCallback(handleBooleanChange(setKeepChildrenMounted), []);
+    const handleClick = useCallback(() => setIsOpen(!isOpen), [isOpen]);
 
-export class CollapseExample extends React.PureComponent<IExampleProps, ICollapseExampleState> {
-    public state: ICollapseExampleState = {
-        isOpen: false,
-        keepChildrenMounted: false,
-    };
+    const options = (
+        <>
+            <H5>Props</H5>
+            <Switch
+                checked={keepChildrenMounted}
+                label="Keep children mounted"
+                onChange={handleChildrenMountedChange}
+            />
+        </>
+    );
 
-    private handleChildrenMountedChange = handleBooleanChange(keepChildrenMounted => {
-        this.setState({ keepChildrenMounted });
-    });
-
-    public render() {
-        const options = (
-            <>
-                <H5>Props</H5>
-                <Switch
-                    checked={this.state.keepChildrenMounted}
-                    label="Keep children mounted"
-                    onChange={this.handleChildrenMountedChange}
-                />
-            </>
-        );
-
-        return (
-            <Example options={options} {...this.props}>
-                <div style={{ width: "100%", height: "100%", margin: 0 }}>
-                    <Button onClick={this.handleClick}>{this.state.isOpen ? "Hide" : "Show"} build logs</Button>
-                    <Collapse isOpen={this.state.isOpen} keepChildrenMounted={this.state.keepChildrenMounted}>
-                        <Pre>
-                            [11:53:30] Finished 'typescript-bundle-blueprint' after 769 ms
-                            <br />
-                            [11:53:30] Starting 'typescript-typings-blueprint'...
-                            <br />
-                            [11:53:30] Finished 'typescript-typings-blueprint' after 198 ms
-                            <br />
-                            [11:53:30] write ./blueprint.css
-                            <br />
-                            [11:53:30] Finished 'sass-compile-blueprint' after 2.84 s
-                        </Pre>
-                    </Collapse>
-                </div>
-            </Example>
-        );
-    }
-
-    private handleClick = () => this.setState({ isOpen: !this.state.isOpen });
-}
+    return (
+        <Example options={options} {...props}>
+            <div style={{ width: "100%", height: "100%", margin: 0 }}>
+                <Button onClick={handleClick}>{isOpen ? "Hide" : "Show"} build logs</Button>
+                <Collapse isOpen={isOpen} keepChildrenMounted={keepChildrenMounted}>
+                    <Pre>
+                        [11:53:30] Finished 'typescript-bundle-blueprint' after 769 ms
+                        <br />
+                        [11:53:30] Starting 'typescript-typings-blueprint'...
+                        <br />
+                        [11:53:30] Finished 'typescript-typings-blueprint' after 198 ms
+                        <br />
+                        [11:53:30] write ./blueprint.css
+                        <br />
+                        [11:53:30] Finished 'sass-compile-blueprint' after 2.84 s
+                    </Pre>
+                </Collapse>
+            </div>
+        </Example>
+    );
+};
