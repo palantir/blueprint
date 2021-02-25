a11f3557310a09557e18edbdf5ea0c8f331d3eff
Adi Dahiya <adahiya@palantir.com>
2021-02-25 14:07:04 -0500

    remove extraneous line change

--- packages/core/src/context/hotkeys/hotkeysProvider.tsx
+++ packages/core/src/context/hotkeys/hotkeysProvider.tsx
@@ -16,7 +16,6 @@
 
 import React, { createContext, useReducer, Dispatch, useCallback } from "react";
 
-import { DISPLAYNAME_PREFIX } from "../../common";
 import { HotkeysDialog2, HotkeysDialog2Props } from "../../components/hotkeys/hotkeysDialog2";
 import { HotkeyConfig } from "../../hooks";
 
@@ -40,7 +39,6 @@ export const HotkeysContext = createContext?.<[HotkeysContextState, Dispatch<Hot
     initialHotkeysState,
     noOpDispatch,
 ]);
-HotkeysContext.displayName = `${DISPLAYNAME_PREFIX}.HotkeysContext`;
 
 const hotkeysReducer = (state: HotkeysContextState, action: HotkeysAction) => {
     switch (action.type) {
