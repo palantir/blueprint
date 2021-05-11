878b042b67e25a7373dd9d5ee1f5b8449fd29ca7
Adi Dahiya <adi.dahiya14@gmail.com>
2021-05-11 22:33:07 +0000

    use HTMLAttributes like other BP APIs

--- packages/popover2/src/contextMenu2.tsx
+++ packages/popover2/src/contextMenu2.tsx
@@ -75,7 +75,7 @@ export interface ContextMenu2ChildrenProps {
 }
 
 export interface ContextMenu2Props
-    extends Omit<React.DOMAttributes<HTMLElement>, "children" | "className" | "onContextMenu">,
+    extends Omit<React.HTMLAttributes<HTMLElement>, "children" | "className" | "onContextMenu">,
         Props {
     /**
      * Menu content. This will usually be a Blueprint `<Menu>` component.
