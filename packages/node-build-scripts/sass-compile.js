9aa10ede56427bae8d5314e861a97f9e67cd97d9
Ian Fisher <ian.p.fisher@gmail.com>
2021-08-30 11:48:35 -0400

    readd file removed in error

--- packages/node-build-scripts/sass-compile.js
+++ packages/node-build-scripts/sass-compile.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-/** Copyright 2020 Palantir Technologies, Inc. All rights reserved.
+/** Copyright 2021 Palantir Technologies, Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
