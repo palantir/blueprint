1dd972acd717e9b6bfac86518d3bc6aa0bb6ff21
John JY Lee <johnlee@palantir.com>
2021-09-15 13:53:10 -0400

    Support outputFileName and retainDefault options

--- packages/node-build-scripts/generate-css-variables.js
+++ packages/node-build-scripts/generate-css-variables.js
@@ -8,12 +8,28 @@
 const fs = require("fs");
 const path = require("path");
 const stripCssComments = require("strip-css-comments");
+const yargs = require("yargs/yargs");
+
 const { COPYRIGHT_HEADER } = require("./constants");
 
 const SRC_DIR = path.resolve(process.cwd(), "./src");
 const DEST_DIR = path.resolve(process.cwd(), "./lib");
 
-const variablesSources = process.argv.slice(2);
+const args = yargs(process.argv.slice(2))
+    .option("outputFileName", {
+        alias: "o",
+        default: "variables",
+        type: "string",
+    })
+    .option("retainDefault", {
+        default: false,
+        type: "boolean",
+    }).argv;
+
+// @ts-ignore
+const variablesSources = args._;
+
+const outputFileName = args["outputFileName"];
 
 // concatenate sources
 let variablesScss = variablesSources.reduce((str, currentFilename) => {
@@ -23,16 +39,18 @@ let variablesScss = variablesSources.reduce((str, currentFilename) => {
 // strip comments, clean up for consumption
 variablesScss = stripCssComments(variablesScss);
 variablesScss = variablesScss
-    .replace(/\ \!default/g, "")
     .replace(/(@import|\/\/).*\n+/g, "")
     .replace(/border-shadow\((.+)\)/g, "0 0 0 1px rgba($black, $1)")
     .replace(/\n{3,}/g, "\n\n");
+if (!args["retainDefault"]) {
+    variablesScss = variablesScss.replace(/\ \!default/g, "");
+}
 variablesScss = COPYRIGHT_HEADER + "\n" + variablesScss;
 
 if (!fs.existsSync(`${DEST_DIR}/scss`)) {
     fs.mkdirSync(`${DEST_DIR}/scss`);
 }
-fs.writeFileSync(`${DEST_DIR}/scss/variables.scss`, variablesScss);
+fs.writeFileSync(`${DEST_DIR}/scss/${outputFileName}.scss`, variablesScss);
 
 // convert scss to less
 const variablesLess = variablesScss
@@ -43,4 +61,4 @@ const variablesLess = variablesScss
 if (!fs.existsSync(`${DEST_DIR}/less`)) {
     fs.mkdirSync(`${DEST_DIR}/less`);
 }
-fs.writeFileSync(`${DEST_DIR}/less/variables.less`, variablesLess);
+fs.writeFileSync(`${DEST_DIR}/less/${outputFileName}.less`, variablesLess);
