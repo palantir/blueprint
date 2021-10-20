cc3f27944e496935f1e676009f91084ec4dff108
Mac Lockard <maclockard@gmail.com>
2021-10-20 09:57:12 -0700

    Readonly select items prop

--- packages/select/src/common/itemListRenderer.ts
+++ packages/select/src/common/itemListRenderer.ts
@@ -37,13 +37,13 @@ export interface IItemListRendererProps<T> {
      * map each item in this array through `renderItem`, with support for
      * optional `noResults` and `initialContent` states.
      */
-    filteredItems: T[];
+    filteredItems: readonly T[];
 
     /**
      * Array of all items in the list.
      * See `filteredItems` for a filtered array based on `query` and predicate props.
      */
-    items: T[];
+    items: readonly T[];
 
     /**
      * The current query string.
