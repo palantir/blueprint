f592f49f5a04c8c902f031e3f3871eb5eac9d8f5
Levan Kvirkvelia <lekvirkvelia@gmail.com>
2021-11-07 17:04:35 +0300

    Update icon.tsx

--- packages/core/src/components/icon/icon.tsx
+++ packages/core/src/components/icon/icon.tsx
@@ -21,7 +21,7 @@ import { IconName, IconSvgPaths16, IconSvgPaths20, iconNameToPathsRecordKey } fr
 
 import { AbstractPureComponent2, Classes, DISPLAYNAME_PREFIX, IntentProps, Props, MaybeElement } from "../../common";
 
-export type { IconName }
+export type { IconName };
 
 export enum IconSize {
     STANDARD = 16,
