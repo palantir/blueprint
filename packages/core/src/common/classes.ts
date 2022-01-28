57b53e5bc01e6c046dffc3a0588f2176b2afa301
Justin Hopper <29075873+justinbhopper@users.noreply.github.com>
2022-01-28 13:15:01 -0600

    feat: Add popper modifier classnames (#5109)

--- packages/core/src/common/classes.ts
+++ packages/core/src/common/classes.ts
@@ -244,6 +244,7 @@ export const POPOVER_CONTENT_SIZING = `${POPOVER_CONTENT}-sizing`;
 export const POPOVER_DISMISS = `${POPOVER}-dismiss`;
 export const POPOVER_DISMISS_OVERRIDE = `${POPOVER_DISMISS}-override`;
 export const POPOVER_OPEN = `${POPOVER}-open`;
+export const POPOVER_OUT_OF_BOUNDARIES = `${POPOVER}-out-of-boundaries`;
 export const POPOVER_TARGET = `${POPOVER}-target`;
 export const POPOVER_WRAPPER = `${POPOVER}-wrapper`;
 export const TRANSITION_CONTAINER = `${NS}-transition-container`;
