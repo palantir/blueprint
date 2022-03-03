f95e3380e0ac382b6d427ac9c843d39630dc6240
Adi Dahiya <adahiya@palantir.com>
2022-03-03 12:36:46 -0500

    [v4] [core] deprecate ContextMenu.show() (#5156)

--- packages/core/src/components/context-menu/contextMenu.tsx
+++ packages/core/src/components/context-menu/contextMenu.tsx
@@ -133,6 +133,8 @@ let contextMenu: ContextMenu | undefined;
  * Show the given menu element at the given offset from the top-left corner of the viewport.
  * The menu will appear below-right of this point and will flip to below-left if there is not enough
  * room onscreen. The optional callback will be invoked when this menu closes.
+ *
+ * @deprecated use ContextMenu2
  */
 export function show(menu: JSX.Element, offset: IOffset, onClose?: () => void, isDarkTheme?: boolean) {
     if (contextMenuElement === undefined) {
