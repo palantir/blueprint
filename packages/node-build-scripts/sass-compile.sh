98b6b51d6994949bb7faed8a03d56b4d92b33909
Peter Goldberg <pgoldberg@palantir.com>
2022-04-01 08:31:58 +0000

    migrate to dart-sass

--- packages/node-build-scripts/sass-compile.sh
+++ packages/node-build-scripts/sass-compile.sh
@@ -11,11 +11,8 @@ OUTPUT="${OUTPUT:-lib/css/}"
 # dependencies are hoisted to root node_modules, so load packages from there
 ROOT_NM=../../node_modules
 
-$ROOT_NM/.bin/node-sass-chokidar \
-  --importer $ROOT_NM/node-sass-package-importer/dist/cli.js \
-  --output $OUTPUT \
-  --source-map true \
-  $@
+# the `dart-sass` CLI doesn't support custom functions or importers, but the JS API does, so delegate to node
+$ROOT_NM/.bin/ts-node -O "{ \"esModuleInterop\": true }" ../node-build-scripts/sass-compile.ts --output $OUTPUT $@
 
 # in source maps, paths to blueprint packages should be direct, rather than
 # going through node_modules. https://github.com/palantir/blueprint/issues/3500
