96ed994adc6b948cd7da7970db22734af2f3f628
Adi Dahiya <adahiya@palantir.com>
2022-04-26 09:43:12 -0400

    chore: merge "modern" CSS overrides into component styles (#5269)

--- packages/node-build-scripts/sass-compile.ts
+++ packages/node-build-scripts/sass-compile.ts
@@ -1,3 +1,4 @@
+import { watch } from "chokidar";
 import fs from "fs-extra";
 import path from "path";
 import * as sass from "sass";
@@ -11,6 +12,7 @@ const args = yargs(process.argv.slice(2))
         description: "Path to file with exported custom sass functions",
     })
     .option("output", { alias: "o", type: "string", description: "Output folder" })
+    .option("watch", { alias: "w", type: "boolean", description: "Watch mode" })
     .check(argv => {
         const hasOneStringArgument = argv._.length === 1 && typeof argv._[0] === "string";
         return hasOneStringArgument;
@@ -18,17 +20,45 @@ const args = yargs(process.argv.slice(2))
     .parseSync();
 
 const functions = args.functions != null ? require(path.resolve(args.functions)) : undefined;
-const files = fs.readdirSync(args._[0]);
-const inFiles = files
-    .filter(file => path.extname(file) === ".scss" && !path.basename(file).startsWith("_"))
-    .map(fileName => path.join(args._[0] as string, fileName));
 
-for (const inFile of inFiles) {
-    const outFile = path.join(args.output, `${path.parse(inFile).name}.css`);
+if (args.watch) {
+    compileAllFiles();
+
+    const folderToWatch = path.resolve(args._[0] as string);
+    console.info(`[sass-compile] Watching ${folderToWatch} for changes...`);
+
+    const watcher = watch([`${folderToWatch}/*.scss`, `${folderToWatch}/**/*.scss`], { persistent: true });
+    watcher.on("change", fileName => {
+        console.info(`[sass-compile] Detected change in ${fileName}, re-compiling.`);
+
+        if (path.basename(fileName).startsWith("_")) {
+            compileAllFiles();
+        } else {
+            compileFile(fileName);
+        }
+    });
+} else {
+    compileAllFiles();
+}
+
+function compileAllFiles() {
+    const files = fs.readdirSync(args._[0]);
+    const inputFiles = files
+        .filter(file => path.extname(file) === ".scss" && !path.basename(file).startsWith("_"))
+        .map(fileName => path.join(args._[0] as string, fileName));
+
+    for (const inputFile of inputFiles) {
+        compileFile(inputFile);
+    }
+    console.info("[sass-compile] Finished compiling all input .scss files.");
+}
+
+function compileFile(inputFile: string) {
+    const outFile = path.join(args.output, `${path.parse(inputFile).name}.css`);
     const outputMapFile = `${outFile}.map`;
     // use deprecated `renderSync` because it supports legacy importers and functions
     const result = sass.renderSync({
-        file: inFile,
+        file: inputFile,
         importer: nodeSassPackageImporter(),
         sourceMap: true,
         outFile,
