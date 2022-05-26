7b6d16a84384610ae9bace4f390c731cafd82abf
bvandercar-vt <87083504+bvandercar-vt@users.noreply.github.com>
2022-05-26 15:54:22 -0600

    [core] fix(Menu): add a11y roles and aria attributes (#5326)

--- packages/core/src/components/menu/menu.tsx
+++ packages/core/src/components/menu/menu.tsx
@@ -41,7 +41,7 @@ export class Menu extends AbstractPureComponent2<MenuProps> {
         const { className, children, large, ulRef, ...htmlProps } = this.props;
         const classes = classNames(Classes.MENU, { [Classes.LARGE]: large }, className);
         return (
-            <ul {...htmlProps} className={classes} ref={ulRef}>
+            <ul role="menu" {...htmlProps} className={classes} ref={ulRef}>
                 {children}
             </ul>
         );
