f3e5d0838226b9e855fbce1708bde9d89569e470
blake.vandercar <blake.vandercar@validatek.com>
2022-06-06 18:10:51 -0600

    add aria-autocmplete because user can type in before selecting

--- packages/select/src/components/multi-select/multiSelect2.tsx
+++ packages/select/src/components/multi-select/multiSelect2.tsx
@@ -157,7 +157,7 @@ export class MultiSelect2<T> extends AbstractPureComponent2<MultiSelect2Props<T>
         return (
             <this.TypedQueryList
                 {...restProps}
-                menuProps={{ id: this.listboxId }}
+                menuProps={{ "aria-multiselectable": true, id: this.listboxId }}
                 onItemSelect={this.handleItemSelect}
                 onQueryChange={this.handleQueryChange}
                 ref={this.refHandlers.queryList}
@@ -241,6 +241,7 @@ export class MultiSelect2<T> extends AbstractPureComponent2<MultiSelect2Props<T>
             };
             return (
                 <div
+                    aria-autocomplete="list"
                     aria-controls={this.listboxId}
                     {...popoverTargetProps}
                     {...targetProps}
