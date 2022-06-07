d9d20dc73dc598ea5c1220a6f42acc8654b60ef9
bvandercar-vt <87083504+bvandercar-vt@users.noreply.github.com>
2022-06-07 07:26:12 -0600

    [datetime] fix(a11y): add aria-labels for various controls (#5358)
    Co-authored-by: Adi Dahiya <adi.dahiya14@gmail.com>

--- packages/core/src/components/html-select/htmlSelect.tsx
+++ packages/core/src/components/html-select/htmlSelect.tsx
@@ -91,7 +91,14 @@ export class HTMLSelect extends AbstractPureComponent2<HTMLSelectProps> {
 
         const optionChildren = options.map(option => {
             const props: OptionProps = typeof option === "object" ? option : { value: option };
-            return <option {...props} key={props.value} children={props.label || props.value} />;
+            return (
+                <option
+                    {...props}
+                    key={props.value}
+                    children={props.label || props.value}
+                    selected={this.props.value === props.value}
+                />
+            );
         });
 
         return (
