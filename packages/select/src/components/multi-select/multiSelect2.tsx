c5a02aacab1bc4f79e7e33629ba99f94e1a78726
Adi Dahiya <adi.dahiya14@gmail.com>
2022-06-15 10:02:59 -0400

    Apply suggestions from code review

--- packages/select/src/components/multi-select/multiSelect2.tsx
+++ packages/select/src/components/multi-select/multiSelect2.tsx
@@ -251,13 +251,15 @@ export class MultiSelect2<T> extends AbstractPureComponent2<MultiSelect2Props<T>
 
             const maybeClearButton =
                 onClear !== undefined && selectedItems.length > 0 ? (
+                    // use both aria-label and title a11y attributes here, for screen readers
+                    // and mouseover interactions respectively
                     <Button
-                        aria-label="Clear values"
+                        aria-label="Clear selected items"
                         disabled={disabled}
                         icon="cross"
                         minimal={true}
                         onClick={this.handleClearButtonClick}
-                        title="Clear values"
+                        title="Clear selected items"
                     />
                 ) : undefined;
 
