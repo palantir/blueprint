66cc39efe929895cf59c6c702d3f977c9f9dcaf7
Adi Dahiya <adahiya@palantir.com>
2022-07-19 22:12:29 -0400

    tooltips on prop options

--- packages/docs-app/src/examples/datetime2-examples/dateInput2Example.tsx
+++ packages/docs-app/src/examples/datetime2-examples/dateInput2Example.tsx
@@ -17,10 +17,11 @@
 import classNames from "classnames";
 import * as React from "react";
 
-import { Classes, H5, Icon, Switch } from "@blueprintjs/core";
+import { Classes, Code, H5, Icon, Switch, Tag } from "@blueprintjs/core";
 import { DateFormatProps, TimePrecision } from "@blueprintjs/datetime";
 import { DateInput2 } from "@blueprintjs/datetime2";
 import { Example, handleBooleanChange, handleValueChange, IExampleProps } from "@blueprintjs/docs-theme";
+import { Tooltip2 } from "@blueprintjs/popover2";
 
 import { PrecisionSelect } from "../datetime-examples/common/precisionSelect";
 import { DATE_FNS_FORMATS, DateFnsFormatSelector } from "./dateFnsFormatSelector";
@@ -106,7 +107,7 @@ export class DateInput2Example extends React.PureComponent<IExampleProps, DateIn
                     }
                     value={date}
                 />
-                {date}
+                {date == null ? <Tag minimal={true}>no date</Tag> : <Tag intent="primary">{date}</Tag>}
             </Example>
         );
     }
@@ -126,22 +127,35 @@ export class DateInput2Example extends React.PureComponent<IExampleProps, DateIn
             showTimezoneSelect,
             timePrecision,
         } = this.state;
+
         return (
             <>
                 <H5>Props</H5>
-                <Switch label="Close on selection" checked={closeOnSelection} onChange={this.toggleSelection} />
-                <Switch
-                    checked={shortcuts}
-                    disabled={showActionsBar}
-                    label="Show shortcuts"
-                    onChange={this.toggleShortcuts}
-                />
-                <Switch
-                    checked={showActionsBar}
-                    disabled={shortcuts}
-                    label="Show actions bar"
-                    onChange={this.toggleActionsBar}
-                />
+                <Tooltip2
+                    content={<Code>closeOnSelection=&#123;{closeOnSelection.toString()}&#125;</Code>}
+                    placement="left"
+                >
+                    <Switch label="Close on selection" checked={closeOnSelection} onChange={this.toggleSelection} />
+                </Tooltip2>
+                <Tooltip2 content={<Code>shortcuts=&#123;{shortcuts.toString()}&#125;</Code>} placement="left">
+                    <Switch
+                        checked={shortcuts}
+                        disabled={showActionsBar}
+                        label="Show shortcuts"
+                        onChange={this.toggleShortcuts}
+                    />
+                </Tooltip2>
+                <Tooltip2
+                    content={<Code>showActionsBar=&#123;{showActionsBar.toString()}&#125;</Code>}
+                    placement="left"
+                >
+                    <Switch
+                        checked={showActionsBar}
+                        disabled={shortcuts}
+                        label="Show actions bar"
+                        onChange={this.toggleActionsBar}
+                    />
+                </Tooltip2>
                 <PrecisionSelect
                     allowNone={true}
                     label="Time precision"
@@ -152,31 +166,59 @@ export class DateInput2Example extends React.PureComponent<IExampleProps, DateIn
                 <H5>Appearance props</H5>
                 <Switch label="Disabled" checked={disabled} onChange={this.toggleDisabled} />
                 <Switch label="Fill" checked={fill} onChange={this.toggleFill} />
-                <Switch label="Reverse month and year menus" checked={reverse} onChange={this.toggleReverseMenus} />
-                <Switch
-                    label="Show time picker arrows"
-                    checked={showTimePickerArrows}
+                <Tooltip2
+                    content={<Code>reverseMonthAndYearMenus=&#123;{reverse.toString()}&#125;</Code>}
+                    placement="left"
+                >
+                    <Switch label="Reverse month and year menus" checked={reverse} onChange={this.toggleReverseMenus} />
+                </Tooltip2>
+                <Tooltip2
+                    content={
+                        <Code>
+                            timePickerProps=&#123;&#123; showArrowButtons: {showTimePickerArrows.toString()}{" "}
+                            &#125;&#125;
+                        </Code>
+                    }
                     disabled={timePrecision === undefined}
-                    onChange={this.toggleTimePickerArrows}
-                />
+                    placement="left"
+                >
+                    <Switch
+                        label="Show time picker arrows"
+                        checked={showTimePickerArrows}
+                        disabled={timePrecision === undefined}
+                        onChange={this.toggleTimePickerArrows}
+                    />
+                </Tooltip2>
                 <Switch label="Show right element" checked={showRightElement} onChange={this.toggleRightElement} />
                 <DateFnsFormatSelector format={format} onChange={this.handleFormatChange} />
 
                 <H5 className={classNames({ [Classes.TEXT_DISABLED]: timePrecision === undefined })}>
                     TimezoneSelect props
                 </H5>
-                <Switch
-                    label="Disable timezone select"
-                    checked={disableTimezoneSelect}
+                <Tooltip2
+                    content={<Code>disableTimezoneSelect=&#123;{disableTimezoneSelect.toString()}&#125;</Code>}
                     disabled={timePrecision === undefined}
-                    onChange={this.toggleDisableTimezoneSelect}
-                />
-                <Switch
-                    label="Show timezone select"
-                    checked={showTimezoneSelect}
+                    placement="left"
+                >
+                    <Switch
+                        label="Disable timezone select"
+                        checked={disableTimezoneSelect}
+                        disabled={timePrecision === undefined}
+                        onChange={this.toggleDisableTimezoneSelect}
+                    />
+                </Tooltip2>
+                <Tooltip2
+                    content={<Code>showTimezoneSelect=&#123;{showTimezoneSelect.toString()}&#125;</Code>}
                     disabled={timePrecision === undefined}
-                    onChange={this.toggleShowTimezoneSelect}
-                />
+                    placement="left"
+                >
+                    <Switch
+                        label="Show timezone select"
+                        checked={showTimezoneSelect}
+                        disabled={timePrecision === undefined}
+                        onChange={this.toggleShowTimezoneSelect}
+                    />
+                </Tooltip2>
             </>
         );
     }
