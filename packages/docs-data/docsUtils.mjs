0dfc2480dc38a91aae6cbccf608985e85ac6576f
Adi Dahiya <adahiya@palantir.com>
2022-10-06 10:06:49 -0400

    [docs-data] chore: migrate scripts to ESM

--- packages/docs-data/docsUtils.mjs
+++ packages/docs-data/docsUtils.mjs
@@ -2,23 +2,29 @@
  * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
  */
 
-const Highlights = require("highlights");
-const { marked } = require("marked");
+// @ts-check
+
+import Highlights from "highlights";
+import { marked } from "marked";
+import { createRequire } from "module";
 
 const DEFAULT_SCOPE = "source.tsx";
 const HIGHLIGHTS_LANGUAGES = ["better-handlebars", "language-less", "tree-sitter-typescript"];
 
-// Highlights configuration (https://github.com/atom/highlights)
+// import.meta.resolve is still experimental under a CLI flag, so we create a require fn instead
+// see https://nodejs.org/docs/latest-v16.x/api/esm.html#importmetaresolvespecifier-parent
+const require = createRequire(import.meta.url);
 
+// Highlights configuration (https://github.com/atom/highlights)
 const highlighter = new Highlights();
-// a few additional languages... highlights comes with a basic set.
+// add a few additional languages... highlights comes with a basic set.
 for (const lang of HIGHLIGHTS_LANGUAGES) {
     highlighter.requireGrammarsSync({ modulePath: require.resolve(`${lang}/package.json`) });
 }
 
 // highlights the given text in the given language scope. returns HTML string wrapped in <pre> tag.
 // must provide full TextMate language scope: "text.html.basic"
-function highlight(fileContents, scopeName = DEFAULT_SCOPE) {
+export function highlight(fileContents, scopeName = DEFAULT_SCOPE) {
     if (fileContents) {
         return highlighter.highlightSync({ fileContents, scopeName });
     }
@@ -27,8 +33,8 @@ function highlight(fileContents, scopeName = DEFAULT_SCOPE) {
 // Marked configuration (https://github.com/chjj/marked)
 
 // custom renderer lets us change tag semantics
-const markedRenderer = new marked.Renderer();
-markedRenderer.code = (textContent, language) => {
+const renderer = new marked.Renderer();
+renderer.code = (textContent, language) => {
     // massage markdown language hint into TM language scope
     if (language === "html") {
         language = "text.html.handlebars";
@@ -39,12 +45,8 @@ markedRenderer.code = (textContent, language) => {
     return highlight(textContent, language);
 };
 
-module.exports = {
-    highlight,
+// render the given text as markdown, using the custom rendering logic above.
+// code blocks are highlighted using highlight() above.
+const markdown = textContent => marked(textContent, { renderer });
 
-    // render the given text as markdown, using the custom rendering logic above.
-    // code blocks are highlighted using highlight() above.
-    markdown: textContent => marked(textContent, { markedRenderer }),
-
-    markedRenderer,
-};
+export { markdown, renderer as markedRenderer };
