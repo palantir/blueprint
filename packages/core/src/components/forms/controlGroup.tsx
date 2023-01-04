021c7ebf5b45b91466b5565d2dc01e6960cc62e3
Adi Dahiya <adahiya@palantir.com>
2023-01-03 19:10:11 -0500

    feat: add elementRef prop to various components

--- packages/core/src/components/forms/controlGroup.tsx
+++ packages/core/src/components/forms/controlGroup.tsx
@@ -18,12 +18,13 @@ import classNames from "classnames";
 import * as React from "react";
 
 import { AbstractPureComponent2, Classes } from "../../common";
-import { DISPLAYNAME_PREFIX, HTMLDivProps, Props } from "../../common/props";
+import { DISPLAYNAME_PREFIX, HTMLDivProps, IElementRefProps, Props } from "../../common/props";
 
 // eslint-disable-next-line deprecation/deprecation
 export type ControlGroupProps = IControlGroupProps;
 /** @deprecated use ControlGroupProps */
-export interface IControlGroupProps extends Props, HTMLDivProps {
+// eslint-disable-next-line deprecation/deprecation
+export interface IControlGroupProps extends Props, HTMLDivProps, IElementRefProps<HTMLDivElement> {
     /** Group contents. */
     children?: React.ReactNode;
 
@@ -53,7 +54,7 @@ export class ControlGroup extends AbstractPureComponent2<ControlGroupProps> {
     public static displayName = `${DISPLAYNAME_PREFIX}.ControlGroup`;
 
     public render() {
-        const { children, className, fill, vertical, ...htmlProps } = this.props;
+        const { children, className, elementRef, fill, vertical, ...htmlProps } = this.props;
 
         const rootClasses = classNames(
             Classes.CONTROL_GROUP,
@@ -65,7 +66,7 @@ export class ControlGroup extends AbstractPureComponent2<ControlGroupProps> {
         );
 
         return (
-            <div {...htmlProps} className={rootClasses}>
+            <div {...htmlProps} className={rootClasses} ref={elementRef}>
                 {children}
             </div>
         );
