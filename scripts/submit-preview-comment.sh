031158e561a22ddbc3691b8ff9f330c0fccf5a5a
Adi Dahiya <adahiya@palantir.com>
2023-01-06 09:53:51 -0500

    fix script file name

--- scripts/submit-preview-comment.sh
+++ scripts/submit-preview-comment.sh
@@ -2,6 +2,11 @@
 
 # Submit Github comment with links to built artifacts
 
+if [ -z "${CIRCLE_BUILD_NUM}" ]; then
+    echo "Not on CircleCI, refusing to run script."
+    exit 1
+fi
+
 if [ -z "${CIRCLE_API_TOKEN}" ]; then
     echo "No CircleCI API token available to query for artifact asset URLs from thsi build. Check the \$CIRCLE_API_TOKEN environment variable."
     exit 1
