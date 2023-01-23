8d793f6c229cf372b11b0909573f69a35692e161
bvandercar-vt <87083504+bvandercar-vt@users.noreply.github.com>
2023-01-23 13:45:55 -0700

    [popover2] feat: new prop targetProps (#5802)
    Co-authored-by: Adi Dahiya <adi.dahiya14@gmail.com>
    Co-authored-by: Adi Dahiya <adahiya@palantir.com>

--- packages/popover2/src/popover2SharedProps.ts
+++ packages/popover2/src/popover2SharedProps.ts
@@ -61,7 +61,8 @@ export type Popover2TargetProps = IPopover2TargetProps;
 /**
  * @deprecated use Popover2TargetProps
  */
-export interface IPopover2TargetProps {
+export interface IPopover2TargetProps
+    extends Pick<React.HTMLAttributes<HTMLElement>, "aria-haspopup" | "className" | "tabIndex"> {
     /** Target ref. */
     ref: React.Ref<any>;
 
@@ -69,8 +70,22 @@ export interface IPopover2TargetProps {
     isOpen: boolean;
 }
 
+/**
+ * Event handlers injected by Popover2 for hover interaction popovers.
+ */
+export type Popover2HoverTargetHandlers<
+    TProps extends DefaultPopover2TargetHTMLProps = DefaultPopover2TargetHTMLProps,
+> = Pick<TProps, "onBlur" | "onContextMenu" | "onFocus" | "onMouseEnter" | "onMouseLeave">;
+
+/**
+ * Event handlers injected by Popover2 for click interaction popovers.
+ */
+export type Popover2ClickTargetHandlers<
+    TProps extends DefaultPopover2TargetHTMLProps = DefaultPopover2TargetHTMLProps,
+> = Pick<TProps, "onClick" | "onKeyDown">;
+
 // eslint-disable-next-line deprecation/deprecation
-export type Popover2SharedProps<T> = IPopover2SharedProps<T>;
+export type Popover2SharedProps<T extends DefaultPopover2TargetHTMLProps> = IPopover2SharedProps<T>;
 /**
  * Props shared between `Popover2` and `Tooltip2`.
  *
@@ -78,7 +93,7 @@ export type Popover2SharedProps<T> = IPopover2SharedProps<T>;
  *                  defaults to props for HTMLElement in IPopover2Props and ITooltip2Props
  * @deprecated use Popover2SharedProps
  */
-export interface IPopover2SharedProps<TProps> extends OverlayableProps, Props {
+export interface IPopover2SharedProps<TProps extends DefaultPopover2TargetHTMLProps> extends OverlayableProps, Props {
     /** Interactive element which will trigger the popover. */
     children?: React.ReactNode;
 
@@ -223,7 +238,9 @@ export interface IPopover2SharedProps<TProps> extends OverlayableProps, Props {
      *
      * Mutually exclusive with `children` and `targetTagName` props.
      */
-    renderTarget?: (props: Popover2TargetProps & TProps) => JSX.Element;
+    renderTarget?: (
+        props: Popover2TargetProps & (Popover2HoverTargetHandlers<TProps> | Popover2ClickTargetHandlers<TProps>),
+    ) => JSX.Element;
 
     /**
      * A root boundary element supplied to the "flip" and "preventOverflow" modifiers.
@@ -282,6 +299,15 @@ export interface IPopover2SharedProps<TProps> extends OverlayableProps, Props {
      * @default "span" ("div" if `fill={true}`)
      */
     targetTagName?: keyof JSX.IntrinsicElements;
+
+    /**
+     * HTML props for the target element. This is useful in some cases where you
+     * need to render some simple attributes on the generated target element.
+     *
+     * For more complex use cases, consider using the `renderTarget` API instead.
+     * This prop will be ignored if `renderTarget` is used.
+     */
+    targetProps?: TProps;
 
     /**
      * Whether the popover should be rendered inside a `Portal` attached to
