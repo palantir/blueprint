e2abc83046272bdeca8d14048e4b671553dea439
Adi Dahiya <adahiya@palantir.com>
2023-01-24 10:27:46 -0500

    Gracefully exit if no CIRCLE_API_TOKEN variable

--- scripts/submit-preview-comment.sh
+++ scripts/submit-preview-comment.sh
@@ -19,8 +19,10 @@ if [ -z "${CIRCLE_BUILD_NUM}" ]; then
 fi
 
 if [ -z "${CIRCLE_API_TOKEN}" ]; then
-    echo "No CircleCI API token available to query for artifact asset URLs from thsi build. Check the \$CIRCLE_API_TOKEN environment variable."
-    exit 1
+    echo "No CircleCI API token available to query for artifact asset URLs from this build."
+    echo " --> If this is a build on a fork of the main repo, this is expected behavior. You can view artifact URLs through the CircleCI job web UI."
+    echo " --> If this is a build on the main repo, something is wrong: check the \$CIRCLE_API_TOKEN environment variable."
+    exit 0
 fi
 
 SCRIPTS_DIR=$(dirname "$(readlink -f "$0")")
