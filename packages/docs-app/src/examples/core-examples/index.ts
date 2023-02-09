b91f8c53e86325fa42f9fc8c9b220926b9e2a999
Adi Dahiya <adahiya@palantir.com>
2023-02-09 13:47:26 -0500

    [popover2] feat: ContextMenu2Popover, show/hide imperative APIs

--- packages/docs-app/src/examples/core-examples/index.ts
+++ packages/docs-app/src/examples/core-examples/index.ts
@@ -26,7 +26,6 @@ export * from "./collapseExample";
 export * from "./cardExample";
 export * from "./collapsibleListExample";
 export * from "./contextMenuExample";
-export * from "./contextMenu2Example";
 export * from "./controlGroupExample";
 export * from "./dialogExample";
 export * from "./multistepDialogExample";
