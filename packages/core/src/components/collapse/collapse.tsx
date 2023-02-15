palantir/blueprint

commit 45d307f573068de6e8223e21915c205f30537806

[core] fix(Collapse): set 'aria-hidden' attr correctly (#5926)

packages/core/src/components/collapse/collapse.tsx
@@ -208,7 +208,7 @@ export class Collapse extends AbstractPureComponent2<CollapseProps, ICollapseSta
                 className={Classes.COLLAPSE_BODY}
                 ref={this.contentsRefHandler}
                 style={contentsStyle}
-                aria-hidden={!isContentVisible && this.props.keepChildrenMounted}
+                aria-hidden={!shouldRenderChildren}
             >
                 {shouldRenderChildren ? this.props.children : null}
             </div>,

commit 293f76171eaa24041ab74bdae9e0cc01c8c41da4

[core] fix(Collapse): fix `aria-hidden`
This fixes regression introduced in #5926 where if `keepChildrenMounted` is set, `aria-hidden` no longer updates correctly.

packages/core/src/components/collapse/collapse.tsx
@@ -208,7 +208,7 @@ export class Collapse extends AbstractPureComponent2<CollapseProps, ICollapseSta
                 className={Classes.COLLAPSE_BODY}
                 ref={this.contentsRefHandler}
                 style={contentsStyle}
-                aria-hidden={!shouldRenderChildren}
+                aria-hidden={!isContentVisible}
             >
                 {shouldRenderChildren ? this.props.children : null}
             </div>,
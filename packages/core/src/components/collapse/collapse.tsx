293f76171eaa24041ab74bdae9e0cc01c8c41da4
shim-flounce <121665773+shim-flounce@users.noreply.github.com>
2023-02-15 15:06:19 +0000

    [core] fix(Collapse): fix `aria-hidden`
    This fixes regression introduced in #5926 where if `keepChildrenMounted` is set, `aria-hidden` no longer updates correctly.

--- packages/core/src/components/collapse/collapse.tsx
+++ packages/core/src/components/collapse/collapse.tsx
@@ -208,7 +208,7 @@ export class Collapse extends AbstractPureComponent2<CollapseProps, ICollapseSta
                 className={Classes.COLLAPSE_BODY}
                 ref={this.contentsRefHandler}
                 style={contentsStyle}
-                aria-hidden={!shouldRenderChildren}
+                aria-hidden={!isContentVisible}
             >
                 {shouldRenderChildren ? this.props.children : null}
             </div>,
