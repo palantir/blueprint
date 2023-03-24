c080e51ab1aea16150dad1ced83172eaca0cf092
Adi Dahiya <adahiya@palantir.com>
2023-03-24 09:51:52 -0400

    [node-build-scripts] fix(sass-compile): relative path to mjs script (#6036)

--- packages/node-build-scripts/sass-compile.sh
+++ packages/node-build-scripts/sass-compile.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+set -e
+set -o pipefail
+
 if [ $# -eq 0 ]; then
   echo "Usage: [OUTPUT=dir] sass-compile <directory> [...args]"
   exit 1
 fi
 
+node_build_scripts_dir="$( dirname "$(readlink -f "$0")" )"
+
 # set OUTPUT env varible to change output directory
 OUTPUT="${OUTPUT:-lib/css/}"
 
 # the `dart-sass` CLI doesn't support custom functions or importers, but the JS API does, so delegate to node
-node ../node-build-scripts/sass-compile.mjs --output $OUTPUT $@
+node $node_build_scripts_dir/sass-compile.mjs --output $OUTPUT $@
