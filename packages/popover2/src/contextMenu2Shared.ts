ac8ceede4dd20b8ee89d8b34dd2ab766e774e037
Adi Dahiya <adahiya@palantir.com>
2023-04-19 12:43:53 -0400

    [popover2] feat(ContextMenu2): allow popover placement override (#6078)

--- packages/popover2/src/contextMenu2Shared.ts
+++ packages/popover2/src/contextMenu2Shared.ts
@@ -25,6 +25,11 @@ export type Offset = {
 
 /**
  * A limited subset of props to forward along to the context menu popover overlay.
+ *
+ * Overriding `placement` is not recommended, as users expect context menus to open towards the bottom right
+ * of their cursor, which is the default placement. However, this option is provided to help with rare cases where
+ * the menu is triggered at the bottom and/or right edge of a window and the built-in popover flipping behavior does
+ * not work effectively.
  */
 export type ContextMenu2PopoverOptions = OverlayLifecycleProps &
-    Pick<Popover2Props, "popoverClassName" | "transitionDuration" | "popoverRef" | "rootBoundary">;
+    Pick<Popover2Props, "placement" | "popoverClassName" | "transitionDuration" | "popoverRef" | "rootBoundary">;
