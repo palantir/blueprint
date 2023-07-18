6f7347fcbe6a61561eacde6d1e0cde14fc942ea6
Adi Dahiya <adahiya@palantir.com>
2023-07-18 14:58:51 -0400

    remove extraneous lint flag

--- packages/core/src/components/icon/icon.tsx
+++ packages/core/src/components/icon/icon.tsx
@@ -75,7 +75,6 @@ interface GenericIcon extends React.FC<IconProps<Element>> {
  *
  * @see https://blueprintjs.com/docs/#core/components/icon
  */
-// eslint-disable-next-line @typescript-eslint/no-redeclare
 export const Icon: GenericIcon = React.forwardRef(function <T extends Element>(props: IconProps<T>, ref: React.Ref<T>) {
     const { autoLoad, className, color, icon, intent, tagName, svgProps, title, htmlTitle, ...htmlProps } = props;
 
