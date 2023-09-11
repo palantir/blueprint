f6cc2060ce6efabed350be947f90d80199dfa274
Adi Dahiya <adahiya@palantir.com>
2023-09-11 10:23:39 -0400

    chore: exclude minimatch from clean lockfile check

--- scripts/verifyCleanLockfile.sh
+++ scripts/verifyCleanLockfile.sh
@@ -6,8 +6,13 @@ set -e
 set -o pipefail
 
 scripts_dir="$( dirname "$(readlink -f "$0")" )"
+
 bin="$scripts_dir/../node_modules/.bin"
-duplicates="$("$bin/yarn-deduplicate" "$scripts_dir/../yarn.lock" --strategy fewer --exclude react-day-picker --list)"
+
+# Exclude certain packages from deduplication, including:
+#     - react-day-picker (we have two major versions while we upgrade from v7 to v8, see https://github.com/palantir/blueprint/pull/5935)
+#     - minimatch (this node.js library is widely used by build tools and its lockfile entries often "dirty" the lockfile in a low-signal manner)
+duplicates="$("$bin/yarn-deduplicate" "$scripts_dir/../yarn.lock" --strategy fewer --exclude minimatch react-day-picker --list)"
 
 if [[ $duplicates ]]; then
     echo "Found duplicate blocks in yarn.lock which can be cleaned up. Please run 'yarn-deduplicate yarn.lock --strategy fewer'"
