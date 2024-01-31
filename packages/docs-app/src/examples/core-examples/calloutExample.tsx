b3bb64853983a76cd8fa8eb707f67d32703b1859
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2024-01-31 09:10:36 -0500

    [core] feat(Callout): `compact` modifier prop (#6592)
    Co-authored-by: Eugene Chan <echan@palantir.com>
    Co-authored-by: Adi Dahiya <adi.dahiya14@gmail.com>

--- packages/docs-app/src/examples/core-examples/calloutExample.tsx
+++ packages/docs-app/src/examples/core-examples/calloutExample.tsx
@@ -23,68 +23,51 @@ import type { IconName } from "@blueprintjs/icons";
 import { IconSelect } from "./common/iconSelect";
 import { IntentSelect } from "./common/intentSelect";
 
-interface CalloutExampleState {
-    contentIndex?: number;
-    icon?: IconName;
-    intent?: Intent;
-    showTitle: boolean;
-}
-
-export class CalloutExample extends React.PureComponent<DocsExampleProps, CalloutExampleState> {
-    public state: CalloutExampleState = {
-        contentIndex: 0,
-        showTitle: true,
-    };
-
-    private handleContentIndexChange = handleNumberChange(contentIndex => this.setState({ contentIndex }));
-
-    private handleIconNameChange = (icon: IconName) => this.setState({ icon });
-
-    private handleIntentChange = (intent: Intent) => this.setState({ intent });
-
-    private handleShowTitleChange = handleBooleanChange((showTitle: boolean) => this.setState({ showTitle }));
-
-    public render() {
-        const { contentIndex, showTitle, ...calloutProps } = this.state;
-        const options = (
-            <>
-                <H5>Props</H5>
-                <Switch checked={showTitle} label="Title" onChange={this.handleShowTitleChange} />
-                <IntentSelect intent={calloutProps.intent} onChange={this.handleIntentChange} showClearButton={true} />
-                <IconSelect iconName={calloutProps.icon} onChange={this.handleIconNameChange} />
-                <H5>Children</H5>
-                <Label>
-                    Example content
-                    <HTMLSelect value={contentIndex} onChange={this.handleContentIndexChange}>
-                        <option value="0">Text with formatting</option>
-                        <option value="1">Simple text string</option>
-                        <option value="2">Button</option>
-                        <option value="3">Empty</option>
-                    </HTMLSelect>
-                </Label>
-            </>
-        );
-
-        return (
-            <Example options={options} {...this.props}>
-                <Callout {...calloutProps} title={showTitle ? "Visually important content" : undefined}>
-                    {this.renderChildren(contentIndex)}
-                </Callout>
-            </Example>
-        );
-    }
+export const CalloutExample: React.FC<DocsExampleProps> = props => {
+    const [compact, setCompact] = React.useState(false);
+    const [contentIndex, setContentIndex] = React.useState(0);
+    const [showTitle, setShowTitle] = React.useState(true);
+    const [icon, setIcon] = React.useState<IconName>();
+    const [intent, setIntent] = React.useState<Intent>();
 
     /* eslint-disable react/jsx-key */
-    private renderChildren(contentIndex: number) {
-        return [
-            <React.Fragment>
-                Long-form information about the important content. This text is styled as{" "}
-                <a href="#core/typography.running-text">"Running text"</a>, so it may contain things like headers,
-                links, lists, <Code>code</Code> etc.
-            </React.Fragment>,
-            "Long-form information about the important content",
-            <Button text="Example button" intent="primary" />,
-            undefined,
-        ][contentIndex];
-    }
-}
+    const children = [
+        <React.Fragment>
+            Long-form information about the important content. This text is styled as{" "}
+            <a href="#core/typography.running-text">"Running text"</a>, so it may contain things like headers, links,
+            lists, <Code>code</Code> etc.
+        </React.Fragment>,
+        "Long-form information about the important content",
+        <Button text="Example button" intent="primary" />,
+        undefined,
+    ][contentIndex];
+    /* eslint-enable react/jsx-key */
+
+    const options = (
+        <>
+            <H5>Props</H5>
+            <Switch checked={showTitle} label="Title" onChange={handleBooleanChange(setShowTitle)} />
+            <Switch checked={compact} label="Compact" onChange={handleBooleanChange(setCompact)} />
+            <IntentSelect intent={intent} onChange={setIntent} showClearButton={true} />
+            <IconSelect iconName={icon} onChange={setIcon} />
+            <H5>Children</H5>
+            <Label>
+                Example content
+                <HTMLSelect value={contentIndex} onChange={handleNumberChange(setContentIndex)}>
+                    <option value={0}>Text with formatting</option>
+                    <option value={1}>Simple text string</option>
+                    <option value={2}>Button</option>
+                    <option value={3}>Empty</option>
+                </HTMLSelect>
+            </Label>
+        </>
+    );
+
+    return (
+        <Example options={options} {...props}>
+            <Callout {...{ compact, intent, icon }} title={showTitle ? "Visually important content" : undefined}>
+                {children}
+            </Callout>
+        </Example>
+    );
+};
