77e8977ab030fc361d4312a4bbc207c0a5ba889b
Adi Dahiya <adahiya@palantir.com>
2024-02-22 12:29:18 -0500

    darken Omnibar styles, use dark theme in docs examples & navigator

--- packages/docs-theme/src/components/documentation.tsx
+++ packages/docs-theme/src/components/documentation.tsx
@@ -170,6 +170,8 @@ export class Documentation extends React.PureComponent<DocumentationProps, Docum
             this.props.className,
         );
         const apiClasses = classNames("docs-api-drawer", this.props.className);
+        const isDarkTheme = rootClasses.includes(Classes.DARK);
+
         return (
             <DocumentationContext.Provider value={this.getDocumentationContextApi()}>
                 <HotkeysTarget2
@@ -246,6 +248,7 @@ export class Documentation extends React.PureComponent<DocumentationProps, Docum
                                 items={nav}
                                 itemExclude={this.props.navigatorExclude}
                                 onClose={this.handleCloseNavigator}
+                                useDarkTheme={isDarkTheme}
                             />
                         </div>
                     </div>
