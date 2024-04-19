a19169ce213f3d6b151012c358c876741cf5b9cc
Nikita Morozov <40804093+okaynik@users.noreply.github.com>
2024-04-19 19:06:22 -0400

    [core] fix(`Hotkeys`): Fix hotkeys dialog having two scrollbars (#6762)
    Co-authored-by: Nikita Morozov <nmorozov@palantir.com>

--- packages/core/src/legacy/hotkeysDialogLegacy.tsx
+++ packages/core/src/legacy/hotkeysDialogLegacy.tsx
@@ -26,7 +26,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Classes } from "../common";
-import { Dialog, type DialogProps, Hotkey, type HotkeyProps, Hotkeys } from "../components";
+import { Dialog, DialogBody, type DialogProps, Hotkey, type HotkeyProps, Hotkeys } from "../components";
 
 interface HotkeysDialogProps extends DialogProps {
     /**
@@ -125,7 +125,7 @@ class HotkeysDialogLegacy {
                 isOpen={this.isDialogShowing}
                 onClose={this.hide}
             >
-                <div className={Classes.DIALOG_BODY}>{this.renderHotkeys()}</div>
+                <DialogBody>{this.renderHotkeys()}</DialogBody>
             </Dialog>
         );
     }
