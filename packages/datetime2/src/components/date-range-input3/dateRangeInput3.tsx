e6c4866ae3d092d4d583a46df8859553c1c9d322
Andrii Lunin <alunin@palantir.com>
2024-05-02 01:03:45 +0400

    nit

--- packages/datetime2/src/components/date-range-input3/dateRangeInput3.tsx
+++ packages/datetime2/src/components/date-range-input3/dateRangeInput3.tsx
@@ -681,11 +681,12 @@ export class DateRangeInput3 extends DateFnsLocalizedComponent<DateRangeInput3Pr
         // If closeOnSelection is false, always keep the popover open.
         if (!this.props.closeOnSelection) return true;
 
-        // Close on date selection if TimePicker is not shown.
+        // Close the popover on date selection if TimePicker is not shown.
         if (this.props.timePrecision == null) return false;
 
         // Close the popover if the date was modified, keep it open if only the time was changed.
-        const [selectedStart, selectedEnd] = this.getSelectedRange();
+        const selectedStart: Date | null = this.state.selectedStart;
+        const selectedEnd: Date | null = this.state.selectedEnd;
         const isStartDateChanged = selectedStart == null || !isSameDay(selectedStart, nextSelectedStart);
         const isEndDateChanged = selectedEnd == null || !isSameDay(selectedEnd, nextSelectedEnd);
 
