d7f702142ffc6a11e6dec680c18c8087cf630ae2
Evan Johnson <esj254@nyu.edu>
2024-06-27 10:55:50 -0400

    more fix

--- packages/datetime2/src/common/errors.ts
+++ packages/datetime2/src/common/errors.ts
@@ -1,2 +1,2 @@
 export const WARNING_IGNORED_SHOW_OUTSIDE_DAYS_PROP =
-    "dayPickerProps.showOutsideDays will only be applied if the DateRangePicker is displaying a single month, either through the singleMonthOnly propm, or with a minDate and maxDate of the same month."
+    "dayPickerProps.showOutsideDays will only be applied if the DateRangePicker is displaying a single month, either through the singleMonthOnly propm, or with a minDate and maxDate of the same month.";
