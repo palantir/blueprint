b9c18e74411dbbb2493fc7d66f3f72ac45986902
Blake Vandercar <blake.vandercar@validatek.com>
2024-09-13 09:08:43 -0600

    remove popupKind

--- packages/docs-app/src/components/navHeader.tsx
+++ packages/docs-app/src/components/navHeader.tsx
@@ -17,7 +17,7 @@
 import type { NpmPackageInfo } from "@documentalist/client";
 import * as React from "react";
 
-import { Classes, HotkeysTarget2, type Intent, Menu, MenuItem, NavbarHeading, Popover, PopupKind, Tag } from "@blueprintjs/core";
+import { Classes, HotkeysTarget2, type Intent, Menu, MenuItem, NavbarHeading, Popover, Tag } from "@blueprintjs/core";
 import { NavButton } from "@blueprintjs/docs-theme";
 
 import { Logo } from "./logo";
@@ -109,7 +109,6 @@ export class NavHeader extends React.PureComponent<NavHeaderProps> {
                     </Menu>
                 }
                 placement="bottom"
-                popupKind={PopupKind.MENU}
             >
                 <Tag
                     interactive={true}
