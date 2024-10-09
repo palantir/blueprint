a5088e92034ab84d2e4c87d54e53d046557a0895
Gregory Douglas <2968519+ggdouglas@users.noreply.github.com>
2024-10-09 18:15:19 -0400

    Theme

--- packages/docs-app/src/examples/core-examples/sandpackButtonsExample.tsx
+++ packages/docs-app/src/examples/core-examples/sandpackButtonsExample.tsx
@@ -2,10 +2,16 @@
  * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
  */
 
-import { SandpackCodeEditor, SandpackLayout, SandpackPreview, SandpackProvider } from "@codesandbox/sandpack-react";
+import {
+    SandpackCodeEditor,
+    SandpackLayout,
+    SandpackPreview,
+    SandpackProvider,
+    type SandpackTheme,
+} from "@codesandbox/sandpack-react";
 import * as React from "react";
 
-import { Example, type ExampleProps } from "@blueprintjs/docs-theme";
+import { Colors } from "@blueprintjs/core";
 
 const app = `import { Button } from '@blueprintjs/core'
 
@@ -37,28 +43,58 @@ root.render(
 );
 `;
 
-export function SandpackButtonsExample(props: ExampleProps) {
+const theme: SandpackTheme = {
+    colors: {
+        accent: Colors.BLUE5,
+        base: Colors.GRAY3,
+        clickable: Colors.GRAY2,
+        disabled: Colors.DARK_GRAY4,
+        error: Colors.RED5,
+        errorSurface: Colors.RED1,
+        hover: Colors.GRAY5,
+        surface1: Colors.DARK_GRAY1,
+        surface2: Colors.DARK_GRAY2,
+        surface3: Colors.DARK_GRAY5,
+    },
+    font: {
+        body: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", blueprint-icons-16, sans-serif',
+        lineHeight: "20px",
+        mono: '"Fira Mono", "DejaVu Sans Mono", Menlo, Consolas, "Liberation Mono", Monaco, "Lucida Console", monospace',
+        size: "13px",
+    },
+    syntax: {
+        comment: { color: Colors.GRAY2, fontStyle: "italic" },
+        definition: Colors.GOLD5,
+        keyword: Colors.VIOLET4,
+        plain: Colors.GOLD5,
+        property: Colors.TURQUOISE3,
+        punctuation: Colors.WHITE,
+        static: Colors.RED3,
+        string: Colors.LIME4,
+        tag: Colors.FOREST3,
+    },
+};
+
+export function SandpackButtonsExample() {
     return (
-        <Example {...props}>
-            <SandpackProvider
-                template="react-ts"
-                theme="auto"
-                options={{ visibleFiles: ["/App.tsx"] }}
-                customSetup={{
-                    dependencies: {
-                        "@blueprintjs/core": "^5.13.1",
-                    },
-                }}
-                files={{
-                    "/App.tsx": app,
-                    "/index.tsx": index,
-                }}
-            >
-                <SandpackLayout className="layout">
-                    <SandpackCodeEditor className="editor" />
-                    <SandpackPreview className="preview" />
-                </SandpackLayout>
-            </SandpackProvider>
-        </Example>
+        <SandpackProvider
+            template="react-ts"
+            theme={theme}
+            options={{ visibleFiles: ["/App.tsx"] }}
+            customSetup={{
+                dependencies: {
+                    "@blueprintjs/core": "^5.13.1",
+                },
+            }}
+            files={{
+                "/App.tsx": app,
+                "/index.tsx": index,
+            }}
+        >
+            <SandpackLayout className="layout">
+                <SandpackCodeEditor className="editor" />
+                <SandpackPreview className="preview" />
+            </SandpackLayout>
+        </SandpackProvider>
     );
 }
