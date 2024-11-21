da86c188068095fbc412f0f1bf1b3a304c18da98
Gregory Douglas <gdouglas@palantir.com>
2024-11-21 14:32:31 -0500

    Update DividerExample

--- packages/docs-app/src/examples/core-examples/dividerExample.tsx
+++ packages/docs-app/src/examples/core-examples/dividerExample.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Palantir Technologies, Inc. All rights reserved.
+ * Copyright 2024 Palantir Technologies, Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,36 +19,28 @@ import * as React from "react";
 import { Button, ButtonGroup, Divider, H5, Switch } from "@blueprintjs/core";
 import { Example, type ExampleProps, handleBooleanChange } from "@blueprintjs/docs-theme";
 
-export interface DividerExampleState {
-    vertical: boolean;
-}
+export const DividerExample: React.FC<ExampleProps> = props => {
+    const [vertical, setVertical] = React.useState(false);
 
-export class DividerExample extends React.PureComponent<ExampleProps, DividerExampleState> {
-    public state: DividerExampleState = { vertical: false };
+    const options = (
+        <>
+            <H5>Example props</H5>
+            <Switch checked={vertical} label="Vertical" onChange={handleBooleanChange(setVertical)} />
+        </>
+    );
 
-    private handleVerticalChange = handleBooleanChange(vertical => this.setState({ vertical }));
-
-    public render() {
-        const { vertical } = this.state;
-        const options = (
-            <>
-                <H5>Example props</H5>
-                <Switch checked={vertical} label="Vertical" onChange={this.handleVerticalChange} />
-            </>
-        );
-        return (
-            <Example options={options} {...this.props}>
-                <ButtonGroup minimal={true} vertical={vertical}>
-                    <Button text="File" />
-                    <Button text="Edit" />
-                    <Divider />
-                    <Button text="Create" />
-                    <Button text="Delete" />
-                    <Divider />
-                    <Button icon="add" />
-                    <Button icon="remove" />
-                </ButtonGroup>
-            </Example>
-        );
-    }
-}
+    return (
+        <Example options={options} {...props}>
+            <ButtonGroup minimal={true} vertical={vertical}>
+                <Button text="File" />
+                <Button text="Edit" />
+                <Divider />
+                <Button text="Create" />
+                <Button text="Delete" />
+                <Divider />
+                <Button icon="add" />
+                <Button icon="remove" />
+            </ButtonGroup>
+        </Example>
+    );
+};
