9e2a59015be6ab8f77dc1c8dbfce915c412f79f8
Jonah Scheinerman <jonah@scheinerman.net>
2025-01-13 17:31:44 -0500

    CR

--- packages/core/test/forms/fileInputTests.tsx
+++ packages/core/test/forms/fileInputTests.tsx
@@ -79,7 +79,7 @@ describe("<FileInput>", () => {
         const onChange = sinon.spy();
         const onInputChange = sinon.spy();
 
-        const wrapper = shallow(<FileInput {...{ inputProps, onChange, onInputChange }} />);
+        const wrapper = shallow(<FileInput inputProps={inputProps} onChange={onChange} onInputChange={onInputChange} />);
         const input = getInput(wrapper);
         input.simulate("change");
 
