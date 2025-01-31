9165893c3272750223083bdb92d20a3fa5730eaf
Greg Douglas <GWShark0@gmail.com>
2025-01-31 16:54:38 -0500

    Update TooltipExample

--- packages/docs-app/src/examples/core-examples/tooltipExample.tsx
+++ packages/docs-app/src/examples/core-examples/tooltipExample.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Palantir Technologies, Inc. All rights reserved.
+ * Copyright 2025 Palantir Technologies, Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,151 +17,134 @@
 import * as React from "react";
 
 import { Button, ButtonGroup, Classes, Code, H1, Popover, Switch, Tooltip } from "@blueprintjs/core";
-import { Example, type ExampleProps } from "@blueprintjs/docs-theme";
+import { Example, type ExampleProps, handleBooleanChange } from "@blueprintjs/docs-theme";
 
-export interface TooltipExampleState {
-    isOpen: boolean;
-}
+export const TooltipExample: React.FC<ExampleProps> = props => {
+    const [isOpen, setIsOpen] = React.useState(false);
 
-export class TooltipExample extends React.PureComponent<ExampleProps, TooltipExampleState> {
-    public static displayName = "TooltipExample";
+    // using JSX instead of strings for all content so the tooltips will re-render
+    // with every update for dark theme inheritance.
+    const lotsOfText = (
+        <span>
+            In facilisis scelerisque dui vel dignissim. Sed nunc orci, ultricies congue vehicula quis, facilisis a orci.
+        </span>
+    );
 
-    public state: TooltipExampleState = {
-        isOpen: false,
-    };
+    const jsxContent = (
+        <em>
+            This tooltip contains an <strong>em</strong> tag.
+        </em>
+    );
 
-    public render() {
-        // using JSX instead of strings for all content so the tooltips will re-render
-        // with every update for dark theme inheritance.
-        const lotsOfText = (
-            <span>
-                In facilisis scelerisque dui vel dignissim. Sed nunc orci, ultricies congue vehicula quis, facilisis a
-                orci.
-            </span>
-        );
-        const jsxContent = (
-            <em>
-                This tooltip contains an <strong>em</strong> tag.
-            </em>
-        );
-        return (
-            <Example options={false} {...this.props}>
-                <div>
-                    Inline text can have{" "}
-                    <Tooltip className={Classes.TOOLTIP_INDICATOR} content={jsxContent}>
-                        a tooltip.
-                    </Tooltip>
-                </div>
-                <div>
-                    <Tooltip content={lotsOfText}>Or, hover anywhere over this whole line.</Tooltip>
-                </div>
-                <div>
-                    This line's tooltip{" "}
-                    <Tooltip className={Classes.TOOLTIP_INDICATOR} content={<span>disabled</span>} disabled={true}>
-                        is disabled.
-                    </Tooltip>
-                </div>
-                <div>
-                    This line's tooltip{" "}
-                    <Tooltip
-                        className={Classes.TOOLTIP_INDICATOR}
-                        content={<span>This tooltip has the minimal style applied!</span>}
-                        minimal={true}
-                    >
-                        is minimal.
-                    </Tooltip>
-                </div>
-                <div>
-                    This line's tooltip{" "}
-                    <Tooltip
-                        content={
-                            <span>
-                                Use <Code>{`compact={true}`}</Code> in data-dense UIs
-                            </span>
-                        }
-                        compact={true}
-                        isOpen={this.state.isOpen}
-                    >
-                        is controlled by external state.
-                    </Tooltip>
-                    <Switch
-                        checked={this.state.isOpen}
-                        label="Open"
-                        onChange={this.toggleControlledTooltip}
-                        style={{ display: "inline-block", marginBottom: 0, marginLeft: 20 }}
-                    />
-                </div>
-                <div>
-                    <Tooltip
-                        className={Classes.TOOLTIP_INDICATOR}
-                        content="primary"
-                        intent="primary"
-                        placement="left"
-                        usePortal={false}
-                    >
-                        Available
-                    </Tooltip>{" "}
-                    <Tooltip
-                        className={Classes.TOOLTIP_INDICATOR}
-                        content="success"
-                        intent="success"
-                        placement="top"
-                        usePortal={false}
-                    >
-                        in the full
-                    </Tooltip>{" "}
-                    <Tooltip
-                        className={Classes.TOOLTIP_INDICATOR}
-                        content="warning"
-                        intent="warning"
-                        placement="bottom"
-                        usePortal={false}
-                    >
-                        range of
-                    </Tooltip>{" "}
-                    <Tooltip
-                        className={Classes.TOOLTIP_INDICATOR}
-                        content="danger"
-                        intent="danger"
-                        placement="right"
-                        usePortal={false}
-                    >
-                        visual intents!
-                    </Tooltip>
-                </div>
-                <br />
-                <Popover
-                    content={<H1>Popover!</H1>}
-                    placement="right"
-                    popoverClassName={Classes.POPOVER_CONTENT_SIZING}
+    return (
+        <Example options={false} {...props}>
+            <div>
+                Inline text can have{" "}
+                <Tooltip className={Classes.TOOLTIP_INDICATOR} content={jsxContent}>
+                    a tooltip.
+                </Tooltip>
+            </div>
+            <div>
+                <Tooltip content={lotsOfText}>Or, hover anywhere over this whole line.</Tooltip>
+            </div>
+            <div>
+                This line's tooltip{" "}
+                <Tooltip className={Classes.TOOLTIP_INDICATOR} content={<span>disabled</span>} disabled={true}>
+                    is disabled.
+                </Tooltip>
+            </div>
+            <div>
+                This line's tooltip{" "}
+                <Tooltip
+                    className={Classes.TOOLTIP_INDICATOR}
+                    content={<span>This tooltip has the minimal style applied!</span>}
+                    minimal={true}
                 >
-                    <Tooltip
-                        content={<span>This button also has a popover!</span>}
-                        openOnTargetFocus={false}
-                        placement="right"
-                        usePortal={false}
-                    >
-                        <Button intent="success" text="Hover and click me" />
-                    </Tooltip>
-                </Popover>
-                <br />
+                    is minimal.
+                </Tooltip>
+            </div>
+            <div>
+                This line's tooltip{" "}
+                <Tooltip
+                    compact={true}
+                    content={
+                        <span>
+                            Use <Code>{`compact={true}`}</Code> in data-dense UIs
+                        </span>
+                    }
+                    isOpen={isOpen}
+                >
+                    is controlled by external state.
+                </Tooltip>
+                <Switch
+                    checked={isOpen}
+                    label="Open"
+                    onChange={handleBooleanChange(setIsOpen)}
+                    style={{ display: "inline-block", marginBottom: 0, marginLeft: 20 }}
+                />
+            </div>
+            <div>
+                <Tooltip
+                    className={Classes.TOOLTIP_INDICATOR}
+                    content="primary"
+                    intent="primary"
+                    placement="left"
+                    usePortal={false}
+                >
+                    Available
+                </Tooltip>{" "}
+                <Tooltip
+                    className={Classes.TOOLTIP_INDICATOR}
+                    content="success"
+                    intent="success"
+                    placement="top"
+                    usePortal={false}
+                >
+                    in the full
+                </Tooltip>{" "}
+                <Tooltip
+                    className={Classes.TOOLTIP_INDICATOR}
+                    content="warning"
+                    intent="warning"
+                    placement="bottom"
+                    usePortal={false}
+                >
+                    range of
+                </Tooltip>{" "}
+                <Tooltip
+                    className={Classes.TOOLTIP_INDICATOR}
+                    content="danger"
+                    intent="danger"
+                    placement="right"
+                    usePortal={false}
+                >
+                    visual intents!
+                </Tooltip>
+            </div>
+            <br />
+            <Popover content={<H1>Popover!</H1>} placement="right" popoverClassName={Classes.POPOVER_CONTENT_SIZING}>
+                <Tooltip
+                    content={<span>This button also has a popover!</span>}
+                    openOnTargetFocus={false}
+                    placement="right"
+                    usePortal={false}
+                >
+                    <Button intent="success" text="Hover and click me" />
+                </Tooltip>
+            </Popover>
+            <br />
 
-                <ButtonGroup>
-                    <Tooltip content="Each" placement="bottom">
-                        <Button intent="primary" text="Group" />
-                    </Tooltip>
-                    <Tooltip content="has" placement="bottom">
-                        <Button intent="primary" text="of" />
-                    </Tooltip>
-                    <Tooltip content="a tooltip" placement="bottom">
-                        <Button intent="primary" text="buttons" />
-                    </Tooltip>
-                </ButtonGroup>
-            </Example>
-        );
-    }
-
-    private toggleControlledTooltip = () => {
-        this.setState({ isOpen: !this.state.isOpen });
-    };
-}
+            <ButtonGroup>
+                <Tooltip content="Each" placement="bottom">
+                    <Button intent="primary" text="Group" />
+                </Tooltip>
+                <Tooltip content="has" placement="bottom">
+                    <Button intent="primary" text="of" />
+                </Tooltip>
+                <Tooltip content="a tooltip" placement="bottom">
+                    <Button intent="primary" text="buttons" />
+                </Tooltip>
+            </ButtonGroup>
+        </Example>
+    );
+};
