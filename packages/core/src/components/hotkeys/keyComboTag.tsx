e9a11241546be98a2a977939e23893db4010f8c6
Gregory Douglas <gdouglas@palantir.com>
2025-02-05 17:16:41 -0500

    Refactor KeyComboTag to a functional component

--- packages/core/src/components/hotkeys/keyComboTag.tsx
+++ packages/core/src/components/hotkeys/keyComboTag.tsx
@@ -15,7 +15,7 @@
  */
 
 import classNames from "classnames";
-import { Fragment } from "react";
+import { Fragment, useCallback } from "react";
 
 import {
     ArrowDown,
@@ -30,7 +30,7 @@ import {
     KeyShift,
 } from "@blueprintjs/icons";
 
-import { AbstractPureComponent, Classes, DISPLAYNAME_PREFIX, type Props } from "../../common";
+import { Classes, DISPLAYNAME_PREFIX, type Props } from "../../common";
 import { Icon } from "../icon/icon";
 
 import { isMac, normalizeKeyCombo } from "./hotkeyParser";
@@ -76,50 +76,55 @@ interface KeyComboTagInternalProps extends KeyComboTagProps {
     platformOverride?: string;
 }
 
-export class KeyComboTagInternal extends AbstractPureComponent<KeyComboTagInternalProps> {
-    public static displayName = `${DISPLAYNAME_PREFIX}.KeyComboTag`;
+export const KeyComboTagInternal: React.FC<KeyComboTagInternalProps> = props => {
+    const { className, combo, minimal, platformOverride } = props;
 
-    public render() {
-        const { className, combo, minimal, platformOverride } = this.props;
-        const normalizedKeys = normalizeKeyCombo(combo, platformOverride);
-        const keys = normalizedKeys
-            .map(key => (key.length === 1 ? key.toUpperCase() : key))
-            .map((key, index) =>
-                minimal
-                    ? this.renderMinimalKey(key, index, index === normalizedKeys.length - 1)
-                    : this.renderKey(key, index),
+    const getKeyIcon = useCallback(
+        (key: string) => {
+            const icon = KEY_ICONS[key.toLowerCase()];
+            if (icon?.isMacOnly && !isMac(platformOverride)) {
+                return undefined;
+            }
+            return icon;
+        },
+        [platformOverride],
+    );
+
+    const renderKey = useCallback(
+        (key: string, index: number) => {
+            const keyString = DISPLAY_ALIASES[key.toLowerCase()] ?? key;
+            const icon = getKeyIcon(key);
+            const reactKey = `key-${index}`;
+            return (
+                <kbd className={classNames(Classes.KEY, { [Classes.MODIFIER_KEY]: icon != null })} key={reactKey}>
+                    {icon != null && <Icon icon={icon.icon} title={icon.iconTitle} />}
+                    {keyString}
+                </kbd>
             );
-        return <span className={classNames(Classes.KEY_COMBO, className, { [Classes.MINIMAL]: minimal })}>{keys}</span>;
-    }
+        },
+        [getKeyIcon],
+    );
 
-    private renderKey = (key: string, index: number) => {
-        const keyString = DISPLAY_ALIASES[key.toLowerCase()] ?? key;
-        const icon = this.getKeyIcon(key);
-        const reactKey = `key-${index}`;
-        return (
-            <kbd className={classNames(Classes.KEY, { [Classes.MODIFIER_KEY]: icon != null })} key={reactKey}>
-                {icon != null && <Icon icon={icon.icon} title={icon.iconTitle} />}
-                {keyString}
-            </kbd>
+    const renderMinimalKey = useCallback(
+        (key: string, index: number, isLastKey: boolean) => {
+            const icon = getKeyIcon(key);
+            if (icon == null) {
+                return isLastKey ? key : <Fragment key={`key-${index}`}>{key}&nbsp;+&nbsp;</Fragment>;
+            }
+            return <Icon icon={icon.icon} title={icon.iconTitle} key={`key-${index}`} />;
+        },
+        [getKeyIcon],
+    );
+
+    const normalizedKeys = normalizeKeyCombo(combo, platformOverride);
+    const keys = normalizedKeys
+        .map(key => (key.length === 1 ? key.toUpperCase() : key))
+        .map((key, index) =>
+            minimal ? renderMinimalKey(key, index, index === normalizedKeys.length - 1) : renderKey(key, index),
         );
-    };
+    return <span className={classNames(Classes.KEY_COMBO, className, { [Classes.MINIMAL]: minimal })}>{keys}</span>;
+};
 
-    private renderMinimalKey = (key: string, index: number, isLastKey: boolean) => {
-        const icon = this.getKeyIcon(key);
-        if (icon == null) {
-            return isLastKey ? key : <Fragment key={`key-${index}`}>{key}&nbsp;+&nbsp;</Fragment>;
-        }
-        return <Icon icon={icon.icon} title={icon.iconTitle} key={`key-${index}`} />;
-    };
-
-    private getKeyIcon(key: string) {
-        const { platformOverride } = this.props;
-        const icon = KEY_ICONS[key.toLowerCase()];
-        if (icon?.isMacOnly && !isMac(platformOverride)) {
-            return undefined;
-        }
-        return icon;
-    }
-}
+KeyComboTagInternal.displayName = `${DISPLAYNAME_PREFIX}.KeyComboTag`;
 
 export const KeyComboTag: React.ComponentType<KeyComboTagProps> = KeyComboTagInternal;
