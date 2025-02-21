00321152228203f1bf83f19a944d5b1a11f086ce
Gregory Douglas <gdouglas@palantir.com>
2025-02-21 10:50:20 -0500

    nit: year

--- packages/docs-app/src/examples/core-examples/editableTextExamples.tsx
+++ packages/docs-app/src/examples/core-examples/editableTextExamples.tsx
@@ -1,5 +1,5 @@
 /* !
- * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ * (c) Copyright 2025 Palantir Technologies Inc. All rights reserved.
  */
 
 import dedent from "dedent";
