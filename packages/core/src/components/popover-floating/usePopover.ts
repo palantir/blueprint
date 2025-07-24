4317aa4de60dafff5b82911db11c521b388aac69
Greg Douglas <GWShark0@gmail.com>
2025-07-24 10:49:50 -0400

    Update popover width calculation to use exact reference width instead of minimum width

--- packages/core/src/components/popover-floating/usePopover.ts
+++ packages/core/src/components/popover-floating/usePopover.ts
@@ -105,7 +105,7 @@ export function usePopover({
                 ? size({
                       apply({ rects, elements }) {
                           Object.assign(elements.floating.style, {
-                              minWidth: `${rects.reference.width}px`,
+                              width: `${rects.reference.width}px`,
                           });
                       },
                   })
