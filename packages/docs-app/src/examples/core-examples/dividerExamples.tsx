53e216eb8a76c33336724bfa9fd93ada96e8c1c6
Gregory Douglas <gdouglas@palantir.com>
2025-08-11 10:56:51 -0400

    Move Divider examples

--- packages/docs-app/src/examples/core-examples/dividerExamples.tsx
+++ packages/docs-app/src/examples/core-examples/dividerExamples.tsx
@@ -2,45 +2,25 @@
  * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
  */
 
-import dedent from "dedent";
-
-import { Divider } from "@blueprintjs/core";
 import { CodeExample, type ExampleProps } from "@blueprintjs/docs-theme";
 
+import DividerBasic from "./divider/DividerBasic";
+import dividerBasicPreview from "./divider/DividerBasic.tsx.preview?raw";
+import DividerVertical from "./divider/DividerVertical";
+import dividerVerticalPreview from "./divider/DividerVertical.tsx.preview?raw";
+
 export const DividerBasicExample: React.FC<ExampleProps> = props => {
-    const code = dedent`
-        Content above
-        <Divider />
-        Content below`;
     return (
-        <CodeExample code={code} {...props}>
-            <div>
-                Content above
-                <Divider />
-                Content below
-            </div>
+        <CodeExample code={dividerBasicPreview} {...props}>
+            <DividerBasic />
         </CodeExample>
     );
 };
 
 export const DividerVerticalExample: React.FC<ExampleProps> = props => {
-    const code = dedent`
-        <div style={{ display: "flex" }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis aliquam elit. Quisque ultricies
-            dolor non sapien dictum semper.
-            <Divider />
-            Praesent auctor eros vitae lacus porttitor elementum. Curabitur dolor dolor, sodales eget mi eget,
-            convallis scelerisque eros.
-        </div>`;
     return (
-        <CodeExample code={code} {...props}>
-            <div style={{ display: "flex" }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis aliquam elit. Quisque ultricies
-                dolor non sapien dictum semper.
-                <Divider />
-                Praesent auctor eros vitae lacus porttitor elementum. Curabitur dolor dolor, sodales eget mi eget,
-                convallis scelerisque eros.
-            </div>
+        <CodeExample code={dividerVerticalPreview} {...props}>
+            <DividerVertical />
         </CodeExample>
     );
 };
