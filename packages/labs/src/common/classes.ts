2a0195162226d5d647166eb574eb3bcd28d88d78
Margaret Wang <margaretw@palantir.com>
2025-10-31 14:40:02 -0700

    Getting to compile since we don't need this right now with no exports

--- packages/labs/src/common/classes.ts
+++ packages/labs/src/common/classes.ts
@@ -3,17 +3,17 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  */
 
-import { MAJOR_VERSION } from "./constants";
+// import { MAJOR_VERSION } from "./constants";
 
-declare let BLUEPRINT_NAMESPACE: string | undefined;
-declare let REACT_APP_BLUEPRINT_NAMESPACE: string | undefined;
+// declare let BLUEPRINT_NAMESPACE: string | undefined;
+// declare let REACT_APP_BLUEPRINT_NAMESPACE: string | undefined;
 
-let NS = `bp${MAJOR_VERSION}-labs`;
+// let NS = `bp${MAJOR_VERSION}-labs`;
 
-if (typeof BLUEPRINT_NAMESPACE !== "undefined") {
-    NS = BLUEPRINT_NAMESPACE;
-} else if (typeof REACT_APP_BLUEPRINT_NAMESPACE !== "undefined") {
-    NS = REACT_APP_BLUEPRINT_NAMESPACE;
-}
+// if (typeof BLUEPRINT_NAMESPACE !== "undefined") {
+//     NS = BLUEPRINT_NAMESPACE;
+// } else if (typeof REACT_APP_BLUEPRINT_NAMESPACE !== "undefined") {
+//     NS = REACT_APP_BLUEPRINT_NAMESPACE;
+// }
 
 // Example: export const SAMPLE_COMPONENT = `${NS}-sample-component`;
