841eb6e21a7d0bc709f94156cd8f953ecf20a543
Gregory Douglas <gdouglas@palantir.com>
2025-11-05 16:10:37 -0500

    Additional tweaks to `$ns` in labs

--- packages/labs/src/common/classes.ts
+++ packages/labs/src/common/classes.ts
@@ -7,17 +7,7 @@ import { Classes } from "@blueprintjs/core";
 
 const CORE_NS = Classes.getClassNamespace();
 
-// injected by webpack.DefinePlugin
-declare let BLUEPRINT_NAMESPACE: string | undefined;
-declare let REACT_APP_BLUEPRINT_NAMESPACE: string | undefined;
-
-let NS = `${CORE_NS}-labs`;
-
-if (typeof BLUEPRINT_NAMESPACE !== "undefined") {
-    NS = BLUEPRINT_NAMESPACE;
-} else if (typeof REACT_APP_BLUEPRINT_NAMESPACE !== "undefined") {
-    NS = REACT_APP_BLUEPRINT_NAMESPACE;
-}
+const NS = `${CORE_NS}-labs`;
 
 /**
  * Returns the namespace prefix for all Blueprint CSS classes.
