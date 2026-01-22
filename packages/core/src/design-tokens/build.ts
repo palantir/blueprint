121a9ca25878f6966b7b39ee7e46e6e19a4df9f1
Margaret Wang <margaretw@palantir.com>
2026-01-21 16:52:41 -0800

    Adding design tokens to core package

---
+++ packages/core/src/design-tokens/build.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+/**
+ * @license Copyright 2025 Palantir Technologies, Inc. All rights reserved.
+ */
+
+import StyleDictionary from "style-dictionary";
+
+import { config } from "./sd.config.ts";
+
+async function build() {
+    console.info("🎨 Building design tokens...\n");
+
+    try {
+        const sd = new StyleDictionary(config);
+
+        // Build all platforms
+        await sd.buildAllPlatforms();
+
+        console.info("\n✔︎ Style Dictionary build complete!");
+        console.info("   Generated: TypeScript, CSS, SCSS, Less\n");
+    } catch (error) {
+        console.error("✗ Style Dictionary build failed:");
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+build();
