8a93f5586eafb123143e70b351ae487ce195a357
Cameron Joyner <cjoyner@palantir.com>
2026-02-10 11:38:24 -0500

    2026 copyright

--- packages/labs/src/components/listogram/listogramItemProps.ts
+++ packages/labs/src/components/listogram/listogramItemProps.ts
@@ -1,5 +1,5 @@
 /* !
- * (c) Copyright 2025 Palantir Technologies Inc. All rights reserved.
+ * (c) Copyright 2026 Palantir Technologies Inc. All rights reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  */
 
