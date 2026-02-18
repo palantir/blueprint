484990ead8035f62667f2fbec08f30bfc9846f29
Cameron Joyner <cjoyner@palantir.com>
2026-02-18 10:43:55 -0500

    update copyright

--- packages/docs-app/src/mdxComponents.tsx
+++ packages/docs-app/src/mdxComponents.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Palantir Technologies, Inc. All rights reserved.
+ * Copyright 2026 Palantir Technologies, Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,7 +31,6 @@ import * as datetimeExamples from "./examples/datetime-examples";
 import * as labsExamples from "./examples/labs-examples";
 import * as selectExamples from "./examples/select-examples";
 import * as tableExamples from "./examples/table-examples";
-
 
 /**
  * Placeholder for InterfaceTable — API docs have been removed for now.
