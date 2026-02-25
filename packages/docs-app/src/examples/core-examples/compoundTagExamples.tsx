f98534d9bcaffe03bd8d40c76f80571cd094545b
Cameron Joyner <cjoyner@palantir.com>
2026-02-25 10:03:43 -0500

    fixing copyright usage

--- packages/docs-app/src/examples/core-examples/compoundTagExamples.tsx
+++ packages/docs-app/src/examples/core-examples/compoundTagExamples.tsx
@@ -1,5 +1,6 @@
 /* !
- * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ * (c) Copyright 2026 Palantir Technologies Inc. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
  */
 
 import { CodeExample, type ExampleProps } from "@blueprintjs/docs-theme";
