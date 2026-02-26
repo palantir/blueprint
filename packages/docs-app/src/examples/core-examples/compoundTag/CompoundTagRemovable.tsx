0f4b175d6d14e68113f50a8b249f0ef3ae2aed64
Cam Joyner <cameronjoyner@gmail.com>
2026-02-26 12:34:23 -0500

    Update CompoundTagRemovable.tsx

--- packages/docs-app/src/examples/core-examples/compoundTag/CompoundTagRemovable.tsx
+++ packages/docs-app/src/examples/core-examples/compoundTag/CompoundTagRemovable.tsx
@@ -24,7 +24,7 @@ export default function CompoundTagRemovable() {
                 ))}
             </div>
             {tags.length === 0 && (
-                <Button icon="refresh" variant="outlined" text="Reset tags" onClick={handleReset} />
+                <Button icon="refresh" variant="outlined" size="small" text="Reset tags" onClick={handleReset} />
             )}
         </div>
     );
