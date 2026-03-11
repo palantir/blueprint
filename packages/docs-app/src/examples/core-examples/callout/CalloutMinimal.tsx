f6209e604216982b553af0c18493e2fceba7724b
Cameron Joyner <cjoyner@palantir.com>
2026-03-11 10:55:05 -0400

    callout minimal

--- packages/docs-app/src/examples/core-examples/callout/CalloutMinimal.tsx
+++ packages/docs-app/src/examples/core-examples/callout/CalloutMinimal.tsx
@@ -3,8 +3,12 @@ import { Callout } from "@blueprintjs/core";
 export default function CalloutMinimal() {
     return (
         <div className="stack" style={{ width: "100%" }}>
-            <Callout minimal={true} title="Minimal Callout">A minimal Callout with default colors.</Callout>
-            <Callout minimal={true} intent="primary" title="Minimal Primary">A minimal Callout with primary intent.</Callout>
+            <Callout minimal={true} title="Minimal Callout">
+                A minimal Callout with default colors.
+            </Callout>
+            <Callout minimal={true} intent="primary" title="Minimal Primary">
+                A minimal Callout with primary intent.
+            </Callout>
         </div>
     );
 }
