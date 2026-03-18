5f93474d544951a56f451aa75b8d7b625c3b1b77
Gregory Douglas <gdouglas@palantir.com>
2026-03-18 11:10:28 -0400

    Fix flaky DateRangeInput tests by unmounting Enzyme wrappers

--- packages/datetime/src/components/date-range-input/dateRangeInput.test.tsx
+++ packages/datetime/src/components/date-range-input/dateRangeInput.test.tsx
@@ -68,6 +68,7 @@ const DATETIME_FORMAT = getDateFnsFormatter("M/d/yyyy HH:mm:ss");
 
 describe("<DateRangeInput>", () => {
     let containerElement: HTMLElement;
+    let mountedWrappers: ReactWrapper[] = [];
 
     beforeEach(() => {
         containerElement = document.createElement("div");
@@ -75,6 +76,11 @@ describe("<DateRangeInput>", () => {
     });
 
     afterEach(() => {
+        // Unmount all Enzyme wrappers before removing the container to prevent
+        // React from trying to commit updates to removed DOM nodes, which causes
+        // "window is not defined" and "Should not already be working" errors.
+        mountedWrappers.forEach(wrapper => wrapper.unmount());
+        mountedWrappers = [];
         containerElement.remove();
     });
 
@@ -3150,6 +3156,7 @@ describe("<DateRangeInput>", () => {
     function wrap(dateRangeInput: React.JSX.Element, attachToDOM = false) {
         const mountOptions = attachToDOM ? { attachTo: containerElement } : undefined;
         const wrapper = mount(dateRangeInput, mountOptions);
+        mountedWrappers.push(wrapper);
         return {
             getDayElement: (dayNumber = 1, fromLeftMonth = true) => {
                 const monthElement = wrapper.find(`.${ReactDayPickerClasses.RDP_MONTH}`).at(fromLeftMonth ? 0 : 1);
