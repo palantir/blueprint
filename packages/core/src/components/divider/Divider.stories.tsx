3041440a869ff8fe817856c5ec4d61228c0f724d
Margaret Wang <margaretw@palantir.com>
2026-03-30 15:30:50 -0700

    Adding more space in divider, adding another story, updating spacing Adding more spacing, adding new story

--- packages/core/src/components/divider/Divider.stories.tsx
+++ packages/core/src/components/divider/Divider.stories.tsx
@@ -1,4 +1,4 @@
-/* !
+/*
  * (c) Copyright 2026 Palantir Technologies Inc. All rights reserved.
  */
 
@@ -28,7 +28,8 @@ const meta: Meta<typeof Divider> = {
             control: "boolean",
         },
         tagName: {
-            control: "text",
+            control: "select",
+            options: ["div", "span", "br", "hr", "p"], // Can be any HTML tag
         },
     },
 } satisfies Meta<typeof Divider>;
@@ -52,6 +53,21 @@ export const Default: Story = {
 };
 
 /**
+ * A basic vertical divider inside a flex container
+ */
+export const Vertical: Story = {
+    decorators: [
+        Story => (
+            <div style={{ display: "flex", width: "300px" }}>
+                <span>Content to the left that wraps around a bit more than you'd expect</span>
+                <Story />
+                <span>Content to the right that also wraps around a bit more than you'd expect</span>
+            </div>
+        ),
+    ],
+};
+
+/**
  * Use the `compact` prop to remove the default margin around the Divider,
  * making it flush with adjacent content.
  */
@@ -61,15 +77,15 @@ export const CompactExample: Story = {
         compact: { table: { disable: true } },
     },
     render: args => (
-        <div style={{ display: "flex", flexDirection: "column", gap: 0, width: "300px" }}>
+        <div style={{ display: "flex", flexDirection: "column", gap: "3em", width: "300px" }}>
             <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
-                <span style={{ fontSize: 12, opacity: 0.6, marginBottom: 4 }}>Default (with margin)</span>
+                <span style={{ fontSize: 12, opacity: 0.6 }}>Default</span>
                 <span>Above</span>
                 <Divider {...args} compact={false} />
                 <span>Below</span>
             </div>
-            <div style={{ marginTop: 16, display: "flex", flexDirection: "column", width: "100%" }}>
-                <span style={{ fontSize: 12, opacity: 0.6, marginBottom: 4 }}>Compact (no margin)</span>
+            <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
+                <span style={{ fontSize: 12, opacity: 0.6 }}>Compact</span>
                 <span>Above</span>
                 <Divider {...args} compact={true} />
                 <span>Below</span>
