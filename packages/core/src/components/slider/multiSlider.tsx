7bbb2f8ba64879fed848405394a65bce62d4d057
Braeden Smith <braedens@palantir.com>
2026-03-30 12:24:01 -0400

    Prettier

--- packages/core/src/components/slider/multiSlider.tsx
+++ packages/core/src/components/slider/multiSlider.tsx
@@ -223,7 +223,11 @@ export class MultiSlider extends AbstractPureComponent<MultiSliderProps, SliderS
 
     public componentDidUpdate(prevProps: MultiSliderProps, prevState: SliderState) {
         super.componentDidUpdate(prevProps, prevState);
-        if (prevProps.min !== this.props.min || prevProps.max !== this.props.max || prevProps.vertical !== this.props.vertical) {
+        if (
+            prevProps.min !== this.props.min ||
+            prevProps.max !== this.props.max ||
+            prevProps.vertical !== this.props.vertical
+        ) {
             this.updateTickSize();
         }
     }
