f0f7f73d6c75f1464f6f6b7b04262ae64ef5ebdc
Cameron Joyner <cjoyner@palantir.com>
2026-03-30 14:20:40 -0400

    scaffolding

--- packages/docs-data/src/index.d.ts
+++ packages/docs-data/src/index.d.ts
@@ -2,9 +2,9 @@
  * Copyright 2017 Palantir Technologies, Inc. All rights reserved.
  */
 
-import { NpmPluginData, MarkdownPluginData, KssPluginData, TypescriptPluginData } from "@documentalist/client";
+import { MarkdownPluginData, KssPluginData, TypescriptPluginData } from "@documentalist/client";
 
-export type DocsCompleteData = MarkdownPluginData & NpmPluginData & KssPluginData & TypescriptPluginData;
+export type DocsCompleteData = MarkdownPluginData & KssPluginData & TypescriptPluginData;
 
 export const docsData: DocsCompleteData;
 
