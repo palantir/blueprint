4495195c8477d1de71d3a5c5974ab770b126a8d1
Cam Joyner <cameronjoyner@gmail.com>
2026-03-30 12:39:50 -0400

    Documentalist Migration pt2 - remove @documentalist from docs-data (#7774)
    Co-authored-by: Cameron Joyner <cjoyner@palantir.com>

--- packages/docs-data/src/index.d.ts
+++ packages/docs-data/src/index.d.ts
@@ -10,3 +10,25 @@ export const docsData: DocsCompleteData;
 
 export { PACKAGES, SECTIONS } from "../navTypes.mts";
 export type { Package, Section } from "../navTypes.mts";
+
+export interface NpmPackageInfo {
+    name: string;
+    version: string;
+    versions: string[];
+    nextVersion?: string;
+}
+
+export type NpmData = Record<string, NpmPackageInfo>;
+
+export const npmData: NpmData;
+
+export interface HeadingNode {
+    route: string;
+    level: number;
+    title: string;
+}
+
+export interface PageNode extends HeadingNode {
+    children: Array<PageNode | HeadingNode>;
+    reference: string;
+}
