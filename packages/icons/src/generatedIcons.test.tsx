a07c57743b2fbcfae9c8caae00f16b4d344ebf7a
Greg Douglas <GWShark0@gmail.com>
2026-04-07 21:05:10 -0400

    Move *Icon into icon modules for reliable `@deprecated`

--- packages/icons/src/generatedIcons.test.tsx
+++ packages/icons/src/generatedIcons.test.tsx
@@ -5,26 +5,9 @@
 import { render } from "@testing-library/react";
 import { describe, it } from "vitest";
 
-import { Add, AddIcon } from "./generated/components";
+import { AddIcon } from "./generated/components";
 
-describe("<Add> icon component", () => {
-    it("allows attaching an event handler", () => {
-        const handleClick: React.MouseEventHandler<HTMLSpanElement> = () => undefined;
-        render(<Add onClick={handleClick} />);
-    });
-
-    it("disallows child elements", () => {
-        const handleClick: React.MouseEventHandler<HTMLSpanElement> = () => undefined;
-        render(
-            <Add onClick={handleClick}>
-                {/* @ts-expect-error */}
-                <path />
-            </Add>,
-        );
-    });
-});
-
-describe("<AddIcon> alias component", () => {
+describe("<AddIcon> icon component", () => {
     it("allows attaching an event handler", () => {
         const handleClick: React.MouseEventHandler<HTMLSpanElement> = () => undefined;
         render(<AddIcon onClick={handleClick} />);
