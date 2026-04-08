78d7db95886699736711988844fd2e0bce79381a
Greg Douglas <GWShark0@gmail.com>
2026-04-07 22:22:24 -0400

    fix(icons): build static icon components from resource SVG paths

---
+++ packages/icons/scripts/extractPathsFromResourceSvg.mjs
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2026 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// @ts-check
+
+import { readFileSync } from "node:fs";
+import { join } from "node:path";
+
+import { svgOptimizer } from "@blueprintjs/node-build-scripts";
+
+import { iconResourcesDir } from "./common.mjs";
+
+/**
+ * Extracts path `d` strings from an on-disk icon SVG. This matches the pipeline used for
+ * {@link generate-icon-paths.mjs} and the path modules consumed by {@code <Icon />} from core.
+ *
+ * @param {16 | 20} iconSize
+ * @param {string} iconName
+ * @returns {Promise<string[]>}
+ */
+export async function extractPathsFromResourceSvg(iconSize, iconName) {
+    const filepath = join(iconResourcesDir, `${iconSize}px`, `${iconName}.svg`);
+    const svg = readFileSync(filepath, "utf-8");
+    const optimizedSvg = await svgOptimizer.optimize(svg, { path: filepath });
+    /** @type string[] */
+    const paths = [];
+    const re = /\sd="([^"]+)"/g;
+    let m;
+    while ((m = re.exec(optimizedSvg.data)) !== null) {
+        paths.push(m[1].replace(/[\n\t]/g, ""));
+    }
+    return paths;
+}
