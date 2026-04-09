47d5c9457c022244b6cbf746410a45ddc3643b68
Greg Douglas <GWShark0@gmail.com>
2026-04-09 14:47:16 -0400

    Add icon onboarding and verification scripts

--- packages/icons/scripts/extractPathsFromResourceSvg.mjs
+++ packages/icons/scripts/extractPathsFromResourceSvg.mjs
@@ -32,15 +32,15 @@ import { iconResourcesDir } from "./common.mjs";
  * @returns {Promise<string[]>}
  */
 export async function extractPathsFromResourceSvg(iconSize, iconName) {
-    const filepath = join(iconResourcesDir, `${iconSize}px`, `${iconName}.svg`);
-    const svg = readFileSync(filepath, "utf-8");
-    const optimizedSvg = (await svgOptimizer.optimize(svg, { path: filepath })).data;
+    const path = join(iconResourcesDir, `${iconSize}px`, `${iconName}.svg`);
+    const source = readFileSync(path, "utf-8");
+    const optimized = svgOptimizer(source, { path }).data;
     /** @type string[] */
     const paths = [];
     // Match `d` attributes on `<path>` elements from our normalized SVGO output.
     const re = /<path[^>]*\sd="([^"]+)"/g;
     let m;
-    while ((m = re.exec(optimizedSvg)) !== null) {
+    while ((m = re.exec(optimized)) !== null) {
         paths.push(m[1]);
     }
     return paths;
