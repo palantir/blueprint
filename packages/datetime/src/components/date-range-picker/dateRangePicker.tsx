59f69207a83d722c5ad356dc38a4efdf39da1240
21f3002231 <cdnayak2000@gmail.com>
2026-05-22 14:00:12 +0530

    feature/date-reverse-ordered-range

--- packages/datetime/src/components/date-range-picker/dateRangePicker.tsx
+++ packages/datetime/src/components/date-range-picker/dateRangePicker.tsx
@@ -267,6 +267,16 @@ export class DateRangePicker extends DateFnsLocalizedComponent<DateRangePickerPr
         const newTimeRange: DateRange = [time[0], time[1]];
         newTimeRange[dateIndex] = newTime;
 
+        if (newDateRange[0] != null && newDateRange[1] != null && newDateRange[0] > newDateRange[1]) {
+        const temp = newDateRange[0];
+        newDateRange[0] = newDateRange[1];
+        newDateRange[1] = temp;
+
+        const tempTime = newTimeRange[0];
+        newTimeRange[0] = newTimeRange[1];
+        newTimeRange[1] = tempTime;
+    }
+
         this.props.onChange?.(newDateRange);
         this.setState({ time: newTimeRange, value: newDateRange });
     };
