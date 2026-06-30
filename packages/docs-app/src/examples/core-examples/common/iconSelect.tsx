be95028f3a8b6c6151c6dac09786f80ee09988c5
Greg Douglas <GWShark0@gmail.com>
2026-06-30 10:58:29 -0400

    Refactor IconSelect

--- packages/docs-app/src/examples/core-examples/common/iconSelect.tsx
+++ packages/docs-app/src/examples/core-examples/common/iconSelect.tsx
@@ -15,7 +15,7 @@
  */
 
 import classNames from "classnames";
-import { PureComponent } from "react";
+import { useCallback } from "react";
 
 import { Alignment, Button, Classes, MenuItem } from "@blueprintjs/core";
 import type { IconName } from "@blueprintjs/icons";
@@ -31,69 +31,72 @@ export interface IconSelectProps {
     onChange: (iconName?: IconName) => void;
 }
 
-export class IconSelect extends PureComponent<IconSelectProps> {
-    public render() {
-        const { disabled, iconName } = this.props;
-        return (
-            <label
-                className={classNames("icon-select", Classes.LABEL, {
-                    [Classes.DISABLED]: disabled,
-                })}
+export const IconSelect: React.FC<IconSelectProps> = ({ disabled, iconName, onChange }) => {
+    const renderIconItem: ItemRenderer<IconName> = useCallback(
+        (icon, { handleClick, handleFocus, modifiers }) => {
+            if (!modifiers.matchesPredicate) {
+                return null;
+            }
+            return (
+                <MenuItem
+                    roleStructure="listoption"
+                    active={modifiers.active}
+                    selected={iconName === icon}
+                    icon={icon}
+                    key={icon}
+                    onClick={handleClick}
+                    onFocus={handleFocus}
+                    text={icon}
+                />
+            );
+        },
+        [iconName],
+    );
+
+    const filterIconName = useCallback(
+        (query: string, name: IconName | undefined) => {
+            if (query === "") {
+                return name === iconName;
+            }
+            return name.toLowerCase().indexOf(query.toLowerCase()) >= 0;
+        },
+        [iconName],
+    );
+
+    const handleIconChange = useCallback(
+        (icon: IconName) => {
+            onChange(icon === iconName ? undefined : icon);
+        },
+        [iconName, onChange],
+    );
+
+    return (
+        <label
+            className={classNames("icon-select", Classes.LABEL, {
+                [Classes.DISABLED]: disabled,
+            })}
+        >
+            Icon
+            <Select
+                disabled={disabled}
+                items={ICON_NAMES}
+                itemPredicate={filterIconName}
+                itemRenderer={renderIconItem}
+                noResults={<MenuItem disabled={true} text="No results" />}
+                placeholder="Start typing to search…"
+                onItemSelect={handleIconChange}
+                popoverProps={{ minimal: true }}
             >
-                Icon
-                <Select<IconName>
+                <Button
+                    alignText={Alignment.START}
+                    textClassName={Classes.TEXT_OVERFLOW_ELLIPSIS}
                     disabled={disabled}
-                    items={ICON_NAMES}
-                    itemPredicate={this.filterIconName}
-                    itemRenderer={this.renderIconItem}
-                    noResults={<MenuItem disabled={true} text="No results" />}
-                    placeholder="Start typing to search…"
-                    onItemSelect={this.handleIconChange}
-                    popoverProps={{ minimal: true }}
-                >
-                    <Button
-                        alignText={Alignment.START}
-                        textClassName={Classes.TEXT_OVERFLOW_ELLIPSIS}
-                        disabled={disabled}
-                        fill={true}
-                        icon={iconName}
-                        text={iconName || "None"}
-                        endIcon="caret-down"
-                    />
-                </Select>
-            </label>
-        );
-    }
-
-    private renderIconItem: ItemRenderer<IconName | undefined> = (
-        icon,
-        { handleClick, handleFocus, modifiers },
-    ) => {
-        if (!modifiers.matchesPredicate) {
-            return null;
-        }
-        return (
-            <MenuItem
-                roleStructure="listoption"
-                active={modifiers.active}
-                selected={this.props.iconName === icon}
-                icon={icon}
-                key={icon}
-                onClick={handleClick}
-                onFocus={handleFocus}
-                text={icon}
-            />
-        );
-    };
-
-    private filterIconName = (query: string, iconName: IconName | undefined) => {
-        if (query === "") {
-            return iconName === this.props.iconName;
-        }
-        return iconName.toLowerCase().indexOf(query.toLowerCase()) >= 0;
-    };
-
-    private handleIconChange = (icon: IconName) => {
-        this.props.onChange(icon === this.props.iconName ? undefined : icon);
-    };
-}
+                    fill={true}
+                    icon={iconName}
+                    text={iconName || "None"}
+                    endIcon="caret-down"
+                />
+            </Select>
+        </label>
+    );
+};
