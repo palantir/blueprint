d871b7d6c62158143736729870fcd0b8aa3d64d2
nfdiop <nfdiop@palantir.com>
2026-09-01 13:14:16 +0200

    fix compilation

--- packages/core/src/context/blueprintThemeProvider.tsx
+++ packages/core/src/context/blueprintThemeProvider.tsx
@@ -2,15 +2,19 @@
  * (c) Copyright 2026 Palantir Technologies Inc. All rights reserved.
  */
 
+import classNames from "classnames";
+
+import { type Props } from "../common/props";
+
 import { BLUEPRINT_NEXT_CLASS, BlueprintThemeContext, type BlueprintTokenMap } from "./blueprintThemeContext";
 
-export interface BlueprintThemeProviderProps {
+export interface BlueprintThemeProviderProps extends Props {
     readonly children?: React.ReactNode;
     readonly tokens: BlueprintTokenMap;
 }
 
 /** Applies BP7 light tokens to a DOM subtree and any Blueprint portals it opens. */
-export function BlueprintThemeProvider({ children, tokens }: BlueprintThemeProviderProps) {
+export function BlueprintThemeProvider({ children, className, tokens }: BlueprintThemeProviderProps) {
     const style: React.CSSProperties = {
         // The wrapper provides a CSS inheritance boundary without participating in layout.
         display: "contents",
@@ -19,7 +23,7 @@ export function BlueprintThemeProvider({ children, tokens }: BlueprintThemeProvi
 
     return (
         <BlueprintThemeContext.Provider value={tokens}>
-            <div className={BLUEPRINT_NEXT_CLASS} style={style}>
+            <div className={classNames(BLUEPRINT_NEXT_CLASS, className)} style={style}>
                 {children}
             </div>
         </BlueprintThemeContext.Provider>
